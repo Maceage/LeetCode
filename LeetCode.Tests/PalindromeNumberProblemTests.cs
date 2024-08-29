132ec6b13f48cfef5822221cbac931a13c3141cd
Graham Mace <graham@maceage.com>
2024-08-29 16:03:48 +1000

    Add PalindromeNumberProblem.IsPalindromeInteger() alternative and tests

--- LeetCode.Tests/PalindromeNumberProblemTests.cs
+++ LeetCode.Tests/PalindromeNumberProblemTests.cs
@@ -40,4 +40,40 @@ public class PalindromeNumberProblemTests
         // Act/Assert
         palindromeNumberProblem.IsPalindrome(x).Should().BeFalse();
     }
+
+    [Fact]
+    public void IsPalindromeInteger_TestCase_1()
+    {
+        // Arrange
+        int x = 121;
+
+        PalindromeNumberProblem palindromeNumberProblem = new PalindromeNumberProblem();
+
+        // Act/Assert
+        palindromeNumberProblem.IsPalindromeInteger(x).Should().BeTrue();
+    }
+
+    [Fact]
+    public void IsPalindromeInteger_TestCase_2()
+    {
+        // Arrange
+        int x = -121;
+
+        PalindromeNumberProblem palindromeNumberProblem = new PalindromeNumberProblem();
+
+        // Act/Assert
+        palindromeNumberProblem.IsPalindromeInteger(x).Should().BeFalse();
+    }
+
+    [Fact]
+    public void IsPalindromeInteger_TestCase_3()
+    {
+        // Arrange
+        int x = 10;
+
+        PalindromeNumberProblem palindromeNumberProblem = new PalindromeNumberProblem();
+
+        // Act/Assert
+        palindromeNumberProblem.IsPalindromeInteger(x).Should().BeFalse();
+    }
 }
