a0b1462d1a8c280b6f7a93b4509faffb2825df24
Graham Mace <graham@maceage.com>
2024-08-30 01:20:36 +1000

    Add LongestCommonPrefixProblem and tests

---
+++ LeetCode.Problems/LongestCommonPrefixProblem.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.Problems;
+
+public class LongestCommonPrefixProblem
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        string longestPrefix = string.Empty;
+        string prefix = string.Empty;
+
+        string firstString = strs[0];
+
+        foreach (var t in firstString)
+        {
+            prefix += t;
+
+            bool allStartWith = true;
+
+            foreach (string str in strs)
+            {
+                if (!str.StartsWith(prefix))
+                {
+                    allStartWith = false;
+                    break;
+                }
+            }
+
+            if (allStartWith == false)
+            {
+                longestPrefix = prefix.Remove(prefix.Length - 1);
+                break;
+            }
+        }
+
+        return longestPrefix;
+    }
+}
---
+++ LeetCode.Tests/LongestCommonPrefixProblemTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using LeetCode.Problems;
+
+namespace LeetCode.Tests;
+
+public class LongestCommonPrefixProblemTests
+{
+    [Fact]
+    public void LongestCommonPrefix_TestCase_1()
+    {
+        // Arrange
+        string[] strings = ["flower", "flow", "flight"];
+
+        // Act
+        LongestCommonPrefixProblem longestCommonPrefixProblem = new LongestCommonPrefixProblem();
+
+        // Assert
+        longestCommonPrefixProblem.LongestCommonPrefix(strings).Should().Be("fl");
+    }
+
+    [Fact]
+    public void LongestCommonPrefix_TestCase_2()
+    {
+        string[] strings = ["dog", "racecar", "car"];
+
+        // Act
+        LongestCommonPrefixProblem longestCommonPrefixProblem = new LongestCommonPrefixProblem();
+
+        // Assert
+        longestCommonPrefixProblem.LongestCommonPrefix(strings).Should().BeEmpty();
+    }
+}
