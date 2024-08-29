d2e210e0633601c2004bfc9ff01e9936db7dd0ec
Graham Mace <graham@maceage.com>
2024-08-29 16:01:51 +1000

    Add PalindromeNumberProblem and tests

---
+++ LeetCode.Problems/PalindromeNumberProblem.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode.Problems;
+
+public class PalindromeNumberProblem
+{
+    public bool IsPalindrome(int x)
+    {
+        bool isPalindrome = true;
+
+        string integerString = x.ToString();
+
+        Queue<char> charQueue = new Queue<char>();
+
+        if (integerString.Length > 0)
+        {
+            foreach (var c in integerString)
+            {
+                charQueue.Enqueue(c);
+            }
+
+            for (int i = integerString.Length - 1; i > 0; i--)
+            {
+                char arrayChar = integerString[i];
+                char queueChar = charQueue.Dequeue();
+
+                var matchesChar = (arrayChar == queueChar);
+
+                if (!matchesChar)
+                {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+        }
+
+        return isPalindrome;
+    }
+}
---
+++ LeetCode.Tests/PalindromeNumberProblemTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using LeetCode.Problems;
+
+namespace LeetCode.Tests;
+
+public class PalindromeNumberProblemTests
+{
+    [Fact]
+    public void IsPalindrome_TestCase_1()
+    {
+        // Arrange
+        int x = 121;
+
+        PalindromeNumberProblem palindromeNumberProblem = new PalindromeNumberProblem();
+
+        // Act/Assert
+        palindromeNumberProblem.IsPalindrome(x).Should().BeTrue();
+    }
+
+    [Fact]
+    public void IsPalindrome_TestCase_2()
+    {
+        // Arrange
+        int x = -121;
+
+        PalindromeNumberProblem palindromeNumberProblem = new PalindromeNumberProblem();
+
+        // Act/Assert
+        palindromeNumberProblem.IsPalindrome(x).Should().BeFalse();
+    }
+
+    [Fact]
+    public void IsPalindrome_TestCase_3()
+    {
+        // Arrange
+        int x = 10;
+
+        PalindromeNumberProblem palindromeNumberProblem = new PalindromeNumberProblem();
+
+        // Act/Assert
+        palindromeNumberProblem.IsPalindrome(x).Should().BeFalse();
+    }
+}
