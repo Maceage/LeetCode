c9934d411e2f364dfa2d8fdba6ff8c2db5acbac8
Graham Mace <graham@maceage.com>
2024-09-04 14:13:30 +1000

    Add ValidParenthesesProblem and tests

---
+++ LeetCode.Problems/ValidParenthesesProblem.cs
@@ -0,0 +1,49 @@
+﻿namespace LeetCode.Problems;
+
+public class ValidParenthesesProblem
+{
+    private readonly Stack<char> _charStack = new();
+
+    private readonly Dictionary<char, char> _openingBracketsDictionary = new()
+    {
+        {'(', ')'},
+        {'{', '}'},
+        {'[', ']'}
+    };
+
+    private readonly Dictionary<char, char> _closingBracketsDictionary = new()
+    {
+        {')', '('},
+        {'}', '{'},
+        {']', '['}
+    };
+
+    public bool IsValid(string s)
+    {
+        bool isValid = false;
+
+        foreach (var c in s.ToCharArray())
+        {
+            if (_openingBracketsDictionary.ContainsKey(c))
+            {
+                _charStack.Push(c);
+            }
+            else if (_closingBracketsDictionary.TryGetValue(c, out var openingBracket))
+            {
+                char lastChar = _charStack.Pop();
+
+                if (lastChar != openingBracket)
+                {
+                    break;
+                }
+            }
+        }
+
+        if (_charStack.Count == 0)
+        {
+            isValid = true;
+        }
+
+        return isValid;
+    }
+}
