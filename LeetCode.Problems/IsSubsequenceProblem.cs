ac75ab1e1782fab18a553567cb51e23cba657a20
Graham Mace <graham@maceage.com>
2024-09-19 17:11:13 +1000

    Add IsSubsequenceProblem and tests

---
+++ LeetCode.Problems/IsSubsequenceProblem.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Problems;
+
+public class IsSubsequenceProblem
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        int sourceIndex = 0;
+        int targetIndex = 0;
+
+        while ((sourceIndex < s.Length) && (targetIndex < t.Length))
+        {
+            char sourceChar = s[sourceIndex];
+
+            while (targetIndex < t.Length)
+            {
+                char targetChar = t[targetIndex];
+
+                if (targetChar == sourceChar)
+                {
+                    sourceIndex++;
+                    targetIndex++;
+                    break;
+                }
+
+                targetIndex++;
+            }
+        }
+
+        return (sourceIndex == s.Length);
+    }
+}
---
+++ LeetCode.Tests/IsSubsequenceProblemTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using LeetCode.Problems;
+
+namespace LeetCode.Tests;
+
+public class IsSubsequenceProblemTests
+{
+    [Fact]
+    public void IsSubsequence_TestCase_1()
+    {
+        // Arrange
+        string s = "abc";
+        string t = "ahbgdc";
+
+        IsSubsequenceProblem isSubsequenceProblem = new IsSubsequenceProblem();
+
+        // Act
+        bool actual = isSubsequenceProblem.IsSubsequence(s, t);
+
+        // Assert
+        actual.Should().BeTrue();
+    }
+
+    [Fact]
+    public void IsSubsequence_TestCase_2()
+    {
+        // Arrange
+        string s = "axc";
+        string t = "ahbgdc";
+
+        IsSubsequenceProblem isSubsequenceProblem = new IsSubsequenceProblem();
+
+        // Act
+        bool actual = isSubsequenceProblem.IsSubsequence(s, t);
+
+        // Assert
+        actual.Should().BeFalse();
+    }
+}
