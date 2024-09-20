e1c5d11154006d371d4b8a4d2ebaeb1dcf6479ce
Graham Mace <graham@maceage.com>
2024-09-20 15:48:19 +1000

    Add ContainerWithTheMostWaterProblem and tests

---
+++ LeetCode.Problems/ContainerWithTheMostWaterProblem.cs
@@ -0,0 +1,66 @@
+﻿namespace LeetCode.Problems;
+
+public class ContainerWithTheMostWaterProblem
+{
+    public int MaxArea(int[] height)
+    {
+        int maxArea = 0;
+        int leftIndex = 0;
+        int rightIndex = height.Length - 1;
+
+        while (leftIndex < rightIndex)
+        {
+            int width = rightIndex - leftIndex;
+            
+            int leftValue = height[leftIndex];
+            int rightValue = height[rightIndex];
+
+            int minHeight = leftValue > rightValue ? rightValue : leftValue;
+
+            int containerArea = minHeight * width;
+
+            if (containerArea > maxArea)
+            {
+                maxArea = containerArea;
+            }
+
+            if (leftValue <= rightValue)
+            {
+                leftIndex++;
+            }
+            else
+            {
+                rightIndex--;
+            }
+        }
+
+        return maxArea;
+    }
+
+    //public int MaxArea(int[] height)
+    //{
+    //    int maxArea = 0;
+
+    //    for (int i = 0; i < height.Length; i++)
+    //    {
+    //        int firstValue = height[i];
+
+    //        for (int j = i + 1; j < height.Length; j++)
+    //        {
+    //            int secondValue = height[j];
+
+    //            int minHeight = firstValue > secondValue ? secondValue : firstValue;
+    //            int width = j - i;
+
+    //            int containerArea = minHeight * width;
+
+    //            if (containerArea > maxArea)
+    //            {
+    //                maxArea = containerArea;
+    //            }
+    //        }
+    //    }
+
+    //    return maxArea;
+    //}
+}
---
+++ LeetCode.Tests/ContainerWithTheMostWaterProblemTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using LeetCode.Problems;
+
+namespace LeetCode.Tests;
+
+public class ContainerWithTheMostWaterProblemTests
+{
+    [Fact]
+    public void MaxArea_TestCase_1()
+    {
+        // Arrange
+        int[] height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+        int expected = 49;
+
+        ContainerWithTheMostWaterProblem containerWithTheMostWater = new ContainerWithTheMostWaterProblem();
+
+        // Act
+        int actual = containerWithTheMostWater.MaxArea(height);
+
+        // Assert
+        actual.Should().Be(expected);
+    }
+
+    [Fact]
+    public void MaxArea_TestCase_2()
+    {
+        // Arrange
+        int[] height = [1, 1];
+        int expected = 1;
+
+        ContainerWithTheMostWaterProblem containerWithTheMostWater = new ContainerWithTheMostWaterProblem();
+
+        // Act
+        int actual = containerWithTheMostWater.MaxArea(height);
+
+        // Assert
+        actual.Should().Be(expected);
+    }
+}
