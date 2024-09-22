7215448a2438eade6ef3641ee3adb49fba7d899e
Graham Mace <graham@maceage.com>
2024-09-22 17:56:43 +1000

    Add MaximumNumberOfVowelsInASubstringOfGivenLengthProblem and tests

---
+++ LeetCode.Problems/MaximumNumberOfVowelsInASubstringOfGivenLengthProblem.cs
@@ -0,0 +1,51 @@
+﻿namespace LeetCode.Problems;
+
+public class MaximumNumberOfVowelsInASubstringOfGivenLengthProblem
+{
+    private const string Vowels = "aeiou";
+
+    public int MaxVowels(string s, int k)
+    {
+        int totalVowels = 0;
+
+        for (int i = 0; i < k; i++)
+        {
+            char currentChar = s[i];
+
+            if (Vowels.Contains(currentChar))
+            {
+                totalVowels++;
+            }
+        }
+
+        int maxVowels = totalVowels;
+
+        for (int i = k; i <= s.Length - 1; i++)
+        {
+            char currentChar = s[i];
+            char previousChar = s[i - k];
+
+            bool isCurrentCharVowel = Vowels.Contains(currentChar);
+            bool isPreviousCharVowel = Vowels.Contains(previousChar);
+
+            if ((totalVowels > 0) && isPreviousCharVowel)
+            {
+                totalVowels--;
+            }
+
+            if (isCurrentCharVowel)
+            {
+                totalVowels++;
+            }
+
+            if (maxVowels == k)
+            {
+                break;
+            }
+
+            maxVowels = Math.Max(maxVowels, totalVowels);
+        }
+
+        return maxVowels;
+    }
+}
