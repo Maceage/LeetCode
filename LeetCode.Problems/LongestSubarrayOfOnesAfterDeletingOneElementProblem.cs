4e3e5e9fe9689e0fa92fee7200164b7da7aaa284
Graham Mace <graham@maceage.com>
2024-09-23 16:23:23 +1000

    Add LongestSubarrayOfOnesAfterDeletingOneElementProblem and tests

---
+++ LeetCode.Problems/LongestSubarrayOfOnesAfterDeletingOneElementProblem.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCode.Problems;
+
+public class LongestSubarrayOfOnesAfterDeletingOneElementProblem
+{
+    public int LongestSubarray(int[] nums)
+    {
+        int longestSubarray = 0;
+
+        int left = 0;
+        int right = 1;
+
+        int lastZeroIndex = -1;
+
+        if (nums[left] == 0)
+        {
+            lastZeroIndex = 0;
+        }
+
+        while (right < nums.Length)
+        {
+            int rightValue = nums[right];
+
+            if (rightValue == 1)
+            {
+                right++;
+            }
+            else if (rightValue == 0)
+            {
+                left = lastZeroIndex + 1;
+                lastZeroIndex = right;
+
+                right++;
+            }
+
+            int subArrayLength = right - left;
+
+            longestSubarray = Math.Max(longestSubarray, subArrayLength);
+        }
+
+        longestSubarray = longestSubarray > 0 ? longestSubarray - 1 : 0;
+
+        return longestSubarray;
+    }
+}
