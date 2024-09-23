cf4dca198bdaeaf920c1d5c2d0a34fd1b8731414
Graham Mace <graham@maceage.com>
2024-09-23 17:56:43 +1000

    Add FindTheDifferenceOfTwoArraysProblem and tests

---
+++ LeetCode.Problems/FindTheDifferenceOfTwoArraysProblem.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCode.Problems;
+
+public class FindTheDifferenceOfTwoArraysProblem
+{
+    public IList<IList<int>> FindDifference(int[] nums1, int[] nums2)
+    {
+        Dictionary<int, bool> nums1Dictionary = new Dictionary<int, bool>();
+        Dictionary<int, bool> nums2Dictionary = new Dictionary<int, bool>();
+
+        foreach (var nums1Value in nums1)
+        {
+            nums1Dictionary.TryAdd(nums1Value, true);
+        }
+
+        foreach (var nums2Value in nums2)
+        {
+            nums2Dictionary.TryAdd(nums2Value, true);
+        }
+
+        List<int> nums1Distinct = new List<int>();
+
+        foreach (var nums1Value in nums1)
+        {
+            if (!nums2Dictionary.ContainsKey(nums1Value) && (!nums1Distinct.Contains(nums1Value)))
+            {
+                nums1Distinct.Add(nums1Value);
+            }
+        }
+
+        List<int> nums2Distinct = new List<int>();
+
+        foreach (var nums2Value in nums2)
+        {
+            if (!nums1Dictionary.ContainsKey(nums2Value) && (!nums2Distinct.Contains(nums2Value)))
+            {
+                nums2Distinct.Add(nums2Value);
+            }
+        }
+
+        return new List<IList<int>>
+        {
+            nums1Distinct,
+            nums2Distinct,
+        };
+    }
+}
---
+++ LeetCode.Tests/FindTheDifferenceOfTwoArraysProblemTests.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using LeetCode.Problems;
+
+namespace LeetCode.Tests;
+
+public class FindTheDifferenceOfTwoArraysProblemTests
+{
+    [Fact]
+    public void FindDifference_TestCase_1()
+    {
+        // Arrange
+        int[] nums1 = [1, 2, 3];
+        int[] nums2 = [2, 4, 6];
+
+        FindTheDifferenceOfTwoArraysProblem findTheDifferenceOfTwoArraysProblem = new FindTheDifferenceOfTwoArraysProblem();
+
+        // Act
+        IList<IList<int>> actual = findTheDifferenceOfTwoArraysProblem.FindDifference(nums1, nums2);
+
+        // Assert
+        List<List<int>> expected =
+        [
+            new() { 1, 3 },
+            new() { 4, 6 }
+        ];
+
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void FindDifference_TestCase_2()
+    {
+        // Arrange
+        int[] nums1 = [1, 2, 3, 3];
+        int[] nums2 = [1, 1, 2, 2];
+
+        FindTheDifferenceOfTwoArraysProblem findTheDifferenceOfTwoArraysProblem = new FindTheDifferenceOfTwoArraysProblem();
+
+        // Act
+        IList<IList<int>> actual = findTheDifferenceOfTwoArraysProblem.FindDifference(nums1, nums2);
+
+        // Assert
+        List<List<int>> expected =
+        [
+            new() { 3 },
+            new() {}
+        ];
+
+        actual.Should().BeEquivalentTo(expected);
+    }
+}
