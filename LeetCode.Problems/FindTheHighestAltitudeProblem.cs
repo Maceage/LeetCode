b4e2c5545e825c1ac3adacb75788e14e126776f6
Graham Mace <graham@maceage.com>
2024-09-23 16:42:13 +1000

    Add FindTheHighestAltitudeProblem and tests

---
+++ LeetCode.Problems/FindTheHighestAltitudeProblem.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Problems;
+
+public class FindTheHighestAltitudeProblem
+{
+    public int LargestAltitude(int[] gain)
+    {
+        int currentGain = 0;
+        int highestAltitude = 0;
+
+        foreach (var g in gain)
+        {
+            currentGain += g;
+
+            highestAltitude = Math.Max(highestAltitude, currentGain);
+        }
+
+        return highestAltitude;
+    }
+}
---
+++ LeetCode.Tests/FindTheHighestAltitudeProblemTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using LeetCode.Problems;
+
+namespace LeetCode.Tests;
+
+public class FindTheHighestAltitudeProblemTests
+{
+    [Fact]
+    public void LargestAltitude_TestCase_1()
+    {
+        // Arrange
+        int[] gain = [-5, 1, 5, 0, -7];
+
+        FindTheHighestAltitudeProblem findTheHighestAltitudeProblem = new FindTheHighestAltitudeProblem();
+
+        // Act
+        int actual = findTheHighestAltitudeProblem.LargestAltitude(gain);
+
+        // Assert
+        actual.Should().Be(1);
+    }
+
+    [Fact]
+    public void LargestAltitude_TestCase_2()
+    {
+        // Arrange
+        int[] gain = [-4, -3, -2, -1, 4, 3, 2];
+
+        FindTheHighestAltitudeProblem findTheHighestAltitudeProblem = new FindTheHighestAltitudeProblem();
+
+        // Act
+        int actual = findTheHighestAltitudeProblem.LargestAltitude(gain);
+
+        // Assert
+        actual.Should().Be(0);
+    }
+}
