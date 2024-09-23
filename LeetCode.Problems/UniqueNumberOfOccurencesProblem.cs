5877a506f3a77d748fc86e67d727a29ff4439ecf
Graham Mace <graham@maceage.com>
2024-09-23 18:19:02 +1000

    Add UniqueNumberOfOccurrencesProblem and tests

---
+++ LeetCode.Problems/UniqueNumberOfOccurencesProblem.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Problems;
+
+public class UniqueNumberOfOccurrencesProblem
+{
+    public bool UniqueOccurrences(int[] arr)
+    {
+        Dictionary<int, int> occurrencesDictionary = new Dictionary<int, int>();
+
+        foreach (int i in arr)
+        {
+            if (!occurrencesDictionary.TryAdd(i, 1))
+            {
+                occurrencesDictionary[i]++;
+            }
+        }
+
+        Dictionary<int, int> occurrencesCountDictionary = new Dictionary<int, int>();
+
+        foreach (var keyValuePair in occurrencesDictionary)
+        {
+            int count = keyValuePair.Value;
+
+            if (!occurrencesCountDictionary.TryAdd(count, 1))
+            {
+                occurrencesCountDictionary[count]++;
+            }
+        }
+
+        return (occurrencesDictionary.Count == occurrencesCountDictionary.Count);
+    }
+}
---
+++ LeetCode.Tests/UniqueNumberOfOccurrencesProblemTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using LeetCode.Problems;
+
+namespace LeetCode.Tests;
+
+public class UniqueNumberOfOccurrencesProblemTests
+{
+    [Fact]
+    public void UniqueOccurrences_TestCase_1()
+    {
+        // Arrange
+        int[] arr = [1, 2, 2, 1, 1, 3];
+
+        UniqueNumberOfOccurrencesProblem uniqueNumberOfOccurrencesProblem = new UniqueNumberOfOccurrencesProblem();
+
+        // Act
+        bool actual = uniqueNumberOfOccurrencesProblem.UniqueOccurrences(arr);
+
+        // Assert
+        actual.Should().BeTrue();
+    }
+
+    [Fact]
+    public void UniqueOccurrences_TestCase_2()
+    {
+        // Arrange
+        int[] arr = [1, 2];
+
+        UniqueNumberOfOccurrencesProblem uniqueNumberOfOccurrencesProblem = new UniqueNumberOfOccurrencesProblem();
+
+        // Act
+        bool actual = uniqueNumberOfOccurrencesProblem.UniqueOccurrences(arr);
+
+        // Assert
+        actual.Should().BeFalse();
+    }
+
+    [Fact]
+    public void UniqueOccurrences_TestCase_3()
+    {
+        // Arrange
+        int[] arr = [-3, 0, 1, -3, 1, 1, 1, -3, 10, 0];
+
+        UniqueNumberOfOccurrencesProblem uniqueNumberOfOccurrencesProblem = new UniqueNumberOfOccurrencesProblem();
+
+        // Act
+        bool actual = uniqueNumberOfOccurrencesProblem.UniqueOccurrences(arr);
+
+        // Assert
+        actual.Should().BeTrue();
+    }
+}
