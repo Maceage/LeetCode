cda0f5b399b6aa737477c9fa71e3bee7da1b972a
Graham Mace <graham@maceage.com>
2024-09-23 14:58:56 +1000

    Add MaxConsecutiveOnesProblem and tests

---
+++ LeetCode.Problems/MaxConsecutiveOnesProblem.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Problems;
+
+public class MaxConsecutiveOnesProblem
+{
+    public int LongestOnes(int[] nums, int k)
+    {
+        int left = 0;
+        int right;
+
+        for (right = 0; right < nums.Length; right++)
+        {
+            int rightValue = nums[right];
+
+            // Reduce k if we find a zero
+            if (rightValue == 0)
+            {
+                k--;
+            }
+
+            // If left is zero then increase k again
+            if (k < 0)
+            {
+                int leftValue = nums[left];
+
+                k += 1 - leftValue;
+                left++;
+            }
+        }
+
+        return right - left;
+    }
+}
---
+++ LeetCode.Tests/MaxConsecutiveOnesProblemTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using LeetCode.Problems;
+
+namespace LeetCode.Tests;
+
+public class MaxConsecutiveOnesProblemTests
+{
+    [Fact]
+    public void LongestOnes_TestCase_1()
+    {
+        // Arrange
+        int[] nums = [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0];
+        int k = 2;
+
+        MaxConsecutiveOnesProblem maxConsecutiveOnesProblem = new MaxConsecutiveOnesProblem();
+
+        // Act
+        int actual = maxConsecutiveOnesProblem.LongestOnes(nums, k);
+
+        // Assert
+        actual.Should().Be(6);
+    }
+
+    [Fact]
+    public void LongestOnes_TestCase_2()
+    {
+        // Arrange
+        int[] nums = [0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1];
+        int k = 3;
+
+        MaxConsecutiveOnesProblem maxConsecutiveOnesProblem = new MaxConsecutiveOnesProblem();
+
+        // Act
+        int actual = maxConsecutiveOnesProblem.LongestOnes(nums, k);
+
+        // Assert
+        actual.Should().Be(10);
+    }
+}
