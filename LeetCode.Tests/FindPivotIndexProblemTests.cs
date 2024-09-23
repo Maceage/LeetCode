c1f8cbf0350c363d2c350399263033dd62f94932
Graham Mace <graham@maceage.com>
2024-09-23 17:21:31 +1000

    Add FindPivotIndexProblem and tests

---
+++ LeetCode.Tests/FindPivotIndexProblemTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using LeetCode.Problems;
+
+namespace LeetCode.Tests;
+
+public class FindPivotIndexProblemTests
+{
+    [Fact]
+    public void PivotIndex_TestCase_1()
+    {
+        // Arrange
+        int[] nums = [1, 7, 3, 6, 5, 6];
+
+        FindPivotIndexProblem findPivotIndexProblem = new FindPivotIndexProblem();
+
+        // Act
+        int actual = findPivotIndexProblem.PivotIndex(nums);
+
+        // Assert
+        actual.Should().Be(3);
+    }
+
+    [Fact]
+    public void PivotIndex_TestCase_2()
+    {
+        // Arrange
+        int[] nums = [1, 2, 3];
+
+        FindPivotIndexProblem findPivotIndexProblem = new FindPivotIndexProblem();
+
+        // Act
+        int actual = findPivotIndexProblem.PivotIndex(nums);
+
+        // Assert
+        actual.Should().Be(-1);
+    }
+
+    [Fact]
+    public void PivotIndex_TestCase_3()
+    {
+        // Arrange
+        int[] nums = [2, 1, -1];
+
+        FindPivotIndexProblem findPivotIndexProblem = new FindPivotIndexProblem();
+
+        // Act
+        int actual = findPivotIndexProblem.PivotIndex(nums);
+
+        // Assert
+        actual.Should().Be(0);
+    }
+}
