17747cb38ddb04be07387480b4c1044dc69a54d7
Graham Mace <graham@maceage.com>
2024-09-24 18:40:20 +1000

    Add EqualRowAndColumnPairsProblem and tests

---
+++ LeetCode.Problems/EqualRowAndColumnPairsProblem.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+namespace LeetCode.Problems;
+
+public class EqualRowAndColumnPairsProblem
+{
+    public int EqualPairs(int[][] grid)
+    {
+        int pairCount = 0;
+
+        Dictionary<string, int> gridRowCounts = new Dictionary<string, int>();
+
+        foreach (var pair in grid)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (var i in pair)
+            {
+                stringBuilder.Append(i);
+                stringBuilder.Append(",");
+            }
+
+            string gridKey = stringBuilder.ToString();
+
+            if (!gridRowCounts.TryAdd(gridKey, 1))
+            {
+                gridRowCounts[gridKey]++;
+            }
+        }
+
+        for (int i = 0; i < grid.Length; i++)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (var pair in grid)
+            {
+                int key = pair[i];
+
+                stringBuilder.Append(key);
+                stringBuilder.Append(",");
+            }
+
+            string gridKey = stringBuilder.ToString();
+
+            if (gridRowCounts.TryGetValue(gridKey, out var count))
+            {
+                pairCount += count;
+            }
+        }
+
+        return pairCount;
+    }
+}
---
+++ LeetCode.Tests/EqualRowAndColumnPairsProblemTests.cs
@@ -0,0 +1,67 @@
+﻿using FluentAssertions;
+using LeetCode.Problems;
+
+namespace LeetCode.Tests;
+
+public class EqualRowAndColumnPairsProblemTests
+{
+    [Fact]
+    public void EqualPairs_TestCase_1()
+    {
+        // Arrange
+        int[][] grid = [[3, 2, 1], [1, 7, 6], [2, 7, 7]];
+
+        EqualRowAndColumnPairsProblem equalRowAndColumnPairsProblem = new EqualRowAndColumnPairsProblem();
+
+        // Act
+        int actual = equalRowAndColumnPairsProblem.EqualPairs(grid);
+
+        // Assert
+        actual.Should().Be(1);
+    }
+
+    [Fact]
+    public void EqualPairs_TestCase_2()
+    {
+        // Arrange
+        int[][] grid = [[3, 1, 2, 2], [1, 4, 4, 5], [2, 4, 2, 2], [2, 4, 2, 2]];
+
+        EqualRowAndColumnPairsProblem equalRowAndColumnPairsProblem = new EqualRowAndColumnPairsProblem();
+
+        // Act
+        int actual = equalRowAndColumnPairsProblem.EqualPairs(grid);
+
+        // Assert
+        actual.Should().Be(3);
+    }
+
+    [Fact]
+    public void EqualPairs_TestCase_3()
+    {
+        // Arrange
+        int[][] grid = [[3, 2, 1], [1, 7, 6], [2, 7, 7]];
+
+        EqualRowAndColumnPairsProblem equalRowAndColumnPairsProblem = new EqualRowAndColumnPairsProblem();
+
+        // Act
+        int actual = equalRowAndColumnPairsProblem.EqualPairs(grid);
+
+        // Assert
+        actual.Should().Be(1);
+    }
+
+    [Fact]
+    public void EqualPairs_TestCase_4()
+    {
+        // Arrange
+        int[][] grid = [[11, 1], [1, 11]];
+
+        EqualRowAndColumnPairsProblem equalRowAndColumnPairsProblem = new EqualRowAndColumnPairsProblem();
+
+        // Act
+        int actual = equalRowAndColumnPairsProblem.EqualPairs(grid);
+
+        // Assert
+        actual.Should().Be(2);
+    }
+}
