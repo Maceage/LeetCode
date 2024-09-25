44dc2eb0ff13d16fd31011baf1742221c54d8010
Graham Mace <graham@maceage.com>
2024-09-25 14:14:46 +1000

    Add RemoveStarsFromAStringProblem and tests

---
+++ LeetCode.Tests/RemoveStarsFromAStringProblemTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using LeetCode.Problems;
+
+namespace LeetCode.Tests;
+
+public class RemoveStarsFromAStringProblemTests
+{
+    [Fact]
+    public void RemoveStars_TestCase_1()
+    {
+        // Arrange
+        string s = "leet**cod*e";
+
+        RemoveStarsFromAStringProblem removeStarsFromAStringProblem = new RemoveStarsFromAStringProblem();
+
+        // Act
+        string actual = removeStarsFromAStringProblem.RemoveStars(s);
+
+        // Assert
+        actual.Should().Be("lecoe");
+    }
+
+    [Fact]
+    public void RemoveStars_TestCase_2()
+    {
+        // Arrange
+        string s = "erase*****";
+
+        RemoveStarsFromAStringProblem removeStarsFromAStringProblem = new RemoveStarsFromAStringProblem();
+
+        // Act
+        string actual = removeStarsFromAStringProblem.RemoveStars(s);
+
+        // Assert
+        actual.Should().Be(string.Empty);
+    }
+}
