e7010c299075f7ba8dbbc62c827ca0f5ac2feb94
Graham Mace <graham@maceage.com>
2024-09-30 16:56:25 +1000

    Add Dota2SenateProblem and tests

---
+++ LeetCode.Tests/Dota2SenateProblemTests.cs
@@ -0,0 +1,67 @@
+﻿using FluentAssertions;
+using LeetCode.Problems;
+
+namespace LeetCode.Tests;
+
+public class Dota2SenateProblemTests
+{
+    [Fact]
+    public void PredictPartyVictory_TestCase_1()
+    {
+        // Arrange
+        string senate = "RD";
+
+        Dota2SenateProblem dota2SenateProblem = new Dota2SenateProblem();
+
+        // Act
+        string actual = dota2SenateProblem.PredictPartyVictory(senate);
+
+        // Assert
+        actual.Should().Be("Radiant");
+    }
+
+    [Fact]
+    public void PredictPartyVictory_TestCase_2()
+    {
+        // Arrange
+        string senate = "RDD";
+
+        Dota2SenateProblem dota2SenateProblem = new Dota2SenateProblem();
+
+        // Act
+        string actual = dota2SenateProblem.PredictPartyVictory(senate);
+
+        // Assert
+        actual.Should().Be("Dire");
+    }
+
+    [Fact]
+    public void PredictPartyVictory_TestCase_3()
+    {
+        // Arrange
+        string senate = "RDDRDRD";
+
+        Dota2SenateProblem dota2SenateProblem = new Dota2SenateProblem();
+
+        // Act
+        string actual = dota2SenateProblem.PredictPartyVictory(senate);
+
+        // Assert
+        actual.Should().Be("Dire");
+    }
+
+    [Fact]
+    public void PredictPartyVictory_TestCase_4()
+    {
+        // Arrange
+        string senate = "DRDRDRDDR";
+
+        Dota2SenateProblem dota2SenateProblem = new Dota2SenateProblem();
+
+        // Act
+        string actual = dota2SenateProblem.PredictPartyVictory(senate);
+
+        // Assert
+        actual.Should().Be("Dire");
+    }
+}
