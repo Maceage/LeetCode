21a12cd186536b6e8f3091039ecf643f9831b2d3
Graham Mace <graham@maceage.com>
2024-09-30 18:36:12 +1000

    Add ReverseLinkedListProblem and tests

---
+++ LeetCode.Problems/ReverseLinkedListProblem.cs
@@ -0,0 +1,76 @@
+﻿using LeetCode.Problems.Data;
+
+namespace LeetCode.Problems;
+
+public class ReverseLinkedListProblem
+{
+    public ListNode ReverseList(ListNode head)
+    {
+        if ((head == null) || head.Next == null)
+        {
+            return head;
+        }
+
+        ListNode result = ReverseList(head.Next);
+
+        head.Next.Next = head;
+        head.Next = null;
+
+        return result;
+    }
+
+    //public ListNode ReverseList(ListNode head)
+    //{
+    //    ListNode previousNode = null;
+    //    ListNode currentNode = head;
+
+    //    while (currentNode != null)
+    //    {
+    //        ListNode nextNode = currentNode.Next;
+
+    //        currentNode.Next = previousNode;
+
+    //        previousNode = currentNode;
+
+    //        currentNode = nextNode;
+    //    }
+
+    //    return previousNode;
+    //}
+
+    //public ListNode ReverseList(ListNode head)
+    //{
+    //    if ((head == null) || (head.Next == null))
+    //    {
+    //        return head;
+    //    }
+
+    //    ListNode currentNode = head;
+
+    //    Stack<ListNode> listNodeStack = new Stack<ListNode>();
+
+    //    while (currentNode != null)
+    //    {
+    //        listNodeStack.Push(currentNode);
+
+    //        currentNode = currentNode.Next;
+    //    }
+
+    //    ListNode newHead = listNodeStack.Pop();
+
+    //    newHead.Next = null;
+
+    //    currentNode = newHead;
+
+    //    while (listNodeStack.TryPop(out var nextNode))
+    //    {
+    //        nextNode.Next = null;
+
+    //        currentNode.Next = nextNode;
+
+    //        currentNode = nextNode;
+    //    }
+
+    //    return newHead;
+    //}
+}
---
+++ LeetCode.Tests/ReverseLinkedListProblemTests.cs
@@ -0,0 +1,38 @@
+﻿using LeetCode.Problems.Data;
+using LeetCode.Problems;
+using LeetCode.Tests.Utilities;
+
+namespace LeetCode.Tests;
+
+public class ReverseLinkedListProblemTests
+{
+    [Fact]
+    public void ReverseList_TestCase_1()
+    {
+        // Arrange
+        ListNode headNode = ListNodeUtilities.CreateLinkedList([1, 2, 3, 4, 5]);
+
+        ReverseLinkedListProblem reverseLinkedListProblem = new ReverseLinkedListProblem();
+
+        // Act
+        var actual = reverseLinkedListProblem.ReverseList(headNode);
+
+        // Assert
+        ListNodeUtilities.AssertLinkedList(actual, [5, 4, 3, 2, 1]);
+    }
+
+    [Fact]
+    public void ReverseList_TestCase_2()
+    {
+        // Arrange
+        ListNode headNode = ListNodeUtilities.CreateLinkedList([1, 2]);
+
+        ReverseLinkedListProblem reverseLinkedListProblem = new ReverseLinkedListProblem();
+
+        // Act
+        var actual = reverseLinkedListProblem.ReverseList(headNode);
+
+        // Assert
+        ListNodeUtilities.AssertLinkedList(actual, [2, 1]);
+    }
+}
