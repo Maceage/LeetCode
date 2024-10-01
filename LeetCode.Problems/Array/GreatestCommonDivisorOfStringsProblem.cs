911b98736046e69072ab322548699994cc0745d3
Graham Mace <graham@maceage.com>
2024-10-01 16:49:16 +1000

    Add GreatestCommonDivisorOfStringsProblem and tests

---
+++ LeetCode.Problems/Array/GreatestCommonDivisorOfStringsProblem.cs
@@ -0,0 +1,43 @@
+﻿namespace LeetCode.Problems.Array;
+
+public class GreatestCommonDivisorOfStringsProblem
+{
+    public string GcdOfStrings(string string1, string string2)
+    {
+        string gcd = string.Empty;
+
+        int length1 = string1.Length;
+        int length2 = string2.Length;
+
+        for (int i = Math.Min(length1, length2); i >= 1; --i)
+        {
+            if (IsValid(string1, string2, i))
+            {
+                gcd = string1.Substring(0, i);
+                break;
+            }
+        }
+
+        return gcd;
+    }
+
+    private static bool IsValid(string string1, string string2, int k)
+    {
+        bool isValid = false;
+
+        int length1 = string1.Length;
+        int length2 = string2.Length;
+
+        if ((length1 % k <= 0) && (length2 % k <= 0))
+        {
+            string baseString = string1.Substring(0, k);
+
+            bool isString1Empty = string.IsNullOrEmpty(string1.Replace(baseString, string.Empty));
+            bool isString2Empty = string.IsNullOrEmpty(string2.Replace(baseString, string.Empty));
+
+            isValid = (isString1Empty && isString2Empty);
+        }
+
+        return isValid;
+    }
+}
---
+++ LeetCode.Tests/Array/GreatestCommonDivisorOfStringsProblemTests.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.Array;
+
+namespace LeetCode.Tests.Array;
+
+public class GreatestCommonDivisorOfStringsProblemTests
+{
+    [Fact]
+    public void GcdOfStrings_TestCase_1()
+    {
+        // Arrange
+        string string1 = "ABCABC";
+        string string2 = "ABC";
+
+        GreatestCommonDivisorOfStringsProblem greatestCommonDivisorOfStringsProblem = new GreatestCommonDivisorOfStringsProblem();
+
+        // Act
+        string actual = greatestCommonDivisorOfStringsProblem.GcdOfStrings(string1, string2);
+
+        // Assert
+        actual.Should().Be("ABC");
+    }
+
+    [Fact]
+    public void GcdOfStrings_TestCase_2()
+    {
+        // Arrange
+        string string1 = "ABABAB";
+        string string2 = "ABAB";
+
+        GreatestCommonDivisorOfStringsProblem greatestCommonDivisorOfStringsProblem = new GreatestCommonDivisorOfStringsProblem();
+
+        // Act
+        string actual = greatestCommonDivisorOfStringsProblem.GcdOfStrings(string1, string2);
+
+        // Assert
+        actual.Should().Be("AB");
+    }
+
+    [Fact]
+    public void GcdOfStrings_TestCase_3()
+    {
+        // Arrange
+        string string1 = "LEET";
+        string string2 = "CODE";
+
+        GreatestCommonDivisorOfStringsProblem greatestCommonDivisorOfStringsProblem = new GreatestCommonDivisorOfStringsProblem();
+
+        // Act
+        string actual = greatestCommonDivisorOfStringsProblem.GcdOfStrings(string1, string2);
+
+        // Assert
+        actual.Should().Be(string.Empty);
+    }
+}
