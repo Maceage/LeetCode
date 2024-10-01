c4340b4b096fa1cbbd758e4cbdd47be7aad349fa
Graham Mace <graham@maceage.com>
2024-10-01 17:05:42 +1000

    Add DecodeStringProblem and tests

---
+++ LeetCode.Problems/Stack/DecodeStringProblem.cs
@@ -0,0 +1,62 @@
+﻿namespace LeetCode.Problems.Stack;
+
+public class DecodeStringProblem
+{
+    public string DecodeString(string s)
+    {
+        Stack<char> charStack = new Stack<char>();
+
+        foreach (var c in s)
+        {
+            if (c == ']')
+            {
+                List<char> decodedString = new List<char>();
+
+                while (charStack.Peek() != '[')
+                {
+                    char currentChar = charStack.Pop();
+
+                    decodedString.Add(currentChar);
+                }
+
+                // Pop [ from the stack
+                charStack.Pop();
+
+                int baseTen = 1;
+                int k = 0;
+
+                // Get the number k
+                while ((charStack.Count > 0) && (char.IsDigit(charStack.Peek())))
+                {
+                    k += (charStack.Pop() - '0') * baseTen;
+                    baseTen *= 10;
+                }
+
+                // Decode k[decodedString] by pushing decodedString k times into stack
+                while (k != 0)
+                {
+                    for (int j = decodedString.Count - 1; j >= 0; j--)
+                    {
+                        charStack.Push(decodedString[j]);
+                    }
+
+                    k--;
+                }
+            }
+            else
+            {
+                charStack.Push(c);
+            }
+        }
+
+        // Get result from the stack
+        char[] resultChars = new char[charStack.Count];
+
+        for (int i = charStack.Count - 1; i >= 0; i--)
+        {
+            resultChars[i] = charStack.Pop();
+        }
+
+        return new string(resultChars);
+    }
+}
---
+++ LeetCode.Tests/Stack/DecodeStringProblemTests.cs
@@ -0,0 +1,67 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.Stack;
+
+namespace LeetCode.Tests.Stack;
+
+public class DecodeStringProblemTests
+{
+    [Fact]
+    public void DecodeString_TestCase_1()
+    {
+        // Arrange
+        string s = "3[a]2[bc]";
+
+        DecodeStringProblem decodeStringProblem = new DecodeStringProblem();
+
+        // Act
+        string actual = decodeStringProblem.DecodeString(s);
+
+        // Assert
+        actual.Should().Be("aaabcbc");
+    }
+
+    [Fact]
+    public void DecodeString_TestCase_2()
+    {
+        // Arrange
+        string s = "3[a2[c]]";
+
+        DecodeStringProblem decodeStringProblem = new DecodeStringProblem();
+
+        // Act
+        string actual = decodeStringProblem.DecodeString(s);
+
+        // Assert
+        actual.Should().Be("accaccacc");
+    }
+
+    [Fact]
+    public void DecodeString_TestCase_3()
+    {
+        // Arrange
+        string s = "2[abc]3[cd]ef";
+
+        DecodeStringProblem decodeStringProblem = new DecodeStringProblem();
+
+        // Act
+        string actual = decodeStringProblem.DecodeString(s);
+
+        // Assert
+        actual.Should().Be("abcabccdcdcdef");
+    }
+
+    [Fact]
+    public void DecodeString_TestCase_4()
+    {
+        // Arrange
+        string s = "12[abc]12[cd]ef";
+
+        DecodeStringProblem decodeStringProblem = new DecodeStringProblem();
+
+        // Act
+        string actual = decodeStringProblem.DecodeString(s);
+
+        // Assert
+        actual.Should().Be("abcabcabcabcabcabcabcabcabcabcabcabccdcdcdcdcdcdcdcdcdcdcdcdef");
+    }
+}
