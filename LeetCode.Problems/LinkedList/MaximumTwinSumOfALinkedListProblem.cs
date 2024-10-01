b8ba05f4728814e929e620d284a645bc566b7bd1
Graham Mace <graham@maceage.com>
2024-10-01 15:49:58 +1000

    Add MaximumTwinSumOfALinkedListProblem and tests

---
+++ LeetCode.Problems/LinkedList/MaximumTwinSumOfALinkedListProblem.cs
@@ -0,0 +1,75 @@
+﻿using LeetCode.Problems.Data;
+
+namespace LeetCode.Problems.LinkedList;
+
+public class MaximumTwinSumOfALinkedListProblem
+{
+    public int PairSum(ListNode head)
+    {
+        // Find the midpoint of the linked list
+        ListNode midPointNode = FindMidPointNode(head);
+
+        // Split the latter half from the linked list
+        ListNode secondHalfHead = midPointNode.Next;
+        midPointNode.Next = null;
+
+        // Reverse latter half of linked list
+        secondHalfHead = ReverseList(secondHalfHead);
+
+        // Iterate through each linked list half and find the highest twin sum
+        ListNode leftNode = head;
+        ListNode rightNode = secondHalfHead;
+
+        int highestSum = 0;
+
+        do
+        {
+            int leftValue = leftNode.Value;
+            int rightValue = rightNode.Value;
+
+            int currentSum = leftValue + rightValue;
+
+            highestSum = Math.Max(highestSum, currentSum);
+
+            leftNode = leftNode.Next;
+            rightNode = rightNode.Next;
+        }
+        while ((leftNode != null) && (rightNode != null));
+
+        return highestSum;
+    }
+
+    private static ListNode FindMidPointNode(ListNode head)
+    {
+        if (head.Next == null)
+        {
+            return null;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.Next.Next;
+
+        while (fast != null && fast.Next != null)
+        {
+            slow = slow.Next;
+            fast = fast.Next.Next;
+        }
+
+        return slow;
+    }
+
+    private static ListNode ReverseList(ListNode head)
+    {
+        if ((head == null) || (head.Next == null))
+        {
+            return head;
+        }
+
+        ListNode result = ReverseList(head.Next);
+
+        head.Next.Next = head;
+        head.Next = null;
+
+        return result;
+    }
+}
---
+++ LeetCode.Tests/LinkedList/MaximumTwinSumOfALinkedListProblemTests.cs
@@ -0,0 +1,77 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.Data;
+using LeetCode.Tests.Extensions;
+using LeetCode.Problems.LinkedList;
+
+namespace LeetCode.Tests.LinkedList;
+
+public class MaximumTwinSumOfALinkedListProblemTests
+{
+    [Fact]
+    public void PairSum_TestCase_1()
+    {
+        // Arrange
+        int[] numberArray = [5, 4, 2, 1];
+
+        ListNode headNode = numberArray.CreateLinkedList();
+
+        MaximumTwinSumOfALinkedListProblem maximumTwinSumOfALinkedListProblem = new MaximumTwinSumOfALinkedListProblem();
+
+        // Act
+        var actual = maximumTwinSumOfALinkedListProblem.PairSum(headNode);
+
+        // Assert
+        actual.Should().Be(6);
+    }
+
+    [Fact]
+    public void PairSum_TestCase_2()
+    {
+        // Arrange
+        int[] numberArray = [4, 2, 2, 3];
+
+        ListNode headNode = numberArray.CreateLinkedList();
+
+        MaximumTwinSumOfALinkedListProblem maximumTwinSumOfALinkedListProblem = new MaximumTwinSumOfALinkedListProblem();
+
+        // Act
+        var actual = maximumTwinSumOfALinkedListProblem.PairSum(headNode);
+
+        // Assert
+        actual.Should().Be(7);
+    }
+
+    [Fact]
+    public void PairSum_TestCase_3()
+    {
+        // Arrange
+        int[] numberArray = [1, 10000];
+
+        ListNode headNode = numberArray.CreateLinkedList();
+
+        MaximumTwinSumOfALinkedListProblem maximumTwinSumOfALinkedListProblem = new MaximumTwinSumOfALinkedListProblem();
+
+        // Act
+        var actual = maximumTwinSumOfALinkedListProblem.PairSum(headNode);
+
+        // Assert
+        actual.Should().Be(10001);
+    }
+
+    [Fact]
+    public void PairSum_TestCase_4()
+    {
+        // Arrange
+        int[] numberArray = [5, 4, 2, 1, 1, 2, 4, 5];
+
+        ListNode headNode = numberArray.CreateLinkedList();
+
+        MaximumTwinSumOfALinkedListProblem maximumTwinSumOfALinkedListProblem = new MaximumTwinSumOfALinkedListProblem();
+
+        // Act
+        var actual = maximumTwinSumOfALinkedListProblem.PairSum(headNode);
+
+        // Assert
+        actual.Should().Be(10);
+    }
+}
