0e12baf44883bbcc1e7ff28b899cd79832c8738f
Graham Mace <graham@maceage.com>
2024-10-01 18:07:26 +1000

    Update GreatestCommonDivisorOfStringsProblem solution

--- LeetCode.Problems/Array/GreatestCommonDivisorOfStringsProblem.cs
+++ LeetCode.Problems/Array/GreatestCommonDivisorOfStringsProblem.cs
@@ -4,40 +4,64 @@ public class GreatestCommonDivisorOfStringsProblem
 {
     public string GcdOfStrings(string string1, string string2)
     {
-        string gcd = string.Empty;
-
-        int length1 = string1.Length;
-        int length2 = string2.Length;
-
-        for (int i = Math.Min(length1, length2); i >= 1; --i)
+        if (!(string1 + string2).Equals(string2 + string1))
         {
-            if (IsValid(string1, string2, i))
-            {
-                gcd = string1.Substring(0, i);
-                break;
-            }
+            return string.Empty;
         }
 
-        return gcd;
+        int gcdLength = GcdLength(string1.Length, string2.Length);
+
+        return string1.Substring(0, gcdLength);
     }
 
-    private static bool IsValid(string string1, string string2, int k)
+    private static int GcdLength(int x, int y)
     {
-        bool isValid = false;
-
-        int length1 = string1.Length;
-        int length2 = string2.Length;
-
-        if ((length1 % k <= 0) && (length2 % k <= 0))
+        if (y == 0)
         {
-            string baseString = string1.Substring(0, k);
-
-            bool isString1Empty = string.IsNullOrEmpty(string1.Replace(baseString, string.Empty));
-            bool isString2Empty = string.IsNullOrEmpty(string2.Replace(baseString, string.Empty));
-
-            isValid = (isString1Empty && isString2Empty);
+            return x;
         }
-
-        return isValid;
+        else
+        {
+            return GcdLength(y, x % y);
+        }
     }
+
+    //public string GcdOfStrings(string string1, string string2)
+    //{
+    //    string gcd = string.Empty;
+
+    //    int length1 = string1.Length;
+    //    int length2 = string2.Length;
+
+    //    for (int i = Math.Min(length1, length2); i >= 1; --i)
+    //    {
+    //        if (IsValid(string1, string2, i))
+    //        {
+    //            gcd = string1.Substring(0, i);
+    //            break;
+    //        }
+    //    }
+
+    //    return gcd;
+    //}
+
+    //private static bool IsValid(string string1, string string2, int k)
+    //{
+    //    bool isValid = false;
+
+    //    int length1 = string1.Length;
+    //    int length2 = string2.Length;
+
+    //    if ((length1 % k <= 0) && (length2 % k <= 0))
+    //    {
+    //        string baseString = string1.Substring(0, k);
+
+    //        bool isString1Empty = string.IsNullOrEmpty(string1.Replace(baseString, string.Empty));
+    //        bool isString2Empty = string.IsNullOrEmpty(string2.Replace(baseString, string.Empty));
+
+    //        isValid = (isString1Empty && isString2Empty);
+    //    }
+
+    //    return isValid;
+    //}
 }
