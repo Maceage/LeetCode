2060a779028e4cc8759e8da7b4a00017de1c968d
Graham Mace <graham@maceage.com>
2024-10-02 15:36:41 +1000

    Add StringCompressionProblem and tests

---
+++ LeetCode.Problems/TwoPointers/StringCompressionProblem.cs
@@ -0,0 +1,64 @@
+﻿namespace LeetCode.Problems.TwoPointers;
+
+public class StringCompressionProblem
+{
+    public int Compress(ref char[] chars)
+    {
+        var i = 0;
+        int result = 0;
+
+        while (i < chars.Length)
+        {
+            int groupLength = 1;
+
+            while (i + groupLength < chars.Length && chars[i + groupLength] == chars[i])
+            {
+                groupLength++;
+            }
+
+            chars[result++] = chars[i];
+
+            if (groupLength > 1)
+            {
+                foreach (char c in groupLength.ToString())
+                {
+                    chars[result++] = c;
+                }
+            }
+
+            i += groupLength;
+        }
+
+        return result;
+    }
+
+    //public int Compress(ref char[] chars)
+    //{
+    //    var i = 0;
+    //    var j = 0;
+
+    //    while (i < chars.Length)
+    //    {
+    //        var current = chars[i];
+    //        var counter = 0;
+
+    //        while (i < chars.Length && chars[i] == current)
+    //        {
+    //            i++;
+    //            counter++;
+    //        }
+
+    //        chars[j++] = current;
+
+    //        if (counter > 1)
+    //        {
+    //            foreach (var counterChar in counter.ToString())
+    //            {
+    //                chars[j++] = counterChar;
+    //            }
+    //        }
+    //    }
+
+    //    return j;
+    //}
+}
---
+++ LeetCode.Tests/TwoPointers/StringCompressionProblemTests.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.TwoPointers;
+
+namespace LeetCode.Tests.TwoPointers;
+
+public class StringCompressionProblemTests
+{
+    [Fact]
+    public void Compress_TestCase_1()
+    {
+        // Arrange
+        char[] chars = ['a', 'a', 'b', 'b', 'c', 'c', 'c'];
+        int expected = 6;
+
+        StringCompressionProblem stringCompressionProblem = new StringCompressionProblem();
+
+        // Act
+        int actual = stringCompressionProblem.Compress(ref chars);
+
+        // Assert
+        actual.Should().Be(expected);
+
+        //chars.Should().BeEquivalentTo(['a', '2', 'b', '2', 'c', '3'], options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public void Compress_TestCase_2()
+    {
+        // Arrange
+        char[] chars = ['a'];
+        int expected = 1;
+
+        StringCompressionProblem stringCompressionProblem = new StringCompressionProblem();
+
+        // Act
+        int actual = stringCompressionProblem.Compress(ref chars);
+
+        // Assert
+        actual.Should().Be(expected);
+
+        //chars.Should().BeEquivalentTo(['a']);
+    }
+
+    [Fact]
+    public void Compress_TestCase_3()
+    {
+        // Arrange
+        char[] chars = ['a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'];
+        int expected = 4;
+
+        StringCompressionProblem stringCompressionProblem = new StringCompressionProblem();
+
+        // Act
+        int actual = stringCompressionProblem.Compress(ref chars);
+
+        // Assert
+        actual.Should().Be(expected);
+
+        //chars.Should().BeEquivalentTo(['a', 'b', '1', '2'], options => options.WithStrictOrdering());
+    }
+}
