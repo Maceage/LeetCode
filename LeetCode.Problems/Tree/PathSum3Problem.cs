b6bf6c8ca939a2292ee9be2830ed40a7abc36dda
Graham Mace <graham@maceage.com>
2024-10-03 16:30:19 +1000

    Add PathSum3Problem and tests

---
+++ LeetCode.Problems/Tree/PathSum3Problem.cs
@@ -0,0 +1,58 @@
+﻿using LeetCode.Problems.Data;
+
+namespace LeetCode.Problems.Tree;
+
+public class PathSum3Problem
+{
+    private int _count;
+    private int _k;
+
+    private readonly Dictionary<long, int> _sumCounts = new();
+
+    public int PathSum(TreeNode rootNode, int targetSum)
+    {
+        _k = targetSum;
+
+        Preorder(rootNode, 0);
+
+        return _count;
+    }
+
+    private void Preorder(TreeNode treeNode, long currentSum)
+    {
+        if (treeNode == null)
+        {
+            return;
+        }
+
+        currentSum += treeNode.Value;
+
+        if (currentSum == _k)
+        {
+            _count++;
+        }
+
+        _count += _sumCounts.GetValueOrDefault(currentSum - _k, 0);
+
+        if (_sumCounts.TryGetValue(currentSum, out var value))
+        {
+            _sumCounts[currentSum] = value + 1;
+        }
+        else
+        {
+            _sumCounts.Add(currentSum, _sumCounts.GetValueOrDefault(currentSum) + 1);
+        }
+
+        Preorder(treeNode.Left, currentSum);
+        Preorder(treeNode.Right, currentSum);
+
+        if (_sumCounts.TryGetValue(currentSum, out value))
+        {
+            _sumCounts[currentSum] = value - 1;
+        }
+        else
+        {
+            _sumCounts.Add(currentSum, _sumCounts.GetValueOrDefault(currentSum) - 1);
+        }
+    }
+}
---
+++ LeetCode.Tests/Tree/PathSum3ProblemTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.Data;
+using LeetCode.Problems.Tree;
+using LeetCode.Tests.Extensions;
+
+namespace LeetCode.Tests.Tree;
+
+public class PathSum3ProblemTests
+{
+    [Fact]
+    public void PathSum_TestCase_1()
+    {
+        // Arrange
+        int?[] numberArray = [10, 5, -3, 3, 2, null, 11, 3, -2, null, 1];
+
+        TreeNode rootNode = numberArray.CreateTree();
+
+        int targetSum = 8;
+
+        PathSum3Problem pathSum3Problem = new PathSum3Problem();
+
+        // Act
+        int actual = pathSum3Problem.PathSum(rootNode, targetSum);
+
+        // Assert
+        actual.Should().Be(3);
+    }
+
+    [Fact]
+    public void PathSum_TestCase_2()
+    {
+        // Arrange
+        int?[] numberArray = [5, 4, 8, 11, null, 13, 4, 7, 2, null, null, 5, 1];
+
+        TreeNode rootNode = numberArray.CreateTree();
+
+        int targetSum = 22;
+
+        PathSum3Problem pathSum3Problem = new PathSum3Problem();
+
+        // Act
+        int actual = pathSum3Problem.PathSum(rootNode, targetSum);
+
+        // Assert
+        actual.Should().Be(3);
+    }
+}
