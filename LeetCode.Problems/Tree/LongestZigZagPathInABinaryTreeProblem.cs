8b5c02f23120ed9e2dd9115f313fe62c782235a8
Graham Mace <graham@maceage.com>
2024-10-03 17:46:03 +1000

    Add LongestZigZagPathInABinaryTreeProblem and tests

---
+++ LeetCode.Problems/Tree/LongestZigZagPathInABinaryTreeProblem.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode.Problems.Data;
+
+namespace LeetCode.Problems.Tree;
+
+public class LongestZigZagPathInABinaryTreeProblem
+{
+    private int _pathLength;
+
+    public int LongestZigZag(TreeNode rootNode)
+    {
+        LongestZigZagInternal(rootNode, 0, 0);
+
+        return _pathLength;
+    }
+
+    private void LongestZigZagInternal(TreeNode treeNode, int leftDepth, int rightDepth)
+    {
+        if (treeNode != null)
+        {
+            _pathLength = Math.Max(_pathLength, Math.Max(leftDepth, rightDepth));
+
+            if (treeNode.Left != null)
+            {
+                LongestZigZagInternal(treeNode.Left, rightDepth + 1, 0);
+            }
+
+            if (treeNode.Right != null)
+            {
+                LongestZigZagInternal(treeNode.Right, 0, leftDepth + 1);
+            }
+        }
+    }
+}
---
+++ LeetCode.Tests/Tree/LongestZigZagPathInABinaryTreeProblemTests.cs
@@ -0,0 +1,60 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.Data;
+using LeetCode.Problems.Tree;
+using LeetCode.Tests.Extensions;
+
+namespace LeetCode.Tests.Tree;
+
+public class LongestZigZagPathInABinaryTreeProblemTests
+{
+    [Fact]
+    public void LongestZigZag_TestCase_1()
+    {
+        // Arrange
+        int?[] numberArray = [1, null, 1, 1, 1, null, null, 1, 1, null, 1, null, null, null, 1];
+
+        TreeNode rootNode = numberArray.CreateTree();
+
+        LongestZigZagPathInABinaryTreeProblem longestZigZagPathInABinaryTreeProblem = new LongestZigZagPathInABinaryTreeProblem();
+
+        // Act
+        int actual = longestZigZagPathInABinaryTreeProblem.LongestZigZag(rootNode);
+
+        // Assert
+        actual.Should().Be(3);
+    }
+
+    [Fact]
+    public void LongestZigZag_TestCase_2()
+    {
+        // Arrange
+        int?[] numberArray = [1, 1, 1, null, 1, null, null, 1, 1, null, 1];
+
+        TreeNode rootNode = numberArray.CreateTree();
+
+        LongestZigZagPathInABinaryTreeProblem longestZigZagPathInABinaryTreeProblem = new LongestZigZagPathInABinaryTreeProblem();
+
+        // Act
+        int actual = longestZigZagPathInABinaryTreeProblem.LongestZigZag(rootNode);
+
+        // Assert
+        actual.Should().Be(4);
+    }
+
+    [Fact]
+    public void LongestZigZag_TestCase_3()
+    {
+        // Arrange
+        int?[] numberArray = [1];
+
+        TreeNode rootNode = numberArray.CreateTree();
+
+        LongestZigZagPathInABinaryTreeProblem longestZigZagPathInABinaryTreeProblem = new LongestZigZagPathInABinaryTreeProblem();
+
+        // Act
+        int actual = longestZigZagPathInABinaryTreeProblem.LongestZigZag(rootNode);
+
+        // Assert
+        actual.Should().Be(0);
+    }
+}
