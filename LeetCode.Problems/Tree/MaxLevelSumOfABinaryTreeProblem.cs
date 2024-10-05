1bf7098958196eb457f9d27ddea2138f1961f81f
Graham Mace <graham@maceage.com>
2024-10-05 19:26:36 +1000

    Add MaxLevelSumOfABinaryTreeProblem and tests

---
+++ LeetCode.Problems/Tree/MaxLevelSumOfABinaryTreeProblem.cs
@@ -0,0 +1,55 @@
+﻿using LeetCode.Problems.Data;
+
+namespace LeetCode.Problems.Tree;
+
+public class MaxLevelSumOfABinaryTreeProblem
+{
+    public int MaxLevelSum(TreeNode root)
+    {
+        int maxLevel = 0;
+
+        if (root != null)
+        {
+            Queue<TreeNode> nodeQueue = new Queue<TreeNode>();
+
+            nodeQueue.Enqueue(root);
+
+            int maxSum = int.MinValue;
+            int currentLevel = 1;
+
+            while (nodeQueue.Count> 0)
+            {
+                int size = nodeQueue.Count;
+
+                int sum = 0;
+
+                for (int i = 0; i < size; i++)
+                {
+                    var currentNode = nodeQueue.Dequeue();
+
+                    sum += currentNode.Value;
+
+                    if (currentNode.Left != null)
+                    {
+                        nodeQueue.Enqueue(currentNode.Left);
+                    }
+
+                    if (currentNode.Right != null)
+                    {
+                        nodeQueue.Enqueue(currentNode.Right);
+                    }
+                }
+
+                if (sum > maxSum)
+                {
+                    maxSum = sum;
+                    maxLevel = currentLevel;
+                }
+
+                currentLevel++;
+            }
+        }
+
+        return maxLevel;
+    }
+}
---
+++ LeetCode.Tests/Tree/MaxLevelSumOfABinaryTreeProblemTests.cs
@@ -0,0 +1,60 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.Data;
+using LeetCode.Problems.Tree;
+using LeetCode.Tests.Extensions;
+
+namespace LeetCode.Tests.Tree;
+
+public class MaxLevelSumOfABinaryTreeProblemTests
+{
+    [Fact]
+    public void MaxLevelSum_TestCase_1()
+    {
+        // Arrange
+        int?[] numberArray = [1, 7, 0, 7, -8, null, null];
+
+        TreeNode rootNode = numberArray.CreateTree();
+
+        MaxLevelSumOfABinaryTreeProblem maxLevelSumOfABinaryTreeProblem = new MaxLevelSumOfABinaryTreeProblem();
+
+        // Act
+        int actual = maxLevelSumOfABinaryTreeProblem.MaxLevelSum(rootNode);
+
+        // Assert
+        actual.Should().Be(2);
+    }
+
+    [Fact]
+    public void MaxLevelSum_TestCase_2()
+    {
+        // Arrange
+        int?[] numberArray = [989, null, 10250, 98693, -89388, null, null, null, -32127];
+
+        TreeNode rootNode = numberArray.CreateTree();
+
+        MaxLevelSumOfABinaryTreeProblem maxLevelSumOfABinaryTreeProblem = new MaxLevelSumOfABinaryTreeProblem();
+
+        // Act
+        int actual = maxLevelSumOfABinaryTreeProblem.MaxLevelSum(rootNode);
+
+        // Assert
+        actual.Should().Be(2);
+    }
+
+    [Fact]
+    public void MaxLevelSum_TestCase_3()
+    {
+        // Arrange
+        int?[] numberArray = [-100, -200, -300, -20, -5, -10, null];
+
+        TreeNode rootNode = numberArray.CreateTree();
+
+        MaxLevelSumOfABinaryTreeProblem maxLevelSumOfABinaryTreeProblem = new MaxLevelSumOfABinaryTreeProblem();
+
+        // Act
+        int actual = maxLevelSumOfABinaryTreeProblem.MaxLevelSum(rootNode);
+
+        // Assert
+        actual.Should().Be(3);
+    }
+}
