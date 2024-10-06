8d4d407b6723bff1e9a74535bb450b0c42ee11dd
Graham Mace <graham@maceage.com>
2024-10-06 17:29:46 +1100

    Add DeleteNodeInABstProblem and tests

---
+++ LeetCode.Problems/Tree/DeleteNodeInABstProblem.cs
@@ -0,0 +1,66 @@
+﻿using LeetCode.Problems.Data;
+
+namespace LeetCode.Problems.Tree;
+
+public class DeleteNodeInABstProblem
+{
+    public TreeNode DeleteNode(TreeNode root, int key)
+    {
+        if (root == null)
+        {
+            return null;
+        }
+
+        if (key > root.Value)
+        {
+            root.Right = DeleteNode(root, key);
+        }
+        else if (key < root.Value)
+        {
+            root.Left = DeleteNode(root.Left, key);
+        }
+        else
+        {
+            if ((root.Left == null) && (root.Right == null))
+            {
+                root = null;
+            }
+            else if (root.Right != null)
+            {
+                root.Value = Successor(root);
+                root.Right = DeleteNode(root.Right, root.Value);
+            }
+            else
+            {
+                root.Value = Predecessor(root);
+                root.Left = DeleteNode(root.Left, root.Value);
+            }
+        }
+
+        return root;
+    }
+
+    private static int Successor(TreeNode root)
+    {
+        root = root.Right;
+
+        while (root.Left != null)
+        {
+            root = root.Left;
+        }
+
+        return root.Value;
+    }
+
+    private static int Predecessor(TreeNode root)
+    {
+        root = root.Left;
+
+        while (root.Right != null)
+        {
+            root = root.Right;
+        }
+
+        return root.Value;
+    }
+}
---
+++ LeetCode.Tests/Tree/DeleteNodeInABstProblemTests.cs
@@ -0,0 +1,109 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.Data;
+using LeetCode.Problems.Tree;
+using LeetCode.Tests.Extensions;
+
+namespace LeetCode.Tests.Tree;
+
+public class DeleteNodeInABstProblemTests
+{
+    [Fact]
+    public void DeleteNode_TestCase_1()
+    {
+        // Arrange
+        int?[] numberArray = [5, 3, 6, 2, 4, null, 7];
+        int key = 3;
+
+        TreeNode rootNode = numberArray.CreateTree();
+
+        DeleteNodeInABstProblem deleteNodeInABstProblem = new DeleteNodeInABstProblem();
+
+        // Act
+        TreeNode actual = deleteNodeInABstProblem.DeleteNode(rootNode, key);
+
+        // Assert
+        int?[] result = actual.GetNodeValues();
+
+        result.Should().BeEquivalentTo([5, 4, 6, 2, (int?)null, null, 7], options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public void DeleteNode_TestCase_2()
+    {
+        // Arrange
+        int?[] numberArray = [5, 3, 6, 2, 4, null, 7];
+        int key = 0;
+
+        TreeNode rootNode = numberArray.CreateTree();
+
+        DeleteNodeInABstProblem deleteNodeInABstProblem = new DeleteNodeInABstProblem();
+
+        // Act
+        TreeNode actual = deleteNodeInABstProblem.DeleteNode(rootNode, key);
+
+        // Assert
+        int?[] result = actual.GetNodeValues();
+
+        result.Should().BeEquivalentTo([5, 3, 6, 2, 4, (int?)null, 7], options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public void DeleteNode_TestCase_3()
+    {
+        // Arrange
+        int?[] numberArray = [];
+        int key = 0;
+
+        TreeNode rootNode = numberArray.CreateTree();
+
+        DeleteNodeInABstProblem deleteNodeInABstProblem = new DeleteNodeInABstProblem();
+
+        // Act
+        TreeNode actual = deleteNodeInABstProblem.DeleteNode(rootNode, key);
+
+        // Assert
+        int?[] result = actual.GetNodeValues();
+
+        result.Should().BeEquivalentTo(new int?[] { }, options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public void DeleteNode_TestCase_4()
+    {
+        // Arrange
+        int?[] numberArray = [0];
+        int key = 0;
+
+        TreeNode rootNode = numberArray.CreateTree();
+
+        DeleteNodeInABstProblem deleteNodeInABstProblem = new DeleteNodeInABstProblem();
+
+        // Act
+        TreeNode actual = deleteNodeInABstProblem.DeleteNode(rootNode, key);
+
+        // Assert
+        int?[] result = actual.GetNodeValues();
+
+        result.Should().BeEquivalentTo(new int?[] { }, options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public void DeleteNode_TestCase_5()
+    {
+        // Arrange
+        int?[] numberArray = [5, 3, 6, 2, 4, null, 7];
+        int key = 5;
+
+        TreeNode rootNode = numberArray.CreateTree();
+
+        DeleteNodeInABstProblem deleteNodeInABstProblem = new DeleteNodeInABstProblem();
+
+        // Act
+        TreeNode actual = deleteNodeInABstProblem.DeleteNode(rootNode, key);
+
+        // Assert
+        int?[] result = actual.GetNodeValues();
+
+        result.Should().BeEquivalentTo([6, 3, 7, 2, 4], options => options.WithStrictOrdering());
+    }
+}
