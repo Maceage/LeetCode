6f30d9a17a9f0a732548134a9d3234bf7872b498
Graham Mace <graham@maceage.com>
2024-10-08 16:10:25 +1100

    Add ReorderRoutesToMakeAllPathsLeadToTheCityZeroProblem and tests

---
+++ LeetCode.Problems/Graph/ReorderRoutesToMakeAllPathsLeadToTheCityZeroProblem.cs
@@ -0,0 +1,97 @@
+﻿namespace LeetCode.Problems.Graph;
+
+public class ReorderRoutesToMakeAllPathsLeadToTheCityZeroProblem
+{
+    private int _count;
+
+    public int MinReorder(int n, int[][] connections)
+    {
+        Dictionary<int, List<List<int>>> adj = new Dictionary<int, List<List<int>>>();
+
+        foreach (int[] connection in connections)
+        {
+            int parent = connection[0];
+            int child = connection[1];
+
+            if (!adj.ContainsKey(parent))
+            {
+                List<List<int>> list = [[child, 1]];
+
+                adj.Add(parent, list);
+            }
+            else
+            {
+                adj[parent].Add([child, 1]);
+            }
+
+            if (!adj.ContainsKey(child))
+            {
+                List<List<int>> list = [[parent, 0]];
+
+                adj.Add(child, list);
+            }
+            else
+            {
+                adj[child].Add([parent, 0]);
+            }
+        }
+
+        ////DepthFirstSearch(0, -1, adj);
+        BreadthFirstSearch(0, n, adj);
+
+        return _count;
+    }
+
+    private void DepthFirstSearch(int node, int parent, Dictionary<int, List<List<int>>> adj)
+    {
+        if (adj.TryGetValue(node, out var value))
+        {
+            foreach (List<int> edges in value)
+            {
+                int neighbour = edges[0];
+                int sign = edges[1];
+
+                if (neighbour != parent)
+                {
+                    _count += sign;
+
+                    DepthFirstSearch(neighbour, node, adj);
+                }
+            }
+        }
+    }
+
+    private void BreadthFirstSearch(int node, int n, Dictionary<int, List<List<int>>> adj)
+    {
+        Queue<int> intQueue = new Queue<int>();
+
+        bool[] visit = new bool[n];
+
+        intQueue.Enqueue(node);
+
+        visit[node] = true;
+
+        while (intQueue.Count > 0)
+        {
+            node = intQueue.Dequeue();
+
+            if (adj.TryGetValue(node, out var value))
+            {
+                foreach (List<int> edges in value)
+                {
+                    int neighbour = edges[0];
+                    int sign = edges[1];
+
+                    if (!visit[neighbour])
+                    {
+                        _count += sign;
+
+                        visit[neighbour] = true;
+
+                        intQueue.Enqueue(neighbour);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ LeetCode.Tests/Graph/ReorderRoutesToMakeAllPathsLeadToTheCityZeroProblemTests.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.Graph;
+
+namespace LeetCode.Tests.Graph;
+
+public class ReorderRoutesToMakeAllPathsLeadToTheCityZeroProblemTests
+{
+    [Fact]
+    public void MinReorder_TestCase_1()
+    {
+        // Arrange
+        int n = 6;
+        int[][] connections = [[0, 1], [1, 3], [2, 3], [4, 0], [4, 5]];
+
+        int expected = 3;
+
+        ReorderRoutesToMakeAllPathsLeadToTheCityZeroProblem reorderRoutesToMakeAllPathsLeadToTheCityZeroProblem = new ReorderRoutesToMakeAllPathsLeadToTheCityZeroProblem();
+
+        // Act
+        int actual = reorderRoutesToMakeAllPathsLeadToTheCityZeroProblem.MinReorder(n, connections);
+
+        // Assert
+        actual.Should().Be(expected);
+    }
+
+    [Fact]
+    public void MinReorder_TestCase_2()
+    {
+        // Arrange
+        int n = 5;
+        int[][] connections = [[1, 0], [1, 2], [3, 2], [3, 4]];
+
+        int expected = 2;
+
+        ReorderRoutesToMakeAllPathsLeadToTheCityZeroProblem reorderRoutesToMakeAllPathsLeadToTheCityZeroProblem = new ReorderRoutesToMakeAllPathsLeadToTheCityZeroProblem();
+
+        // Act
+        int actual = reorderRoutesToMakeAllPathsLeadToTheCityZeroProblem.MinReorder(n, connections);
+
+        // Assert
+        actual.Should().Be(expected);
+    }
+
+    [Fact]
+    public void MinReorder_TestCase_3()
+    {
+        // Arrange
+        int n = 3;
+        int[][] connections = [[1, 0], [2, 0]];
+
+        int expected = 0;
+
+        ReorderRoutesToMakeAllPathsLeadToTheCityZeroProblem reorderRoutesToMakeAllPathsLeadToTheCityZeroProblem = new ReorderRoutesToMakeAllPathsLeadToTheCityZeroProblem();
+
+        // Act
+        int actual = reorderRoutesToMakeAllPathsLeadToTheCityZeroProblem.MinReorder(n, connections);
+
+        // Assert
+        actual.Should().Be(expected);
+    }
+}
