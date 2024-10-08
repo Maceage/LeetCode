40adb9cb7d298d8607e40640bf65a64484444320
Graham Mace <graham@maceage.com>
2024-10-08 14:13:24 +1100

    Add NumberOfProvincesProblem and tests

---
+++ LeetCode.Problems/Graph/NumberOfProvincesProblem.cs
@@ -0,0 +1,61 @@
+﻿namespace LeetCode.Problems.Graph;
+
+public class NumberOfProvincesProblem
+{
+    public int FindCircleNum(int[][] isConnected)
+    {
+        int n = isConnected.Length;
+        int numberOfComponents = 0;
+
+        bool[] visit = new bool[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            if (!visit[i])
+            {
+                numberOfComponents++;
+
+                //DepthFirstSearch(i, isConnected, visit);
+                BreadthFirstSearch(i, isConnected, visit);
+            }
+        }
+
+        return numberOfComponents;
+    }
+
+    private static void DepthFirstSearch(int node, int[][] isConnected, bool[] visit)
+    {
+        visit[node] = true;
+
+        for (int i = 0; i < isConnected.Length; i++)
+        {
+            if (isConnected[node][i] == 1 && !visit[i])
+            {
+                DepthFirstSearch(i, isConnected, visit);
+            }
+        }
+    }
+
+    private static void BreadthFirstSearch(int node, int[][] isConnected, bool[] visit)
+    {
+        Queue<int> queue = new Queue<int>();
+
+        queue.Enqueue(node);
+
+        visit[node] = true;
+
+        while (queue.Count > 0)
+        {
+            node = queue.Dequeue();
+
+            for (int i = 0; i < isConnected.Length; i++)
+            {
+                if (isConnected[node][i] == 1 && !visit[i])
+                {
+                    queue.Enqueue(i);
+                    visit[i] = true;
+                }
+            }
+        }
+    }
+}
---
+++ LeetCode.Tests/Graph/NumberOfProvincesProblemTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.Graph;
+
+namespace LeetCode.Tests.Graph;
+
+public class NumberOfProvincesProblemTests
+{
+    [Fact]
+    public void FindCircleNum_TestCase_1()
+    {
+        // Arrange
+        int[][] isConnected = [[1, 1, 0], [1, 1, 0], [0, 0, 1]];
+        int expected = 2;
+
+        NumberOfProvincesProblem numberOfProvincesProblem = new NumberOfProvincesProblem();
+
+        // Act
+        int actual = numberOfProvincesProblem.FindCircleNum(isConnected);
+
+        // Assert
+        actual.Should().Be(expected);
+    }
+
+    [Fact]
+    public void FindCircleNum_TestCase_2()
+    {
+        // Arrange
+        int[][] isConnected = [[1, 0, 0], [0, 1, 0], [0, 0, 1]];
+        int expected = 3;
+
+        NumberOfProvincesProblem numberOfProvincesProblem = new NumberOfProvincesProblem();
+
+        // Act
+        int actual = numberOfProvincesProblem.FindCircleNum(isConnected);
+
+        // Assert
+        actual.Should().Be(expected);
+    }
+}
