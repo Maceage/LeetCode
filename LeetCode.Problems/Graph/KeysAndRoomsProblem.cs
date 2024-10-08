952b1a763e58d62509a09fecd783842627a51f1b
Graham Mace <graham@maceage.com>
2024-10-08 13:10:04 +1100

    Add KeysAndRoomsProblem and tests

---
+++ LeetCode.Problems/Graph/KeysAndRoomsProblem.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode.Problems.Graph;
+
+public class KeysAndRoomsProblem
+{
+    public bool CanVisitAllRooms(IList<IList<int>> rooms)
+    {
+        bool canVisitAllRooms = true;
+        bool[] seenRooms = new bool[rooms.Count];
+
+        seenRooms[0] = true;
+
+        Stack<int> keyStack = new Stack<int>();
+
+        keyStack.Push(0);
+
+        while (keyStack.Count > 0)
+        {
+            int keyNode = keyStack.Pop();
+
+            foreach (int i in rooms[keyNode])
+            {
+                if (!seenRooms[i])
+                {
+                    seenRooms[i] = true;
+
+                    keyStack.Push(i);
+                }
+            }
+        }
+
+        foreach (bool room in seenRooms)
+        {
+            if (!room)
+            {
+                canVisitAllRooms = false;
+            }
+        }
+
+        return canVisitAllRooms;
+    }
+}
---
+++ LeetCode.Tests/Graph/KeysAndRoomsProblemTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.Graph;
+
+namespace LeetCode.Tests.Graph;
+
+public class KeysAndRoomsProblemTests
+{
+    [Fact]
+    public void CanVisitAllRooms_TestCase_1()
+    {
+        // Arrange
+        IList<IList<int>> rooms = [[1], [2], [3], []];
+
+        KeysAndRoomsProblem keysAndRoomsProblem = new KeysAndRoomsProblem();
+
+        // Act
+        bool actual = keysAndRoomsProblem.CanVisitAllRooms(rooms);
+
+        // Assert
+        actual.Should().BeTrue();
+    }
+
+    [Fact]
+    public void CanVisitAllRooms_TestCase_2()
+    {
+        // Arrange
+        IList<IList<int>> rooms = [[1, 3], [3, 0, 1], [2], [0]];
+
+        KeysAndRoomsProblem keysAndRoomsProblem = new KeysAndRoomsProblem();
+
+        // Act
+        bool actual = keysAndRoomsProblem.CanVisitAllRooms(rooms);
+
+        // Assert
+        actual.Should().BeFalse();
+    }
+}
