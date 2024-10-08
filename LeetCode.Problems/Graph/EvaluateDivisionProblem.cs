0a48dc6431639811fc761c3c05ba98799ecac2b2
Graham Mace <graham@maceage.com>
2024-10-08 17:02:06 +1100

    Add EvaluateDivisionProblem and tests

---
+++ LeetCode.Problems/Graph/EvaluateDivisionProblem.cs
@@ -0,0 +1,93 @@
+﻿namespace LeetCode.Problems.Graph;
+
+public class EvaluateDivisionProblem
+{
+    public double[] CalcEquation(IList<IList<string>> equations, double[] values, IList<IList<string>> queries)
+    {
+        Dictionary<string, Dictionary<string, double>> graph = new();
+
+        for (int i = 0; i < equations.Count; i++)
+        {
+            IList<string> equation = equations[i];
+
+            string dividend = equation[0];
+            string divisor = equation[1];
+
+            double quotient = values[i];
+
+            if (!graph.ContainsKey(dividend))
+            {
+                graph.Add(dividend, new Dictionary<string, double>());
+            }
+
+            if (!graph.ContainsKey(divisor))
+            {
+                graph.Add(divisor, new Dictionary<string, double>());
+            }
+
+            graph[dividend].Add(divisor, quotient);
+            graph[divisor].Add(dividend, 1 / quotient);
+        }
+
+        double[] results = new double[queries.Count];
+
+        for (int i = 0; i < queries.Count; i++)
+        {
+            IList<string> query = queries[i];
+
+            string dividend = query[0];
+            string divisor = query[1];
+
+            if (!graph.ContainsKey(dividend) || !graph.ContainsKey(divisor))
+            {
+                results[i] = -1.0;
+            }
+            else if (dividend == divisor)
+            {
+                results[i] = 1.0;
+            }
+            else
+            {
+                HashSet<string> visited = new HashSet<string>();
+                results[i] = BacktrackEvaluate(graph, dividend, divisor, 1, visited);
+            }
+        }
+
+        return results;
+    }
+
+    private static double BacktrackEvaluate(Dictionary<string, Dictionary<string, double>> graph, string currentNode, string targetNode, double accProduct, HashSet<string> visited)
+    {
+        visited.Add(currentNode);
+
+        double result = -1.0;
+
+        Dictionary<string, double> neighbours = graph[currentNode];
+
+        if (neighbours.TryGetValue(targetNode, out var neighbour))
+        {
+            result = accProduct * neighbour;
+        }
+        else
+        {
+            foreach (var (nextNode, value) in neighbours)
+            {
+                if (visited.Contains(nextNode))
+                {
+                    continue;
+                }
+
+                result = BacktrackEvaluate(graph, nextNode, targetNode, accProduct * value, visited);
+
+                if (result != -1.0)
+                {
+                    break;
+                }
+            }
+        }
+
+        visited.Remove(currentNode);
+
+        return result;
+    }
+}
---
+++ LeetCode.Tests/Graph/EvaluateDivisionProblemTests.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.Graph;
+
+namespace LeetCode.Tests.Graph;
+
+public class EvaluateDivisionProblemTests
+{
+    [Fact]
+    public void CalcEquation_TestCase_1()
+    {
+        // Arrange
+        IList<IList<string>> equations = [["a", "b"], ["b", "c"]];
+        double[] values = [2.0, 3.0];
+        IList<IList<string>> queries = [["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"]];
+
+        double[] expected = [6.00000, 0.50000, -1.00000, 1.00000, -1.00000];
+
+        EvaluateDivisionProblem evaluateDivisionProblem = new EvaluateDivisionProblem();
+
+        // Act
+        double[] actual = evaluateDivisionProblem.CalcEquation(equations, values, queries);
+
+        // Assert
+        actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public void CalcEquation_TestCase_2()
+    {
+        // Arrange
+        IList<IList<string>> equations = [["a", "b"], ["b", "c"], ["bc", "cd"]];
+        double[] values = [1.5, 2.5, 5.0];
+        IList<IList<string>> queries = [["a", "c"], ["c", "b"], ["bc", "cd"], ["cd", "bc"]];
+
+        double[] expected = [3.75000, 0.40000, 5.00000, 0.20000];
+
+        EvaluateDivisionProblem evaluateDivisionProblem = new EvaluateDivisionProblem();
+
+        // Act
+        double[] actual = evaluateDivisionProblem.CalcEquation(equations, values, queries);
+
+        // Assert
+        actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public void CalcEquation_TestCase_3()
+    {
+        // Arrange
+        IList<IList<string>> equations = [["a", "b"]];
+        double[] values = [0.5];
+        IList<IList<string>> queries = [["a", "b"], ["b", "a"], ["a", "c"], ["x", "y"]];
+
+        double[] expected = [0.50000, 2.00000, -1.00000, -1.00000];
+
+        EvaluateDivisionProblem evaluateDivisionProblem = new EvaluateDivisionProblem();
+
+        // Act
+        double[] actual = evaluateDivisionProblem.CalcEquation(equations, values, queries);
+
+        // Assert
+        actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+    }
+}
