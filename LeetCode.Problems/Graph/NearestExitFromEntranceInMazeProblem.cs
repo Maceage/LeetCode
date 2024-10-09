5f0ad9407f887abdbb5ac083d76b81c3bcd6b1f6
Graham Mace <graham@maceage.com>
2024-10-09 16:40:36 +1100

    Add NearestExitFromEntranceInMazeProblem and tests

---
+++ LeetCode.Problems/Graph/NearestExitFromEntranceInMazeProblem.cs
@@ -0,0 +1,57 @@
+﻿namespace LeetCode.Problems.Graph;
+
+public class NearestExitFromEntranceInMazeProblem
+{
+    public int NearestExit(char[][] maze, int[] entrance)
+    {
+        int rows = maze.Length;
+        int cols = maze[0].Length;
+
+        int[][] dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+
+        // Mark entrance as visited since it's not an exit
+        int startRow = entrance[0];
+        int startCol = entrance[1];
+
+        maze[startRow][startCol] = '+';
+
+        // Start BFS from entrance and use queue to store all cells to be visited
+        Queue<int[]> queue = new Queue<int[]>();
+
+        queue.Enqueue([startRow, startCol, 0]);
+
+        while (queue.Count > 0)
+        {
+            int[] currState = queue.Dequeue();
+
+            int currRow = currState[0];
+            int currCol = currState[1];
+            int currDistance = currState[2];
+
+            // Check four neighbour cells from current cell
+            foreach (int[] dir in dirs)
+            {
+                int nextRow = currRow + dir[0];
+                int nextCol = currCol + dir[1];
+
+                // If there's an unvisited empty neighbour...
+                if (0 <= nextRow && nextRow < rows && 0 <= nextCol && nextCol < cols && maze[nextRow][nextCol] == '.')
+                {
+                    // If this empty cell is an exit then return distance + 1
+                    if (nextRow == 0 || nextRow == rows - 1 || nextCol == 0 || nextCol == cols - 1)
+                    {
+                        return currDistance + 1;
+                    }
+
+                    // Otherwise, add cell to queue and mark as visited
+                    maze[nextRow][nextCol] = '+';
+
+                    queue.Enqueue([nextRow, nextCol, currDistance + 1]);
+                }
+            }
+        }
+
+        // Finished iterating without finding an exit
+        return -1;
+    }
+}
---
+++ LeetCode.Tests/Graph/NearestExitFromEntranceInMazeProblemTests.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.Graph;
+
+namespace LeetCode.Tests.Graph;
+
+public class NearestExitFromEntranceInMazeProblemTests
+{
+    [Fact]
+    public void NearestExit_TestCase_1()
+    {
+        // Arrange
+        char[][] maze = [['+', '+', '.', '+'], ['.', '.', '.', '+'], ['+', '+', '+', '.']];
+        int[] entrance = [1, 2];
+
+        NearestExitFromEntranceInMazeProblem nearestExitFromEntranceInMazeProblem = new NearestExitFromEntranceInMazeProblem();
+
+        // Act
+        int actual = nearestExitFromEntranceInMazeProblem.NearestExit(maze, entrance);
+
+        // Assert
+        actual.Should().Be(1);
+    }
+
+    [Fact]
+    public void NearestExit_TestCase_2()
+    {
+        // Arrange
+        char[][] maze = [['+', '+', '+'], ['.', '.', '.'], ['+', '+', '+']];
+        int[] entrance = [1, 0];
+
+        NearestExitFromEntranceInMazeProblem nearestExitFromEntranceInMazeProblem = new NearestExitFromEntranceInMazeProblem();
+
+        // Act
+        int actual = nearestExitFromEntranceInMazeProblem.NearestExit(maze, entrance);
+
+        // Assert
+        actual.Should().Be(2);
+    }
+
+    [Fact]
+    public void NearestExit_TestCase_3()
+    {
+        // Arrange
+        char[][] maze = [['.', '+']];
+        int[] entrance = [0, 0];
+
+        NearestExitFromEntranceInMazeProblem nearestExitFromEntranceInMazeProblem = new NearestExitFromEntranceInMazeProblem();
+
+        // Act
+        int actual = nearestExitFromEntranceInMazeProblem.NearestExit(maze, entrance);
+
+        // Assert
+        actual.Should().Be(-1);
+    }
+}
