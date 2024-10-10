46e4ff3130f9afdc267501748579fb26a65ef464
Graham Mace <graham@maceage.com>
2024-10-10 15:10:18 +1100

    Add SmallestNumberInInfiniteSetProblem and tests

---
+++ LeetCode.Problems/Heap/SmallestNumberInInfiniteSetProblem.cs
@@ -0,0 +1,61 @@
+﻿namespace LeetCode.Problems.Heap;
+
+public class SmallestNumberInInfiniteSetProblem
+{
+    public class SmallestInfiniteSet
+    {
+        private int _counter;
+        private readonly PriorityQueue<int, int> _priorityQueue;
+        private readonly HashSet<int> _numberSet;
+
+        public SmallestInfiniteSet()
+        {
+            _counter = 0;
+            _priorityQueue = new PriorityQueue<int, int>();
+            _numberSet = new HashSet<int>();
+        }
+
+        public int PopSmallest()
+        {
+            int smallest;
+            int nextNumber = _counter + 1;
+
+            if ((_priorityQueue.Count > 0) && (_priorityQueue.Peek() == nextNumber))
+            {
+                _priorityQueue.Dequeue();
+
+                smallest = nextNumber;
+
+                _counter = nextNumber;
+
+                _numberSet.Remove(smallest);
+            }
+            else if ((_priorityQueue.Count > 0) && (_priorityQueue.Peek() < nextNumber))
+            {
+                smallest = _priorityQueue.Dequeue();
+
+                _numberSet.Remove(smallest);
+            }
+            else
+            {
+                smallest = nextNumber;
+
+                _counter = nextNumber;
+            }
+
+            return smallest;
+        }
+
+        public void AddBack(int num)
+        {
+            bool hasNumber = _numberSet.Contains(num);
+
+            if (!hasNumber)
+            {
+                _priorityQueue.Enqueue(num, num);
+
+                _numberSet.Add(num);
+            }
+        }
+    }
+}
---
+++ LeetCode.Tests/Heap/SmallestNumberInInfiniteSetProblemTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.Heap;
+
+namespace LeetCode.Tests.Heap;
+
+public class SmallestNumberInInfiniteSetProblemTests
+{
+    [Fact]
+    public void SmallestInfiniteSet_TestCase_1()
+    {
+        // Arrange
+        SmallestNumberInInfiniteSetProblem.SmallestInfiniteSet smallestInfiniteSet = new SmallestNumberInInfiniteSetProblem.SmallestInfiniteSet();
+
+        // Act/Assert
+        smallestInfiniteSet.AddBack(2);
+
+        smallestInfiniteSet.PopSmallest().Should().Be(1);
+        smallestInfiniteSet.PopSmallest().Should().Be(2);
+        smallestInfiniteSet.PopSmallest().Should().Be(3);
+
+        smallestInfiniteSet.AddBack(1);
+
+        smallestInfiniteSet.PopSmallest().Should().Be(1);
+        smallestInfiniteSet.PopSmallest().Should().Be(4);
+        smallestInfiniteSet.PopSmallest().Should().Be(5);
+    }
+}
