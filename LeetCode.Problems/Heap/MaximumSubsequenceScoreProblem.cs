17865a398e2eae083d602a2ac5924fe409a92306
Graham Mace <graham@maceage.com>
2024-10-11 17:07:17 +1100

    Add MaximumSubsequenceScoreProblem and tests

---
+++ LeetCode.Problems/Heap/MaximumSubsequenceScoreProblem.cs
@@ -0,0 +1,53 @@
+﻿namespace LeetCode.Problems.Heap;
+
+public class MaximumSubsequenceScoreProblem
+{
+    public long MaxScore(int[] nums1, int[] nums2, int k)
+    {
+        // Sort pair (nums1[i], nums2[i]) in decreasing order
+        int n = nums1.Length;
+
+        int[][] pairs = new int[n][];
+
+        for (int i = 0; i < n; ++i)
+        {
+            pairs[i] = [nums1[i], nums2[i]];
+        }
+
+        System.Array.Sort(pairs, (a, b) => b[1] - a[1]);
+
+        // Use a min-heap to maintain the top k elements
+        PriorityQueue<int, int> topKHeap = new PriorityQueue<int, int>(k, Comparer<int>.Create((x, y) => x - y));
+
+        long topKSum = 0;
+
+        for (int i = 0; i < k; ++i)
+        {
+            int firstPairValue = pairs[i][0];
+
+            topKSum += firstPairValue;
+
+            topKHeap.Enqueue(firstPairValue, firstPairValue);
+        }
+
+        // The score of the first k pairs.
+        long answer = topKSum * pairs[k - 1][1];
+
+        // Iterate over ever nums2[i] as minimum from nums2
+        for (int i = k; i < n; ++i)
+        {
+            // Remove the smallest integer from the previous top k elements then add nums1[i] to the top k elements.
+            int firstPairValue = pairs[i][0];
+            int secondPairValue = pairs[i][1];
+
+            topKSum += firstPairValue - topKHeap.Dequeue();
+
+            topKHeap.Enqueue(firstPairValue, firstPairValue);
+
+            // Update answer as maximum score
+            answer = Math.Max(answer, topKSum * secondPairValue);
+        }
+
+        return answer;
+    }
+}
---
+++ LeetCode.Tests/Heap/MaximumSubsequenceScoreProblemTests.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.Heap;
+
+namespace LeetCode.Tests.Heap;
+
+public class MaximumSubsequenceScoreProblemTests
+{
+    [Fact]
+    public void MaxScore_TestCase_1()
+    {
+        // Arrange
+        int[] nums1 = [1, 3, 3, 2];
+        int[] nums2 = [2, 1, 3, 4];
+
+        int k = 3;
+
+        MaximumSubsequenceScoreProblem maximumSubsequenceScoreProblem = new MaximumSubsequenceScoreProblem();
+
+        // Act
+        long actual = maximumSubsequenceScoreProblem.MaxScore(nums1, nums2, k);
+
+        // Assert
+        actual.Should().Be(12);
+    }
+
+    [Fact]
+    public void MaxScore_TestCase_2()
+    {
+        // Arrange
+        int[] nums1 = [4, 2, 3, 1, 1];
+        int[] nums2 = [7, 5, 10, 9, 6];
+
+        int k = 1;
+
+        MaximumSubsequenceScoreProblem maximumSubsequenceScoreProblem = new MaximumSubsequenceScoreProblem();
+
+        // Act
+        long actual = maximumSubsequenceScoreProblem.MaxScore(nums1, nums2, k);
+
+        // Assert
+        actual.Should().Be(30);
+    }
+
+    [Fact]
+    public void MaxScore_TestCase_3()
+    {
+        // Arrange
+        int[] nums1 = [2, 1, 14, 12];
+        int[] nums2 = [11, 7, 13, 6];
+
+        int k = 3;
+
+        MaximumSubsequenceScoreProblem maximumSubsequenceScoreProblem = new MaximumSubsequenceScoreProblem();
+
+        // Act
+        long actual = maximumSubsequenceScoreProblem.MaxScore(nums1, nums2, k);
+
+        // Assert
+        actual.Should().Be(168);
+    }
+}
