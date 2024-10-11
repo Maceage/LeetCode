dc30a577b4be14805197d3b48c60f75d864571b1
Graham Mace <graham@maceage.com>
2024-10-11 18:48:18 +1100

    Add TotalCostToHireKWorkersProblem and tests

---
+++ LeetCode.Problems/Heap/TotalCostToHireKWorkersProblem.cs
@@ -0,0 +1,62 @@
+﻿namespace LeetCode.Problems.Heap;
+
+public class TotalCostToHireKWorkersProblem
+{
+    public long TotalCost(int[] costs, int k, int candidates)
+    {
+        long totalCost = 0;
+
+        PriorityQueue<int, int> leftHeap = new PriorityQueue<int, int>(candidates);
+
+        for (int i = 0; i < candidates; i++)
+        {
+            int cost = costs[i];
+
+            leftHeap.Enqueue(cost, cost);
+        }
+
+        PriorityQueue<int, int> rightHeap = new PriorityQueue<int, int>(candidates);
+
+        for (int i = Math.Max(candidates, costs.Length - candidates); i < costs.Length; i++)
+        {
+            int cost = costs[i];
+
+            rightHeap.Enqueue(cost, cost);
+        }
+
+        int leftNextIndex = candidates;
+        int rightNextIndex = costs.Length - candidates - 1;
+
+        for (int i = 0; i < k; i++)
+        {
+            if ((rightHeap.Count <= 0) || (leftHeap.Count > 0) && (leftHeap.Peek() <= rightHeap.Peek()))
+            {
+                totalCost += leftHeap.Dequeue();
+
+                if (leftNextIndex <= rightNextIndex)
+                {
+                    int cost = costs[leftNextIndex];
+
+                    leftHeap.Enqueue(cost, cost);
+
+                    leftNextIndex++;
+                }
+            }
+            else
+            {
+                totalCost += rightHeap.Dequeue();
+
+                if (leftNextIndex <= rightNextIndex)
+                {
+                    int cost = costs[rightNextIndex];
+
+                    rightHeap.Enqueue(cost, cost);
+
+                    rightNextIndex--;
+                }
+            }
+        }
+
+        return totalCost;
+    }
+}
---
+++ LeetCode.Tests/Heap/TotalCostToHireKWorkersProblemTests.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.Heap;
+
+namespace LeetCode.Tests.Heap;
+
+public class TotalCostToHireKWorkersProblemTests
+{
+    [Fact]
+    public void TotalCost_TestCase_1()
+    {
+        // Arrange
+        int[] costs = [17, 12, 10, 2, 7, 2, 11, 20, 8];
+        int k = 3;
+        int candidates = 4;
+
+        TotalCostToHireKWorkersProblem totalCostToHireKWorkersProblem = new TotalCostToHireKWorkersProblem();
+
+        // Act
+        long actual = totalCostToHireKWorkersProblem.TotalCost(costs, k, candidates);
+
+        // Assert
+        actual.Should().Be(11);
+    }
+
+    [Fact]
+    public void TotalCost_TestCase_2()
+    {
+        // Arrange
+        int[] costs = [1, 2, 4, 1];
+        int k = 3;
+        int candidates = 3;
+
+        TotalCostToHireKWorkersProblem totalCostToHireKWorkersProblem = new TotalCostToHireKWorkersProblem();
+
+        // Act
+        long actual = totalCostToHireKWorkersProblem.TotalCost(costs, k, candidates);
+
+        // Assert
+        actual.Should().Be(4);
+    }
+
+    [Fact]
+    public void TotalCost_TestCase_3()
+    {
+        // Arrange
+        int[] costs = [18, 64, 12, 21, 21, 78, 36, 58, 88, 58, 99, 26, 92, 91, 53, 10, 24, 25, 20, 92, 73, 63, 51, 65, 87, 6, 17, 32, 14, 42, 46, 65, 43, 9, 75];
+        int k = 13;
+        int candidates = 23;
+
+        TotalCostToHireKWorkersProblem totalCostToHireKWorkersProblem = new TotalCostToHireKWorkersProblem();
+
+        // Act
+        long actual = totalCostToHireKWorkersProblem.TotalCost(costs, k, candidates);
+
+        // Assert
+        actual.Should().Be(223);
+    }
+}
