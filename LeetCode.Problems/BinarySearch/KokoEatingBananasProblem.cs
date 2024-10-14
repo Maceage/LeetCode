b9e4461678373f42c68e39c45875529567391ed7
Graham Mace <graham@maceage.com>
2024-10-14 16:02:27 +1100

    Add KokoEatingBananasProblem and tests

---
+++ LeetCode.Problems/BinarySearch/KokoEatingBananasProblem.cs
@@ -0,0 +1,70 @@
+﻿namespace LeetCode.Problems.BinarySearch;
+
+public class KokoEatingBananasProblem
+{
+    public int MinEatingSpeed(int[] piles, int h)
+    {
+        //return MinEatingSpeedBruteForce(piles, h);
+        return MinEatingSpeedBinarySearch(piles, h);
+    }
+
+    private static int MinEatingSpeedBinarySearch(int[] piles, int h)
+    {
+        int left = 1;
+        int right = 1;
+
+        foreach (int pile in piles)
+        {
+            right = Math.Max(right, pile);
+        }
+
+        while (left < right)
+        {
+            int middle = (left + right) / 2;
+            double hourSpent = 0;
+
+            foreach (int pile in piles)
+            {
+                hourSpent += Math.Ceiling((double)pile / middle);
+            }
+
+            if (hourSpent <= h)
+            {
+                right = middle;
+            }
+            else
+            {
+                left = middle + 1;
+            }
+        }
+
+        return right;
+    }
+
+    private static int MinEatingSpeedBruteForce(int[] piles, int h)
+    {
+        int speed = 1;
+
+        while (true)
+        {
+            double hourSpent = 0;
+
+            foreach (int pile in piles)
+            {
+                hourSpent += Math.Ceiling((double)pile / speed);
+
+                if (hourSpent > h)
+                {
+                    break;
+                }
+            }
+
+            if (hourSpent <= h)
+            {
+                return speed;
+            }
+
+            speed += 1;
+        }
+    }
+}
---
+++ LeetCode.Tests/BinarySearch/KokoEatingBananasProblemTests.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.BinarySearch;
+
+namespace LeetCode.Tests.BinarySearch;
+
+public class KokoEatingBananasProblemTests
+{
+    [Fact]
+    public void MinEatingSpeed_TestCase_1()
+    {
+        // Arrange
+        int[] piles = [3, 6, 7, 11];
+        int h = 8;
+
+        KokoEatingBananasProblem kokoEatingBananasProblem = new KokoEatingBananasProblem();
+
+        // Act
+        int actual = kokoEatingBananasProblem.MinEatingSpeed(piles, h);
+
+        // Assert
+        actual.Should().Be(4);
+    }
+
+    [Fact]
+    public void MinEatingSpeed_TestCase_2()
+    {
+        // Arrange
+        int[] piles = [30, 11, 23, 4, 20];
+        int h = 5;
+
+        KokoEatingBananasProblem kokoEatingBananasProblem = new KokoEatingBananasProblem();
+
+        // Act
+        int actual = kokoEatingBananasProblem.MinEatingSpeed(piles, h);
+
+        // Assert
+        actual.Should().Be(30);
+    }
+
+    [Fact]
+    public void MinEatingSpeed_TestCase_3()
+    {
+        // Arrange
+        int[] piles = [30, 11, 23, 4, 20];
+        int h = 6;
+
+        KokoEatingBananasProblem kokoEatingBananasProblem = new KokoEatingBananasProblem();
+
+        // Act
+        int actual = kokoEatingBananasProblem.MinEatingSpeed(piles, h);
+
+        // Assert
+        actual.Should().Be(23);
+    }
+}
