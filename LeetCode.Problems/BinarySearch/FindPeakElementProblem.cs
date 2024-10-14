727104183a72fdf500e4685d265aa95115fc95b7
Graham Mace <graham@maceage.com>
2024-10-14 15:08:22 +1100

    Add FindPeakElementProblem and tests

---
+++ LeetCode.Problems/BinarySearch/FindPeakElementProblem.cs
@@ -0,0 +1,68 @@
+﻿namespace LeetCode.Problems.BinarySearch;
+
+public class FindPeakElementProblem
+{
+    public int FindPeakElement(int[] nums)
+    {
+        //return FindPeakElementLinearScan(nums);
+        //return FindPeakElementRecursiveBinarySearch(nums);
+        return FindPeakElementIterativeBinarySearch(nums);
+    }
+
+    private static int FindPeakElementIterativeBinarySearch(int[] nums)
+    {
+        int left = 0;
+        int right = nums.Length - 1;
+
+        while (left < right)
+        {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] > nums[mid + 1])
+            {
+                right = mid;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    private static int FindPeakElementRecursiveBinarySearch(int[] nums)
+    {
+        return Search(nums, 0, nums.Length - 1);
+    }
+
+    private static int Search(int[] nums, int left, int right)
+    {
+        if (left == right)
+        {
+            return left;
+        }
+
+        int mid = (left + right) / 2;
+
+        if (nums[mid] > nums[mid + 1])
+        {
+            return Search(nums, 1, mid);
+        }
+
+        return Search(nums, mid + 1, right);
+    }
+
+    private static int FindPeakElementLinearScan(int[] nums)
+    {
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            if (nums[i] > nums[i + 1])
+            {
+                return i;
+            }
+        }
+
+        return nums.Length - 1;
+    }
+}
---
+++ LeetCode.Tests/BinarySearch/FindPeakElementProblemTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.BinarySearch;
+
+namespace LeetCode.Tests.BinarySearch;
+
+public class FindPeakElementProblemTests
+{
+    [Fact]
+    public void FindPeakElement_TestCase_1()
+    {
+        // Arrange
+        int[] nums = [1, 2, 3, 1];
+
+        FindPeakElementProblem findPeakElementProblem = new FindPeakElementProblem();
+
+        // Act
+        int actual = findPeakElementProblem.FindPeakElement(nums);
+
+        // Assert
+        actual.Should().Be(2);
+    }
+
+    [Fact]
+    public void FindPeakElement_TestCase_2()
+    {
+        // Arrange
+        int[] nums = [1, 2, 1, 3, 5, 6, 4];
+
+        FindPeakElementProblem findPeakElementProblem = new FindPeakElementProblem();
+
+        // Act
+        int actual = findPeakElementProblem.FindPeakElement(nums);
+
+        // Assert
+        actual.Should().Be(5);
+        //actual.Should().Be(1);
+    }
+}
