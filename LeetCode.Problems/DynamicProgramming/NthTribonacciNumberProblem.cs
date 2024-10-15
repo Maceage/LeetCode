5626c0bb04b6d6d028904f1126a984e9439739a7
Graham Mace <graham@maceage.com>
2024-10-15 16:37:45 +1100

    Add NthTribonacciNumberProblem and tests

---
+++ LeetCode.Problems/DynamicProgramming/NthTribonacciNumberProblem.cs
@@ -0,0 +1,57 @@
+﻿namespace LeetCode.Problems.DynamicProgramming;
+
+public class NthTribonacciNumberProblem
+{
+    public int Tribonacci(int n)
+    {
+        return TribonacciSliding(n);
+        return TribonacciArray(n);
+    }
+
+    private int TribonacciSliding(int n)
+    {
+        if (n < 3)
+        {
+            return n > 0 ? 1 : 0;
+        }
+
+        int a = 0;
+        int b = 1;
+        int c = 1;
+
+        for (int i = 0; i < n - 2; ++i)
+        {
+            int tmp = a + b + c;
+
+            a = b;
+            b = c;
+            c = tmp;
+        }
+
+        return c;
+    }
+
+    private int TribonacciArray(int n)
+    {
+        int[] numberArray = new int[n + 3];
+
+        numberArray[0] = 0;
+        numberArray[1] = 1;
+        numberArray[2] = 1;
+
+        for (int i = 3; i < numberArray.Length; i++)
+        {
+            int firstIndex = i - 3;
+            int secondIndex = i - 2;
+            int thirdIndex = i - 1;
+
+            int first = numberArray[firstIndex];
+            int second = numberArray[secondIndex];
+            int third = numberArray[thirdIndex];
+
+            numberArray[i] = first + second + third;
+        }
+
+        return numberArray[n];
+    }
+}
---
+++ LeetCode.Tests/DynamicProgramming/NthTribonacciNumberProblemTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.DynamicProgramming;
+
+namespace LeetCode.Tests.DynamicProgramming;
+
+public class NthTribonacciNumberProblemTests
+{
+    [Fact]
+    public void Tribonacci_TestCase_1()
+    {
+        // Arrange
+        int n = 4;
+
+        NthTribonacciNumberProblem nthTribonacciNumberProblem = new NthTribonacciNumberProblem();
+
+        // Act
+        int actual = nthTribonacciNumberProblem.Tribonacci(n);
+
+        // Assert
+        actual.Should().Be(4);
+    }
+
+    [Fact]
+    public void Tribonacci_TestCase_2()
+    {
+        // Arrange
+        int n = 25;
+
+        NthTribonacciNumberProblem nthTribonacciNumberProblem = new NthTribonacciNumberProblem();
+
+        // Act
+        int actual = nthTribonacciNumberProblem.Tribonacci(n);
+
+        // Assert
+        actual.Should().Be(1389537);
+    }
+
+    [Fact]
+    public void Tribonacci_TestCase_3()
+    {
+        // Arrange
+        int n = 1;
+
+        NthTribonacciNumberProblem nthTribonacciNumberProblem = new NthTribonacciNumberProblem();
+
+        // Act
+        int actual = nthTribonacciNumberProblem.Tribonacci(n);
+
+        // Assert
+        actual.Should().Be(1);
+    }
+}
