a3f8067b7d54fed438c9b4f73595431915c5efab
Graham Mace <graham@maceage.com>
2024-10-15 16:04:33 +1100

    Add CombinationSumProblem and tests

---
+++ LeetCode.Problems/Backtracking/CombinationSumProblem.cs
@@ -0,0 +1,43 @@
+﻿namespace LeetCode.Problems.Backtracking;
+
+public class CombinationSumProblem
+{
+    private readonly IList<IList<int>> _combinations = new List<IList<int>>();
+
+    public IList<IList<int>> CombinationSum3(int k, int n)
+    {
+        if (n > 1)
+        {
+            Backtrack(0, k, n, 1, []);
+        }
+
+        return _combinations;
+    }
+
+    private void Backtrack(int currentLevel, int maxLevel, int target, int currentNumber, List<int> currentNumbers)
+    {
+        if (currentLevel == maxLevel)
+        {
+            int currentTotal = currentNumbers.Sum();
+
+            if (currentTotal == target)
+            {
+                _combinations.Add(currentNumbers.ToList());
+            }
+        }
+        else
+        {
+            for (int i = currentNumber; i <= 9; i++)
+            {
+                if (!currentNumbers.Contains(i))
+                {
+                    currentNumbers.Add(i);
+
+                    Backtrack(currentLevel + 1, maxLevel, target, i, currentNumbers);
+
+                    currentNumbers.Remove(i);
+                }
+            }
+        }
+    }
+}
---
+++ LeetCode.Tests/BackTracking/CombinationSumProblemTests.cs
@@ -0,0 +1,67 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.Backtracking;
+
+namespace LeetCode.Tests.BackTracking;
+
+public class CombinationSumProblemTests
+{
+    [Fact]
+    public void CombinationSum3_TestCase_1()
+    {
+        // Arrange
+        int k = 3;
+        int n = 7;
+
+        IList<IList<int>> expected = new List<IList<int>>
+        {
+            { [1, 2, 4] }
+        };
+
+        CombinationSumProblem combinationSumProblem = new CombinationSumProblem();
+
+        // Act
+        IList<IList<int>> actual = combinationSumProblem.CombinationSum3(k, n);
+
+        // Assert
+        actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public void CombinationSum3_TestCase_2()
+    {
+        // Arrange
+        int k = 3;
+        int n = 9;
+
+        IList<IList<int>> expected = new List<IList<int>>
+        {
+            { [1, 2, 6] },
+            { [1, 3, 5] },
+            { [2, 3, 4] }
+        };
+
+        CombinationSumProblem combinationSumProblem = new CombinationSumProblem();
+
+        // Act
+        IList<IList<int>> actual = combinationSumProblem.CombinationSum3(k, n);
+
+        // Assert
+        actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public void CombinationSum3_TestCase_3()
+    {
+        // Arrange
+        int k = 4;
+        int n = 1;
+
+        CombinationSumProblem combinationSumProblem = new CombinationSumProblem();
+
+        // Act
+        IList<IList<int>> actual = combinationSumProblem.CombinationSum3(k, n);
+
+        // Assert
+        actual.Should().BeEquivalentTo(new List<IList<int>>(), options => options.WithStrictOrdering());
+    }
+}
