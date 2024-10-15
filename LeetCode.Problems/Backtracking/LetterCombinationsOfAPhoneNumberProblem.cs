582b138356c9c5ea58713e095c75602420056481
Graham Mace <graham@maceage.com>
2024-10-15 14:28:07 +1100

    Add LetterCombinationsOfAPhoneNumberProblem and tests

---
+++ LeetCode.Problems/Backtracking/LetterCombinationsOfAPhoneNumberProblem.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+
+namespace LeetCode.Problems.Backtracking;
+
+public class LetterCombinationsOfAPhoneNumberProblem
+{
+    private readonly Dictionary<char, string> _lettersDictionary = new()
+    {
+        {'2', "abc"},
+        {'3', "def"},
+        {'4', "ghi"},
+        {'5', "jkl"},
+        {'6', "mno"},
+        {'7', "pqrs"},
+        {'8', "tuv"},
+        {'9', "wxyz"},
+    };
+
+    private readonly List<string> _combinations = new();
+
+    private string _phoneDigits;
+
+    public IList<string> LetterCombinations(string digits)
+    {
+        if (digits.Length == 0)
+        {
+            return _combinations;
+        }
+
+        _phoneDigits = digits;
+
+        Backtrack(0, new StringBuilder());
+
+        return _combinations;
+    }
+
+    private void Backtrack(int index, StringBuilder path)
+    {
+        if (path.Length == _phoneDigits.Length)
+        {
+            _combinations.Add(path.ToString());
+            return;
+        }
+
+        string possibleLetters = _lettersDictionary[_phoneDigits[index]];
+
+        foreach (char letter in possibleLetters)
+        {
+            path.Append(letter);
+
+            Backtrack(index + 1, path);
+
+            path.Remove(path.Length - 1, 1);
+        }
+    }
+}
---
+++ LeetCode.Tests/BackTracking/LetterCombinationsOfAPhoneNumberProblemTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.Backtracking;
+
+namespace LeetCode.Tests.BackTracking;
+
+public class LetterCombinationsOfAPhoneNumberProblemTests
+{
+    [Fact]
+    public void LetterCombinations_TestCase_1()
+    {
+        // Arrange
+        string digits = "23";
+
+        LetterCombinationsOfAPhoneNumberProblem letterCombinationsOfAPhoneNumber = new LetterCombinationsOfAPhoneNumberProblem();
+
+        // Act
+        IList<string> actual = letterCombinationsOfAPhoneNumber.LetterCombinations(digits);
+
+        // Assert
+        actual.Should().BeEquivalentTo(["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"], options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public void LetterCombinations_TestCase_2()
+    {
+        // Arrange
+        string digits = "";
+
+        LetterCombinationsOfAPhoneNumberProblem letterCombinationsOfAPhoneNumber = new LetterCombinationsOfAPhoneNumberProblem();
+
+        // Act
+        IList<string> actual = letterCombinationsOfAPhoneNumber.LetterCombinations(digits);
+
+        // Assert
+        actual.Should().BeEquivalentTo([], options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public void LetterCombinations_TestCase_3()
+    {
+        // Arrange
+        string digits = "2";
+
+        LetterCombinationsOfAPhoneNumberProblem letterCombinationsOfAPhoneNumber = new LetterCombinationsOfAPhoneNumberProblem();
+
+        // Act
+        IList<string> actual = letterCombinationsOfAPhoneNumber.LetterCombinations(digits);
+
+        // Assert
+        actual.Should().BeEquivalentTo(["a", "b", "c"], options => options.WithStrictOrdering());
+    }
+}
