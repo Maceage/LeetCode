2844b259aade435d6fa3a676b1b4052dbfafb4b5
Graham Mace <graham@maceage.com>
2024-10-19 16:42:19 +1100

    Add UniquePathsProblem and tests

---
+++ LeetCode.Problems/DynamicProgramming/UniquePathsProblem.cs
@@ -0,0 +1,60 @@
+﻿namespace LeetCode.Problems.DynamicProgramming;
+
+public class UniquePathsProblem
+{
+    public int UniquePaths(int m, int n)
+    {
+        return UniquePathsMaths(m, n);
+        return UniquePathsDynamicProgramming(m, n);
+        return UniquePathsRecursion(m, n);
+    }
+
+    private static int UniquePathsMaths(int m, int n)
+    {
+        long totalPlaces = m + n - 2;
+        long minPlaces = Math.Min(m - 1, n - 1);
+        long result = 1;
+
+        for (int i = 0; i < minPlaces; i++)
+        {
+            result = result * (totalPlaces - i) / (i + 1);
+        }
+
+        return (int)result;
+    }
+
+    private static int UniquePathsDynamicProgramming(int m, int n)
+    {
+        int[][] d = new int[m][];
+
+        for (int i = 0; i < m; ++i)
+        {
+            d[i] = new int[n];
+
+            for (int j = 0; j < n; ++j)
+            {
+                d[i][j] = 1;
+            }
+        }
+
+        for (int col = 1; col < m; ++col)
+        {
+            for (int row = 1; row < n; ++row)
+            {
+                d[col][row] = d[col - 1][row] + d[col][row - 1];
+            }
+        }
+
+        return d[m - 1][n - 1];
+    }
+
+    private static int UniquePathsRecursion(int m, int n)
+    {
+        if (m == 1 || n == 1)
+        {
+            return 1;
+        }
+
+        return UniquePathsRecursion(m - 1, n) + UniquePathsRecursion(m, n - 1);
+    }
+}
---
+++ LeetCode.Tests/DynamicProgramming/UniquePathsProblemTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.DynamicProgramming;
+
+namespace LeetCode.Tests.DynamicProgramming;
+
+public class UniquePathsProblemTests
+{
+    [Fact]
+    public void UniquePaths_TestCase_1()
+    {
+        // Arrange
+        int m = 3;
+        int n = 7;
+
+        UniquePathsProblem uniquePathsProblem = new UniquePathsProblem();
+
+        // Act
+        int actual = uniquePathsProblem.UniquePaths(m, n);
+
+        // Assert
+        actual.Should().Be(28);
+    }
+
+    [Fact]
+    public void UniquePaths_TestCase_2()
+    {
+        // Arrange
+        int m = 3;
+        int n = 2;
+
+        UniquePathsProblem uniquePathsProblem = new UniquePathsProblem();
+
+        // Act
+        int actual = uniquePathsProblem.UniquePaths(m, n);
+
+        // Assert
+        actual.Should().Be(3);
+    }
+}
