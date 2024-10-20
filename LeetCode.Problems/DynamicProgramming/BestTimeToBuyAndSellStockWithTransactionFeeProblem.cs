1526547622304faaa544f4166461a52640660cae
Graham Mace <graham@maceage.com>
2024-10-20 17:48:17 +1100

    Add BestTimeToBuyAndSellStockWithTransactionFeeProblem and tests

---
+++ LeetCode.Problems/DynamicProgramming/BestTimeToBuyAndSellStockWithTransactionFeeProblem.cs
@@ -0,0 +1,45 @@
+﻿namespace LeetCode.Problems.DynamicProgramming;
+
+public class BestTimeToBuyAndSellStockWithTransactionFeeProblem
+{
+    public int MaxProfit(int[] prices, int fee)
+    {
+        return MaxProfitDynamicProgrammingSpaceOptimised(prices, fee);
+        return MaxProfitDynamicProgramming(prices, fee);
+    }
+
+    private static int MaxProfitDynamicProgrammingSpaceOptimised(int[] prices, int fee)
+    {
+        int n = prices.Length;
+        int free = 0;
+        int hold = -prices[0];
+
+        for (int i = 0; i < n; i++)
+        {
+            int temp = hold;
+            hold = Math.Max(hold, free - prices[i]);
+            free = Math.Max(free, temp + prices[i] - fee);
+        }
+
+        return free;
+    }
+
+    private static int MaxProfitDynamicProgramming(int[] prices, int fee)
+    {
+        int n = prices.Length;
+
+        int[] free = new int[n];
+        int[] hold = new int[n];
+
+        // In order to hold stock on day 0 we have no other choice but to buy it for prices[0]
+        hold[0] = -prices[0];
+
+        for (int i = 1; i < n; i++)
+        {
+            hold[i] = Math.Max(hold[i - 1], free[i - 1] - prices[i]);
+            free[i] = Math.Max(free[i - 1], hold[i - 1] + prices[i] - fee);
+        }
+
+        return free[n - 1];
+    }
+}
---
+++ LeetCode.Tests/DynamicProgramming/BestTimeToBuyAndSellStockWithTransactionFeeProblemTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.DynamicProgramming;
+
+namespace LeetCode.Tests.DynamicProgramming;
+
+public class BestTimeToBuyAndSellStockWithTransactionFeeProblemTests
+{
+    [Fact]
+    public void MaxProfit_TestCase_1()
+    {
+        // Arrange
+        int[] prices = [1, 3, 2, 8, 4, 9];
+        int fee = 2;
+
+        BestTimeToBuyAndSellStockWithTransactionFeeProblem bestTimeToBuyAndSellStockWithTransactionFeeProblem = new BestTimeToBuyAndSellStockWithTransactionFeeProblem();
+
+        // Act
+        int actual = bestTimeToBuyAndSellStockWithTransactionFeeProblem.MaxProfit(prices, fee);
+
+        // Assert
+        actual.Should().Be(8);
+    }
+
+    [Fact]
+    public void MaxProfit_TestCase_3()
+    {
+        // Arrange
+        int[] prices = [1, 3, 7, 5, 10, 3];
+        int fee = 3;
+
+        BestTimeToBuyAndSellStockWithTransactionFeeProblem bestTimeToBuyAndSellStockWithTransactionFeeProblem = new BestTimeToBuyAndSellStockWithTransactionFeeProblem();
+
+        // Act
+        int actual = bestTimeToBuyAndSellStockWithTransactionFeeProblem.MaxProfit(prices, fee);
+
+        // Assert
+        actual.Should().Be(6);
+    }
+}
