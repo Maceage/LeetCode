220d3364081d705b69bfd8dc870eb23f5e28bbda
Graham Mace <graham@maceage.com>
2024-10-21 15:26:03 +1100

    Add SingleNumberProblem and tests

---
+++ LeetCode.Problems/BitManipulation/SingleNumberProblem.cs
@@ -0,0 +1,86 @@
+﻿namespace LeetCode.Problems.BitManipulation;
+
+public class SingleNumberProblem
+{
+    public int SingleNumber(int[] nums)
+    {
+        return SingleNumberWithBitManipulation(nums);
+        return SingleNumberWithMath(nums);
+        return SingleNumberWithHashtable(nums);
+        return SingleNumberWithLists(nums);
+    }
+
+    private static int SingleNumberWithBitManipulation(int[] nums)
+    {
+        int a = 0;
+
+        foreach (int i in nums)
+        {
+            a ^= i;
+        }
+
+        return a;
+    }
+
+    private static int SingleNumberWithMath(int[] nums)
+    {
+        int sumOfSet = 0;
+        int sumOfNums = 0;
+
+        HashSet<int> set = new HashSet<int>();
+
+        foreach (var n in nums)
+        {
+            if (set.Add(n))
+            {
+                sumOfSet += n;
+            }
+
+            sumOfNums += n;
+        }
+
+        return 2 * sumOfSet - sumOfNums;
+    }
+
+    private static int SingleNumberWithHashtable(int[] nums)
+    {
+        Dictionary<int, int> numberCounts = new Dictionary<int, int>();
+
+        foreach (int num in nums)
+        {
+            if (!numberCounts.TryAdd(num, 1))
+            {
+                numberCounts[num]++;
+            }
+        }
+
+        foreach (int num in nums)
+        {
+            if (numberCounts[num] == 1)
+            {
+                return num;
+            }
+        }
+
+        return 0;
+    }
+
+    private static int SingleNumberWithLists(int[] nums)
+    {
+        List<int> noDuplicatesList = new List<int>();
+
+        foreach (int i in nums)
+        {
+            if (!noDuplicatesList.Contains(i))
+            {
+                noDuplicatesList.Add(i);
+            }
+            else
+            {
+                noDuplicatesList.Remove(i);
+            }
+        }
+
+        return noDuplicatesList[0];
+    }
+}
---
+++ LeetCode.Tests/BitManipulation/SingleNumberProblemTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.BitManipulation;
+
+namespace LeetCode.Tests.BitManipulation;
+
+public class SingleNumberProblemTests
+{
+    [Fact]
+    public void SingleNumber_TestCase_1()
+    {
+        // Arrange
+        int[] nums = [2, 2, 1];
+
+        SingleNumberProblem singleNumberProblem = new SingleNumberProblem();
+
+        // Act
+        int actual = singleNumberProblem.SingleNumber(nums);
+
+        // Assert
+        actual.Should().Be(1);
+    }
+
+    [Fact]
+    public void SingleNumber_TestCase_2()
+    {
+        // Arrange
+        int[] nums = [4, 1, 2, 1, 2];
+
+        SingleNumberProblem singleNumberProblem = new SingleNumberProblem();
+
+        // Act
+        int actual = singleNumberProblem.SingleNumber(nums);
+
+        // Assert
+        actual.Should().Be(4);
+    }
+
+    [Fact]
+    public void SingleNumber_TestCase_3()
+    {
+        // Arrange
+        int[] nums = [1];
+
+        SingleNumberProblem singleNumberProblem = new SingleNumberProblem();
+
+        // Act
+        int actual = singleNumberProblem.SingleNumber(nums);
+
+        // Assert
+        actual.Should().Be(1);
+    }
+}
