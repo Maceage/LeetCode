2252113e394c9fd1cad81c6ce8d095c8b350a35d
Graham Mace <graham@maceage.com>
2024-10-21 15:02:01 +1100

    Add CountingBitsProblem and tests

---
+++ LeetCode.Problems/BitManipulation/CountingBitsProblem.cs
@@ -0,0 +1,101 @@
+﻿namespace LeetCode.Problems.BitManipulation;
+
+public class CountingBitsProblem
+{
+    public int[] CountBits(int n)
+    {
+        return CountBitsDynamicProgrammingAndLastSetBit(n);
+        return CountBitsDynamicProgrammingAndLeastSignificantBit(n);
+        return CountBitsDynamicProgrammingAndMostSignificantBit(n);
+        return CountBitsPopCount(n);
+        return CountBitsOriginal(n);
+    }
+
+    private static int[] CountBitsDynamicProgrammingAndLastSetBit(int n)
+    {
+        int[] ans = new int[n + 1];
+
+        for (int x = 1; x <= n; ++x)
+        {
+            ans[x] = ans[x & (x - 1)] + 1;
+        }
+
+        return ans;
+    }
+
+    private static int[] CountBitsDynamicProgrammingAndLeastSignificantBit(int n)
+    {
+        int[] ans = new int[n + 1];
+
+        for (int i = 0; i <= n; ++i)
+        {
+            // x / 2 is x >> 1 and x % 2 is x & 1
+            ans[i] = ans[i >> 1] + (i & 1);
+        }
+
+        return ans;
+    }
+
+    private static int[] CountBitsDynamicProgrammingAndMostSignificantBit(int n)
+    {
+        int[] ans = new int[n + 1];
+        int x = 0;
+        int b = 1;
+
+        // [0, b) is calculated
+        while (b <= n)
+        {
+            // Generate [b, 2b) or [b, n) from [0, b)
+            while (x < b && x + b <= n)
+            {
+                ans[x + b] = ans[x] + 1;
+                ++x;
+            }
+
+            x = 0;      // Reset x
+            b <<= 1;    // b = 2b
+        }
+
+        return ans; ;
+    }
+
+    private static int[] CountBitsPopCount(int n)
+    {
+        int[] ans = new int[n + 1];
+
+        for (int i = 0; i <= n; ++i)
+        {
+            ans[i] = PopCount(i);
+        }
+
+        return ans;
+    }
+
+    private static int PopCount(int x)
+    {
+        int count;
+
+        for (count = 0; x != 0; ++count)
+        {
+            x &= x - 1; // Zeroing out the least significant nonzero bit
+        }
+
+        return count;
+    }
+
+    private static int[] CountBitsOriginal(int n)
+    {
+        int[] result = new int[n + 1];
+
+        for (int i = 0; i <= n; i++)
+        {
+            string binaryDigits = Convert.ToString(i, 2);
+
+            int numberOfOnes = binaryDigits.Count(b => b == '1');
+
+            result[i] = numberOfOnes;
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode.Tests/BitManipulation/CountingBitsProblemTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.BitManipulation;
+
+namespace LeetCode.Tests.BitManipulation;
+
+public class CountingBitsProblemTests
+{
+    [Fact]
+    public void CountBits_TestCase_1()
+    {
+        // Arrange
+        int n = 2;
+
+        CountingBitsProblem countingBitsProblem = new CountingBitsProblem();
+
+        // Act
+        int[] actual = countingBitsProblem.CountBits(n);
+
+        // Assert
+        actual.Should().BeEquivalentTo([0, 1, 1], options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public void CountBits_TestCase_2()
+    {
+        // Arrange
+        int n = 5;
+
+        CountingBitsProblem countingBitsProblem = new CountingBitsProblem();
+
+        // Act
+        int[] actual = countingBitsProblem.CountBits(n);
+
+        // Assert
+        actual.Should().BeEquivalentTo([0, 1, 1, 2, 1, 2], options => options.WithStrictOrdering());
+    }
+}
