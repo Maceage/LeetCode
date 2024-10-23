12650d6e9c5b9067adf3dabb3ee93d4986aff4a1
Graham Mace <graham@maceage.com>
2024-10-23 14:21:18 +1100

    Add NonOverlappingIntervalsProblem and tests

---
+++ LeetCode.Problems/Intervals/NonOverlappingIntervalsProblem.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Problems.Intervals;
+
+public class NonOverlappingIntervalsProblem
+{
+    public int EraseOverlapIntervals(int[][] intervals)
+    {
+        System.Array.Sort(intervals, (a, b) => a[1].CompareTo(b[1]));
+
+        int ans = 0;
+        int k = int.MinValue;
+
+        foreach (var i in intervals)
+        {
+            int x = i[0];
+            int y = i[1];
+
+            if (x >= k)
+            {
+                k = y;
+            }
+            else
+            {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
---
+++ LeetCode.Tests/Intervals/NonOverlappingIntervalsProblemTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.Intervals;
+
+namespace LeetCode.Tests.Intervals;
+
+public class NonOverlappingIntervalsProblemTests
+{
+    [Fact]
+    public void EraseOverlapInterval_TestCase_1()
+    {
+        // Arrange
+        int[][] intervals = [[1, 2], [2, 3], [3, 4], [1, 3]];
+
+        NonOverlappingIntervalsProblem nonOverlappingIntervalsProblem = new NonOverlappingIntervalsProblem();
+
+        // Act
+        int actual = nonOverlappingIntervalsProblem.EraseOverlapIntervals(intervals);
+
+        // Assert
+        actual.Should().Be(1);
+    }
+
+    [Fact]
+    public void EraseOverlapInterval_TestCase_2()
+    {
+        // Arrange
+        int[][] intervals = [[1, 2], [1, 2], [1, 2]];
+
+        NonOverlappingIntervalsProblem nonOverlappingIntervalsProblem = new NonOverlappingIntervalsProblem();
+
+        // Act
+        int actual = nonOverlappingIntervalsProblem.EraseOverlapIntervals(intervals);
+
+        // Assert
+        actual.Should().Be(2);
+    }
+
+    [Fact]
+    public void EraseOverlapInterval_TestCase_3()
+    {
+        // Arrange
+        int[][] intervals = [[1, 2], [2, 3]];
+
+        NonOverlappingIntervalsProblem nonOverlappingIntervalsProblem = new NonOverlappingIntervalsProblem();
+
+        // Act
+        int actual = nonOverlappingIntervalsProblem.EraseOverlapIntervals(intervals);
+
+        // Assert
+        actual.Should().Be(0);
+    }
+}
