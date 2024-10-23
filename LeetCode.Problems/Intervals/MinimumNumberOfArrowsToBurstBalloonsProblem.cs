de4a69dbe1a95f6c53e78cc6b281a185b9e3ecc4
Graham Mace <graham@maceage.com>
2024-10-23 14:50:30 +1100

    Add MinimumNumberOfArrowsToBurstBalloonsProblem and tests

---
+++ LeetCode.Problems/Intervals/MinimumNumberOfArrowsToBurstBalloonsProblem.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Problems.Intervals;
+
+public class MinimumNumberOfArrowsToBurstBalloonsProblem
+{
+    public int FindMinArrowShots(int[][] points)
+    {
+        System.Array.Sort(points, (a, b) => a[1].CompareTo(b[1]));
+
+        int arrows = 1;
+        int k = int.MinValue;
+        int lastEnd = points[0][1];
+
+        foreach (var p in points)
+        {
+            int xStart = p[0];
+            int xEnd = p[1];
+
+            if (lastEnd < xStart)
+            {
+                arrows++;
+                lastEnd = xEnd;
+            }
+        }
+
+        return arrows;
+    }
+}
---
+++ LeetCode.Tests/Intervals/MinimumNumberOfArrowsToBurstBalloonsProblemTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.Intervals;
+
+namespace LeetCode.Tests.Intervals;
+
+public class MinimumNumberOfArrowsToBurstBalloonsProblemTests
+{
+    [Fact]
+    public void FindMinArrowShots_TestCase_1()
+    {
+        // Arrange
+        int[][] points = [[10, 16], [2, 8], [1, 6], [7, 12]];
+
+        MinimumNumberOfArrowsToBurstBalloonsProblem minimumNumberOfArrowsToBurstBalloonsProblem = new MinimumNumberOfArrowsToBurstBalloonsProblem();
+
+        // Act
+        int actual = minimumNumberOfArrowsToBurstBalloonsProblem.FindMinArrowShots(points);
+
+        // Assert
+        actual.Should().Be(2);
+    }
+
+    [Fact]
+    public void FindMinArrowShots_TestCase_2()
+    {
+        // Arrange
+        int[][] points = [[1, 2], [3, 4], [5, 6], [7, 8]];
+
+        MinimumNumberOfArrowsToBurstBalloonsProblem minimumNumberOfArrowsToBurstBalloonsProblem = new MinimumNumberOfArrowsToBurstBalloonsProblem();
+
+        // Act
+        int actual = minimumNumberOfArrowsToBurstBalloonsProblem.FindMinArrowShots(points);
+
+        // Assert
+        actual.Should().Be(4);
+    }
+
+    [Fact]
+    public void FindMinArrowShots_TestCase_3()
+    {
+        // Arrange
+        int[][] points = [[1, 2], [2, 3], [3, 4], [4, 5]];
+
+        MinimumNumberOfArrowsToBurstBalloonsProblem minimumNumberOfArrowsToBurstBalloonsProblem = new MinimumNumberOfArrowsToBurstBalloonsProblem();
+
+        // Act
+        int actual = minimumNumberOfArrowsToBurstBalloonsProblem.FindMinArrowShots(points);
+
+        // Assert
+        actual.Should().Be(2);
+    }
+}
