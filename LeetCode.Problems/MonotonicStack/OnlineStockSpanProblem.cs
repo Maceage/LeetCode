bd7463744063bc318b970a7ad733afe6b17a6f75
Graham Mace <graham@maceage.com>
2024-10-24 15:33:38 +1100

    Add OnlineStockSpanProblem and tests

---
+++ LeetCode.Problems/MonotonicStack/OnlineStockSpanProblem.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Problems.MonotonicStack;
+
+public class OnlineStockSpanProblem
+{
+    public class StockSpanner
+    {
+        private readonly Stack<int[]> _stockPrices = new();
+
+        public int Next(int price)
+        {
+            int stockSpan = 1;
+
+            while (_stockPrices.Count > 0 && _stockPrices.Peek()[0] <= price)
+            {
+                stockSpan += _stockPrices.Pop()[1];
+            }
+
+            _stockPrices.Push([price, stockSpan]);
+
+            return stockSpan;
+        }
+    }
+}
---
+++ LeetCode.Tests/MonotonicStack/OnlineStockSpanProblemTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.MonotonicStack;
+
+namespace LeetCode.Tests.MonotonicStack;
+
+public class OnlineStockSpanProblemTests
+{
+    [Fact]
+    public void StockSpanner_TestCase_1()
+    {
+        // Arrange
+        OnlineStockSpanProblem.StockSpanner stockSpanner = new OnlineStockSpanProblem.StockSpanner();
+
+        // Act/Assert
+        stockSpanner.Next(100).Should().Be(1);
+        stockSpanner.Next(80).Should().Be(1);
+        stockSpanner.Next(60).Should().Be(1);
+        stockSpanner.Next(70).Should().Be(2);
+        stockSpanner.Next(60).Should().Be(1);
+        stockSpanner.Next(75).Should().Be(4);
+        stockSpanner.Next(85).Should().Be(6);
+    }
+}
