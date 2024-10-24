1132fef3ebee74cfcb59503c036e40bbcd9c461f
Graham Mace <graham@maceage.com>
2024-10-24 14:56:02 +1100

    Add DailyTemperaturesProblem and tests

---
+++ LeetCode.Problems/MonotonicStack/DailyTemperaturesProblem.cs
@@ -0,0 +1,84 @@
+﻿namespace LeetCode.Problems.MonotonicStack;
+
+public class DailyTemperaturesProblem
+{
+    public int[] DailyTemperatures(int[] temperatures)
+    {
+        return DailyTemperaturesArrayOptimisedSpace(temperatures);
+        return DailyTemperaturesMonotonicStack(temperatures);
+        return DailyTemperaturesBruteForce(temperatures);
+    }
+
+    private static int[] DailyTemperaturesArrayOptimisedSpace(int[] temperatures)
+    {
+        int n = temperatures.Length;
+        int hottest = 0;
+        int[] answer = new int[n];
+
+        for (int currentDay = n - 1; currentDay >= 0; currentDay--)
+        {
+            int currentTemp = temperatures[currentDay];
+
+            if (currentTemp >= hottest)
+            {
+                hottest = currentTemp;
+                continue;
+            }
+
+            int days = 1;
+
+            while (temperatures[currentDay + days] <= currentTemp)
+            {
+                days += answer[currentDay + days];
+            }
+
+            answer[currentDay] = days;
+        }
+
+        return answer;
+    }
+
+    private static int[] DailyTemperaturesMonotonicStack(int[] temperatures)
+    {
+        int n = temperatures.Length;
+        int[] answer = new int[n];
+
+        Stack<int> stack = new Stack<int>();
+
+        for (int currentDay = 0; currentDay < n; currentDay++)
+        {
+            int currentTemp = temperatures[currentDay];
+
+            while ((stack.Count > 0) && (temperatures[stack.Peek()] < currentTemp))
+            {
+                int previousDay = stack.Pop();
+
+                answer[previousDay] = currentDay - previousDay;
+            }
+
+            stack.Push(currentDay);
+        }
+
+        return answer;
+    }
+
+    private static int[] DailyTemperaturesBruteForce(int[] temperatures)
+    {
+        int n = temperatures.Length;
+        int[] answer = new int[n];
+
+        for (int day = 0; day < n; day++)
+        {
+            for (int futureDay = day + 1; futureDay < n; futureDay++)
+            {
+                if (temperatures[futureDay] > temperatures[day])
+                {
+                    answer[day] = futureDay - day;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
---
+++ LeetCode.Tests/MonotonicStack/DailyTemperaturesProblemTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.MonotonicStack;
+
+namespace LeetCode.Tests.MonotonicStack;
+
+public class DailyTemperaturesProblemTests
+{
+    [Fact]
+    public void DailyTemperatures_TestCase_1()
+    {
+        // Arrange
+        int[] temperatures = [73, 74, 75, 71, 69, 72, 76, 73];
+
+        DailyTemperaturesProblem dailyTemperaturesProblem = new DailyTemperaturesProblem();
+
+        // Act
+        int[] actual = dailyTemperaturesProblem.DailyTemperatures(temperatures);
+
+        // Assert
+        actual.Should().BeEquivalentTo([1, 1, 4, 2, 1, 1, 0, 0], options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public void DailyTemperatures_TestCase_2()
+    {
+        // Arrange
+        int[] temperatures = [30, 40, 50, 60];
+
+        DailyTemperaturesProblem dailyTemperaturesProblem = new DailyTemperaturesProblem();
+
+        // Act
+        int[] actual = dailyTemperaturesProblem.DailyTemperatures(temperatures);
+
+        // Assert
+        actual.Should().BeEquivalentTo([1, 1, 1, 0], options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public void DailyTemperatures_TestCase_3()
+    {
+        // Arrange
+        int[] temperatures = [30, 60, 90];
+
+        DailyTemperaturesProblem dailyTemperaturesProblem = new DailyTemperaturesProblem();
+
+        // Act
+        int[] actual = dailyTemperaturesProblem.DailyTemperatures(temperatures);
+
+        // Assert
+        actual.Should().BeEquivalentTo([1, 1, 0], options => options.WithStrictOrdering());
+    }
+}
