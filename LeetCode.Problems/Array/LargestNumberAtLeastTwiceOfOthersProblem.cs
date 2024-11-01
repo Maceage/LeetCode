c9aa85fbce5cc0d22f528cedc911bb904960ee1b
Graham Mace <graham@maceage.com>
2024-11-01 17:20:03 +1100

    Add LargestNumberAtLeastTwiceOfOthersProblem and tests

---
+++ LeetCode.Problems/Array/LargestNumberAtLeastTwiceOfOthersProblem.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode.Problems.Array;
+
+/// <summary>
+/// https://leetcode.com/problems/largest-number-at-least-twice-of-others/
+/// </summary>
+public class LargestNumberAtLeastTwiceOfOthersProblem
+{
+    public int DominantIndex(int[] nums)
+    {
+        int dominantIndex = -1;
+
+        int largest = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > largest)
+            {
+                largest = nums[i];
+                dominantIndex = i;
+            }
+        }
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (i != dominantIndex)
+            {
+                int calculated = nums[i] * 2;
+
+                if (calculated > largest)
+                {
+                    dominantIndex = -1;
+                    break;
+                }
+            }
+        }
+
+        return dominantIndex;
+    }
+}
---
+++ LeetCode.Tests/Array/LargestNumberAtLeastTwiceOfOthersProblemTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.Array;
+
+namespace LeetCode.Tests.Array;
+
+public class LargestNumberAtLeastTwiceOfOthersProblemTests
+{
+    [Fact]
+    public void DominantIndex_TestCase_1()
+    {
+        // Arrange
+        int[] nums = [3, 6, 1, 0];
+
+        LargestNumberAtLeastTwiceOfOthersProblem largestNumberAtLeastTwiceOfOthersProblem = new LargestNumberAtLeastTwiceOfOthersProblem();
+
+        // Act
+        int actual = largestNumberAtLeastTwiceOfOthersProblem.DominantIndex(nums);
+
+        // Assert
+        actual.Should().Be(1);
+    }
+
+    [Fact]
+    public void DominantIndex_TestCase_2()
+    {
+        // Arrange
+        int[] nums = [1, 2, 3, 4];
+
+        LargestNumberAtLeastTwiceOfOthersProblem largestNumberAtLeastTwiceOfOthersProblem = new LargestNumberAtLeastTwiceOfOthersProblem();
+
+        // Act
+        int actual = largestNumberAtLeastTwiceOfOthersProblem.DominantIndex(nums);
+
+        // Assert
+        actual.Should().Be(-1);
+    }
+}
