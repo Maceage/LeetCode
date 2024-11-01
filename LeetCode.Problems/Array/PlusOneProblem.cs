83f398e0a59ba5c563da94a25adcb708f1fc6589
Graham Mace <graham@maceage.com>
2024-11-01 17:20:23 +1100

    Add PlusOneProblem and tests

---
+++ LeetCode.Problems/Array/PlusOneProblem.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Problems.Array;
+
+/// <summary>
+/// https://leetcode.com/problems/plus-one/
+/// </summary>
+public class PlusOneProblem
+{
+    public int[] PlusOne(int[] digits)
+    {
+        for (int i = digits.Length - 1; i >= 0; i--)
+        {
+            int digit = digits[i];
+
+            if (digit == 9)
+            {
+                digits[i] = 0;
+            }
+            else
+            {
+                digits[i]++;
+
+                return digits;
+            }
+        }
+
+        int[] newDigits = new int[digits.Length + 1];
+
+        newDigits[0] = 1;
+
+        return newDigits;
+    }
+}
---
+++ LeetCode.Tests/Array/PlusOneProblemTests.cs
@@ -0,0 +1,67 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.Array;
+
+namespace LeetCode.Tests.Array;
+
+public class PlusOneProblemTests
+{
+    [Fact]
+    public void PlusOne_TestCase_1()
+    {
+        // Arrange
+        int[] digits = [1, 2, 3];
+
+        PlusOneProblem plusOneProblem = new PlusOneProblem();
+
+        // Act
+        int[] actual = plusOneProblem.PlusOne(digits);
+
+        // Assert
+        actual.Should().BeEquivalentTo([1, 2, 4], options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public void PlusOne_TestCase_2()
+    {
+        // Arrange
+        int[] digits = [4, 3, 2, 1];
+
+        PlusOneProblem plusOneProblem = new PlusOneProblem();
+
+        // Act
+        int[] actual = plusOneProblem.PlusOne(digits);
+
+        // Assert
+        actual.Should().BeEquivalentTo([4, 3, 2, 2], options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public void PlusOne_TestCase_3()
+    {
+        // Arrange
+        int[] digits = [9];
+
+        PlusOneProblem plusOneProblem = new PlusOneProblem();
+
+        // Act
+        int[] actual = plusOneProblem.PlusOne(digits);
+
+        // Assert
+        actual.Should().BeEquivalentTo([1, 0], options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public void PlusOne_TestCase_4()
+    {
+        // Arrange
+        int[] digits = [9, 9];
+
+        PlusOneProblem plusOneProblem = new PlusOneProblem();
+
+        // Act
+        int[] actual = plusOneProblem.PlusOne(digits);
+
+        // Assert
+        actual.Should().BeEquivalentTo([1, 0, 0], options => options.WithStrictOrdering());
+    }
+}
