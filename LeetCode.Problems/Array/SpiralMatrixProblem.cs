68bce8fbc0732f29a65262bf3161e830837f2313
Graham Mace <graham@maceage.com>
2024-11-05 15:20:21 +1100

    Add SpiralMatrixProblem and tests

---
+++ LeetCode.Problems/Array/SpiralMatrixProblem.cs
@@ -0,0 +1,92 @@
+﻿namespace LeetCode.Problems.Array;
+
+public class SpiralMatrixProblem
+{
+    private readonly int[] _rightDirection = [1, 0];
+    private readonly int[] _downDirection = [0, 1];
+    private readonly int[] _leftDirection = [-1, 0];
+    private readonly int[] _upDirection = [0, -1];
+
+    public IList<int> SpiralOrder(int[][] matrix)
+    {
+        if (matrix == null || matrix.Length == 0)
+        {
+            return [];
+        }
+
+        int rowCount = matrix.Length;
+        int colCount = matrix[0].Length;
+
+        int row = 0;
+        int col = 0;
+
+        int rowTopBoundary = -1;
+        int rowBottomBoundary = rowCount;
+
+        int colLeftBoundary = -1;
+        int colRightBoundary = colCount;
+
+        int[] direction = _rightDirection;
+
+        List<int> result = new List<int>(rowCount * colCount);
+
+        do
+        {
+            int currentValue = matrix[row][col];
+
+            result.Add(currentValue);
+
+            col += direction[0];
+            row += direction[1];
+
+            if (col >= colRightBoundary)
+            {
+                // Hit the right boundary
+                direction = _downDirection;
+
+                col--;
+
+                rowTopBoundary++;
+
+                col += direction[0];
+                row += direction[1];
+            }
+            else if (row >= rowBottomBoundary)
+            {
+                // Hit the bottom boundary
+                direction = _leftDirection;
+
+                row--;
+                colRightBoundary--;
+
+                col += direction[0];
+                row += direction[1];
+            }
+            else if (col <= colLeftBoundary)
+            {
+                // Hit the left boundary
+                direction = _upDirection;
+
+                col++;
+                rowBottomBoundary--;
+
+                col += direction[0];
+                row += direction[1];
+            }
+            else if (row <= rowTopBoundary)
+            {
+                // Hit the top boundary
+                direction = _rightDirection;
+
+                row++;
+                colLeftBoundary++;
+
+                col += direction[0];
+                row += direction[1];
+            }
+        }
+        while ((row > rowTopBoundary) && (row < rowBottomBoundary) && (col > colLeftBoundary) && (col < colRightBoundary));
+
+        return result;
+    }
+}
---
+++ LeetCode.Tests/Array/SpiralMatrixProblemTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.Array;
+
+namespace LeetCode.Tests.Array;
+
+public class SpiralMatrixProblemTests
+{
+    [Fact]
+    public void SpiralOrder_TestCase_1()
+    {
+        // Arrange
+        int[][] matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+        SpiralMatrixProblem spiralMatrixProblem = new SpiralMatrixProblem();
+
+        // Act
+        IList<int> actual = spiralMatrixProblem.SpiralOrder(matrix);
+
+        // Assert
+        actual.Should().BeEquivalentTo([1, 2, 3, 6, 9, 8, 7, 4, 5], options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public void SpiralOrder_TestCase_2()
+    {
+        // Arrange
+        int[][] matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]];
+
+        SpiralMatrixProblem spiralMatrixProblem = new SpiralMatrixProblem();
+
+        // Act
+        IList<int> actual = spiralMatrixProblem.SpiralOrder(matrix);
+
+        // Assert
+        actual.Should().BeEquivalentTo([1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7], options => options.WithStrictOrdering());
+    }
+}
