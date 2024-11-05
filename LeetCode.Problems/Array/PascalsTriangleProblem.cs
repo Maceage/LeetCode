76b64e43e7ebd72ad9f8ce08194e59fb99e4d537
Graham Mace <graham@maceage.com>
2024-11-05 15:56:24 +1100

    Add PascalsTriangleProblem and tests

---
+++ LeetCode.Problems/Array/PascalsTriangleProblem.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.Problems.Array;
+
+public class PascalsTriangleProblem
+{
+    public IList<IList<int>> Generate(int numRows)
+    {
+        if (numRows <= 0)
+        {
+            return [];
+        }
+
+        IList<IList<int>> triangle = new List<IList<int>>();
+
+        triangle.Add([1]);
+
+        for (int rowNum = 1; rowNum < numRows; rowNum++)
+        {
+            List<int> row = new List<int>();
+
+            List<int> previousRow = (List<int>)triangle[rowNum - 1];
+
+            row.Add(1);
+
+            for (int j = 1; j < rowNum; j++)
+            {
+                row.Add(previousRow[j - 1] + previousRow[j]);
+            }
+
+            row.Add(1);
+
+            triangle.Add(row);
+        }
+
+        return triangle;
+    }
+}
---
+++ LeetCode.Tests/Array/PascalsTriangleProblemTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.Array;
+
+namespace LeetCode.Tests.Array;
+
+public class PascalsTriangleProblemTests
+{
+    [Fact]
+    public void Generate_TestCase_1()
+    {
+        // Arrange
+        int numRows = 5;
+
+        PascalsTriangleProblem pascalsTriangleProblem = new PascalsTriangleProblem();
+
+        // Act
+        IList<IList<int>> actual = pascalsTriangleProblem.Generate(numRows);
+
+        // Assert
+        int[][] expected = [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]];
+
+        actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public void Generate_TestCase_2()
+    {
+        // Arrange
+        int numRows = 1;
+
+        PascalsTriangleProblem pascalsTriangleProblem = new PascalsTriangleProblem();
+
+        // Act
+        IList<IList<int>> actual = pascalsTriangleProblem.Generate(numRows);
+
+        // Assert
+        int[][] expected = [[1]];
+
+        actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+    }
+}
