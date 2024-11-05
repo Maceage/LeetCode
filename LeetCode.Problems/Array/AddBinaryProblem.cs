8a91574b49f51d54907fd7f8a3c98001eddf65e2
Graham Mace <graham@maceage.com>
2024-11-05 17:39:39 +1100

    Add AddBinaryProblem and tests

---
+++ LeetCode.Problems/Array/AddBinaryProblem.cs
@@ -0,0 +1,113 @@
+﻿using System.Numerics;
+using System.Text;
+
+namespace LeetCode.Problems.Array;
+
+public class AddBinaryProblem
+{
+    public string AddBinary(string a, string b)
+    {
+        return AddBinaryBitManipulation(a, b);
+        return AddBinaryBitByBitManipulation(a, b);
+        return AddBinaryBuiltIn(a, b);
+    }
+
+    private static string AddBinaryBitManipulation(string a, string b)
+    {
+        BigInteger x = ConvertBinaryToBigInteger(a);
+        BigInteger y = ConvertBinaryToBigInteger(b);
+        BigInteger zero = BigInteger.Zero;
+
+        while (y != zero)
+        {
+            BigInteger carry = (x & y) << 1;
+            x ^= y;
+            y = carry;
+        }
+
+        return BigIntegerToBinaryString(x);
+    }
+
+    private static BigInteger ConvertBinaryToBigInteger(string binary)
+    {
+        BigInteger result = 0;
+
+        foreach (char c in binary)
+        {
+            result = result * 2 + (c - '0');
+        }
+
+        return result;
+    }
+
+    private static string BigIntegerToBinaryString(BigInteger number)
+    {
+        if (number == 0)
+        {
+            return "0";
+        }
+
+        string binary = "";
+
+        while (number > 0)
+        {
+            binary = ((number & 1) == 1 ? "1" : "0") + binary;
+            number >>= 1;
+        }
+
+        return binary;
+    }
+
+    private static string AddBinaryBitByBitManipulation(string a, string b)
+    {
+        int n = a.Length;
+        int m = b.Length;
+
+        if (n < m)
+        {
+            return AddBinaryBitByBitManipulation(b, a);
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        int carry = 0;
+        int j = m - 1;
+
+        for (int i = n - 1; i >= 0; --i)
+        {
+            if (a[i] == '1')
+            {
+                ++carry;
+            }
+
+            if (j > -1 && b[j--] == '1')
+            {
+                ++carry;
+            }
+
+            stringBuilder.Append((carry % 2).ToString());
+            carry /= 2;
+        }
+
+        if (carry == 1)
+        {
+            stringBuilder.Append('1');
+        }
+
+        char[] charArray = stringBuilder.ToString().ToCharArray();
+
+        System.Array.Reverse(charArray);
+
+        return new string(charArray);
+    }
+
+    private static string AddBinaryBuiltIn(string a, string b)
+    {
+        int aNum = Convert.ToInt32(a, 2);
+        int bNum = Convert.ToInt32(b, 2);
+
+        int result = aNum + bNum;
+
+        return Convert.ToString(result, 2);
+    }
+}
---
+++ LeetCode.Tests/Array/AddBinaryProblemTests.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.Array;
+
+namespace LeetCode.Tests.Array;
+
+public class AddBinaryProblemTests
+{
+    [Fact]
+    public void AddBinary_TestCase_1()
+    {
+        // Arrange
+        string a = "11";
+        string b = "1";
+
+        AddBinaryProblem addBinaryProblem = new AddBinaryProblem();
+
+        // Act
+        string actual = addBinaryProblem.AddBinary(a, b);
+
+        // Assert
+        actual.Should().Be("100");
+    }
+
+    [Fact]
+    public void AddBinary_TestCase_2()
+    {
+        // Arrange
+        string a = "1010";
+        string b = "1011";
+
+        AddBinaryProblem addBinaryProblem = new AddBinaryProblem();
+
+        // Act
+        string actual = addBinaryProblem.AddBinary(a, b);
+
+        // Assert
+        actual.Should().Be("10101");
+    }
+
+    [Fact]
+    public void AddBinary_TestCase_3()
+    {
+        // Arrange
+        string a = "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101";
+        string b = "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011";
+
+        AddBinaryProblem addBinaryProblem = new AddBinaryProblem();
+
+        // Act
+        string actual = addBinaryProblem.AddBinary(a, b);
+
+        // Assert
+        actual.Should().Be("110111101100010011000101110110100000011101000101011001000011011000001100011110011010010011000000000");
+    }
+}
