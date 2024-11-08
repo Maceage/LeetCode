69e1c6e82505729f99c4d94953beb1737156606b
Graham Mace <graham@maceage.com>
2024-11-08 17:42:30 +1100

    Add MaxConsecutiveOnesProblem and tests

---
+++ LeetCode.Problems/TwoPointers/MaxConsecutiveOnesProblem.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Problems.TwoPointers;
+
+/// <summary>
+/// https://leetcode.com/problems/max-consecutive-ones/
+/// </summary>
+public class MaxConsecutiveOnesProblem
+{
+    public int FindMaxConsecutiveOnes(int[] nums)
+    {
+        int result = 0;
+        int maxOnes = 0;
+
+        foreach (var current in nums)
+        {
+            if (current == 1)
+            {
+                maxOnes++;
+            }
+            else
+            {
+                maxOnes = 0;
+            }
+
+            result = Math.Max(maxOnes, result);
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode.Tests/TwoPointers/MaxConsecutiveOnesProblemTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.TwoPointers;
+
+namespace LeetCode.Tests.TwoPointers;
+
+public class MaxConsecutiveOnesProblemTests
+{
+    [Fact]
+    public void FindMaxConsecutiveOnes_TestCase_1()
+    {
+        // Arrange
+        int[] nums = [1, 1, 0, 1, 1, 1];
+
+        MaxConsecutiveOnesProblem maxConsecutiveOnesProblem = new MaxConsecutiveOnesProblem();
+
+        // Act
+        int actual = maxConsecutiveOnesProblem.FindMaxConsecutiveOnes(nums);
+
+        // Assert
+        actual.Should().Be(3);
+    }
+
+    [Fact]
+    public void FindMaxConsecutiveOnes_TestCase_2()
+    {
+        // Arrange
+        int[] nums = [1, 0, 1, 1, 0, 1];
+
+        MaxConsecutiveOnesProblem maxConsecutiveOnesProblem = new MaxConsecutiveOnesProblem();
+
+        // Act
+        int actual = maxConsecutiveOnesProblem.FindMaxConsecutiveOnes(nums);
+
+        // Assert
+        actual.Should().Be(2);
+    }
+}
