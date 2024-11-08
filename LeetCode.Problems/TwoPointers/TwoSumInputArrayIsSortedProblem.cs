726e6bff208b7d6d9e423ce8e37cbac5a14d7394
Graham Mace <graham@maceage.com>
2024-11-08 16:25:30 +1100

    Add TwoSumInputArrayIsSortedProblem and tests

---
+++ LeetCode.Problems/TwoPointers/TwoSumInputArrayIsSortedProblem.cs
@@ -0,0 +1,83 @@
+﻿namespace LeetCode.Problems.TwoPointers;
+
+/// <summary>
+/// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted
+/// </summary>
+public class TwoSumInputArrayIsSortedProblem
+{
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        return TwoSumTwoPointers(numbers, target);
+        return TwoSumBinarySearch(numbers, target);
+    }
+
+    private static int[] TwoSumTwoPointers(int[] numbers, int target)
+    {
+        int low = 0;
+        int high = numbers.Length - 1;
+
+        while (low < high)
+        {
+            int sum = numbers[low] + numbers[high];
+
+            if (sum == target)
+            {
+                return [low + 1, high + 1];
+            }
+            else if (sum < target)
+            {
+                ++low;
+            }
+            else
+            {
+                --high;
+            }
+        }
+
+        return [-1, -1];
+    }
+
+    private static int[] TwoSumBinarySearch(int[] numbers, int target)
+    {
+        int firstIndex = -1;
+        int secondIndex = -1;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            int current = numbers[i];
+
+            int numToFind = target - current;
+
+            int left = i + 1;
+            int right = numbers.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+
+                if (numbers[mid] == numToFind)
+                {
+                    firstIndex = i;
+                    secondIndex = mid;
+                    break;
+                }
+
+                if (numbers[mid] > numToFind)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+
+            if ((firstIndex >= 0) && (secondIndex >= 0))
+            {
+                break;
+            }
+        }
+
+        return [firstIndex + 1, secondIndex + 1];
+    }
+}
---
+++ LeetCode.Tests/TwoPointers/TwoSumInputArrayIsSortedProblemTests.cs
@@ -0,0 +1,71 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.TwoPointers;
+
+namespace LeetCode.Tests.TwoPointers;
+
+public class TwoSumInputArrayIsSortedProblemTests
+{
+    [Fact]
+    public void TwoSum_TestCase_1()
+    {
+        // Arrange
+        int[] numbers = [2, 7, 11, 15];
+        int target = 9;
+
+        TwoSumInputArrayIsSortedProblem twoSumInputArrayIsSortedProblem = new TwoSumInputArrayIsSortedProblem();
+
+        // Act
+        int[] actual = twoSumInputArrayIsSortedProblem.TwoSum(numbers, target);
+
+        // Assert
+        actual.Should().BeEquivalentTo([1, 2], options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public void TwoSum_TestCase_2()
+    {
+        // Arrange
+        int[] numbers = [2, 3, 4];
+        int target = 6;
+
+        TwoSumInputArrayIsSortedProblem twoSumInputArrayIsSortedProblem = new TwoSumInputArrayIsSortedProblem();
+
+        // Act
+        int[] actual = twoSumInputArrayIsSortedProblem.TwoSum(numbers, target);
+
+        // Assert
+        actual.Should().BeEquivalentTo([1, 3], options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public void TwoSum_TestCase_3()
+    {
+        // Arrange
+        int[] numbers = [-1, 0];
+        int target = -1;
+
+        TwoSumInputArrayIsSortedProblem twoSumInputArrayIsSortedProblem = new TwoSumInputArrayIsSortedProblem();
+
+        // Act
+        int[] actual = twoSumInputArrayIsSortedProblem.TwoSum(numbers, target);
+
+        // Assert
+        actual.Should().BeEquivalentTo([1, 2], options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public void TwoSum_TestCase_4()
+    {
+        // Arrange
+        int[] numbers = [-5, -3, 0, 2, 4, 6, 8];
+        int target = 5;
+
+        TwoSumInputArrayIsSortedProblem twoSumInputArrayIsSortedProblem = new TwoSumInputArrayIsSortedProblem();
+
+        // Act
+        int[] actual = twoSumInputArrayIsSortedProblem.TwoSum(numbers, target);
+
+        // Assert
+        actual.Should().BeEquivalentTo([2, 7], options => options.WithStrictOrdering());
+    }
+}
