b3055388a332ac39b2dc1ebbf422fd77ba8bb01b
Graham Mace <graham@maceage.com>
2024-11-08 15:47:26 +1100

    Add ArrayPartitionProblem and tests

---
+++ LeetCode.Problems/TwoPointers/ArrayPartitionProblem.cs
@@ -0,0 +1,56 @@
+﻿namespace LeetCode.Problems.TwoPointers;
+
+/// <summary>
+/// https://leetcode.com/problems/array-partition
+/// </summary>
+public class ArrayPartitionProblem
+{
+    private static int K = 10000;
+
+    public int ArrayPairSum(int[] nums)
+    {
+        return ArrayPairSumCountingSort(nums);
+        return ArrayPairSumSorting(nums);
+    }
+
+    private static int ArrayPairSumCountingSort(int[] nums)
+    {
+        int[] elementToCount = new int[2 * K + 1];
+
+        foreach (int num in nums)
+        {
+            elementToCount[num + K]++;
+        }
+
+        int maxSum = 0;
+        bool isEvenIndex = true;
+
+        for (int i = 0; i <= 2 * K; i++)
+        {
+            while (elementToCount[i] > 0)
+            {
+                maxSum += (isEvenIndex ? i - K : 0);
+
+                isEvenIndex = !isEvenIndex;
+
+                elementToCount[i]--;
+            }
+        }
+
+        return maxSum;
+    }
+
+    private static int ArrayPairSumSorting(int[] nums)
+    {
+        int result = 0;
+
+        System.Array.Sort(nums);
+
+        for (int i = 0; i < nums.Length; i += 2)
+        {
+            result += nums[i];
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode.Tests/TwoPointers/ArrayPartitionProblemTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.TwoPointers;
+
+namespace LeetCode.Tests.TwoPointers;
+
+public class ArrayPartitionProblemTests
+{
+    [Fact]
+    public void ArrayPairSum_TestCase_1()
+    {
+        // Arrange
+        int[] nums = [1, 4, 3, 2];
+
+        ArrayPartitionProblem arrayPartitionProblem = new ArrayPartitionProblem();
+
+        // Act
+        int actual = arrayPartitionProblem.ArrayPairSum(nums);
+
+        // Assert
+        actual.Should().Be(4);
+    }
+
+    [Fact]
+    public void ArrayPairSum_TestCase_2()
+    {
+        // Arrange
+        int[] nums = [6, 2, 6, 5, 1, 2];
+
+        ArrayPartitionProblem arrayPartitionProblem = new ArrayPartitionProblem();
+
+        // Act
+        int actual = arrayPartitionProblem.ArrayPairSum(nums);
+
+        // Assert
+        actual.Should().Be(9);
+    }
+}
