62bfc160fbe2b63467f84464392166f7e89725e7
Graham Mace <graham@maceage.com>
2024-11-09 16:20:18 +1100

    Add MinimumSizeSubarraySumProblem and tests

---
+++ LeetCode.Problems/TwoPointers/MinimumSizeSubarraySumProblem.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Problems.TwoPointers;
+
+/// <summary>
+/// https://leetcode.com/problems/minimum-size-subarray-sum
+/// </summary>
+public class MinimumSizeSubarraySumProblem
+{
+    public int MinSubArrayLen(int target, int[] nums)
+    {
+        int left = 0;
+        int right = 0;
+        int sumOfCurrentWindow = 0;
+
+        int result = int.MaxValue;
+
+        for (right = 0; right < nums.Length; right++)
+        {
+            sumOfCurrentWindow += nums[right];
+
+            while (sumOfCurrentWindow >= target)
+            {
+                result = Math.Min(result, right - left + 1);
+
+                sumOfCurrentWindow -= nums[left++];
+            }
+        }
+
+        return result == int.MaxValue ? 0 : result;
+    }
+}
---
+++ LeetCode.Tests/TwoPointers/MinimumSizeSubarraySumProblemTests.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.TwoPointers;
+
+namespace LeetCode.Tests.TwoPointers;
+
+public class MinimumSizeSubarraySumProblemTests
+{
+    [Fact]
+    public void MinSubArrayLen_TestCase_1()
+    {
+        // Arrange
+        int target = 7;
+        int[] nums = [2, 3, 1, 2, 4, 3];
+
+        MinimumSizeSubarraySumProblem minimumSizeSubarraySumProblem = new MinimumSizeSubarraySumProblem();
+
+        // Act
+        int actual = minimumSizeSubarraySumProblem.MinSubArrayLen(target, nums);
+
+        // Assert
+        actual.Should().Be(2);
+    }
+
+    [Fact]
+    public void MinSubArrayLen_TestCase_2()
+    {
+        // Arrange
+        int target = 4;
+        int[] nums = [1, 4, 4];
+
+        MinimumSizeSubarraySumProblem minimumSizeSubarraySumProblem = new MinimumSizeSubarraySumProblem();
+
+        // Act
+        int actual = minimumSizeSubarraySumProblem.MinSubArrayLen(target, nums);
+
+        // Assert
+        actual.Should().Be(1);
+    }
+
+    [Fact]
+    public void MinSubArrayLen_TestCase_3()
+    {
+        // Arrange
+        int target = 11;
+        int[] nums = [1, 1, 1, 1, 1, 1, 1, 1];
+
+        MinimumSizeSubarraySumProblem minimumSizeSubarraySumProblem = new MinimumSizeSubarraySumProblem();
+
+        // Act
+        int actual = minimumSizeSubarraySumProblem.MinSubArrayLen(target, nums);
+
+        // Assert
+        actual.Should().Be(0);
+    }
+}
