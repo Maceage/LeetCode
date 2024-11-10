dc1199da4d2d1996b605539b036d4429c32f2a52
Graham Mace <graham@maceage.com>
2024-11-10 18:52:41 +1100

    Add RotateArrayProblem and tests

---
+++ LeetCode.Problems/Array/RotateArrayProblem.cs
@@ -0,0 +1,98 @@
+﻿namespace LeetCode.Problems.Array;
+
+/// <summary>
+/// https://leetcode.com/problems/rotate-array/
+/// </summary>
+public class RotateArrayProblem
+{
+    public void Rotate(int[] nums, int k)
+    {
+        RotateUsingReverse(nums, k);
+        //RotateUsingCyclicReplacements(nums, k);
+        //RotateWithAdditionalArray(nums, k);
+        //RotateBruteForce(nums, k);
+    }
+
+    private static void RotateUsingReverse(int[] nums, int k)
+    {
+        k %= nums.Length;
+
+        Reverse(nums, 0, nums.Length - 1);
+        Reverse(nums, 0, k - 1);
+        Reverse(nums, k, nums.Length - 1);
+    }
+
+    private static void Reverse(int[] nums, int start, int end)
+    {
+        while (start < end)
+        {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    private static void RotateUsingCyclicReplacements(int[] nums, int k)
+    {
+        k %= nums.Length;
+
+        int count = 0;
+
+        for (int start = 0; count < nums.Length; start++)
+        {
+            int current = start;
+            int previous = nums[start];
+
+            do
+            {
+                int next = (current + k) % nums.Length;
+                int temp = nums[next];
+
+                nums[next] = previous;
+                previous = temp;
+                current = next;
+                count++;
+            }
+            while (start != current);
+        }
+    }
+
+    private static void RotateWithAdditionalArray(int[] nums, int k)
+    {
+        int[] newArray = new int[nums.Length];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int addition = i + k;
+            int targetIndex = addition % nums.Length;
+
+            newArray[targetIndex] = nums[i];
+        }
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            nums[i] = newArray[i];
+        }
+    }
+
+    private static void RotateBruteForce(int[] nums, int k)
+    {
+        int i = 0;
+
+        while (i < k)
+        {
+            int lastNum = nums[^1];
+
+            for (int j = nums.Length - 1; j >= 1; j--)
+            {
+                nums[j] = nums[j - 1];
+            }
+
+            nums[0] = lastNum;
+
+            i++;
+        }
+    }
+}
---
+++ LeetCode.Tests/Array/RotateArrayProblemTests.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.Array;
+
+namespace LeetCode.Tests.Array;
+
+public class RotateArrayProblemTests
+{
+    [Fact]
+    public void Rotate_TestCase_1()
+    {
+        // Arrange
+        int[] nums = [1, 2, 3, 4, 5, 6, 7];
+        int k = 3;
+
+        RotateArrayProblem rotateArrayProblem = new RotateArrayProblem();
+
+        // Act
+        rotateArrayProblem.Rotate(nums, k);
+
+        // Assert
+        nums.Should().BeEquivalentTo([5, 6, 7, 1, 2, 3, 4], options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public void Rotate_TestCase_2()
+    {
+        // Arrange
+        int[] nums = [-1, -100, 3, 99];
+        int k = 2;
+
+        RotateArrayProblem rotateArrayProblem = new RotateArrayProblem();
+
+        // Act
+        rotateArrayProblem.Rotate(nums, k);
+
+        // Assert
+        nums.Should().BeEquivalentTo([3, 99, -1, -100], options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public void Rotate_TestCase_3()
+    {
+        // Arrange
+        int[] nums = [-1];
+        int k = 3;
+
+        RotateArrayProblem rotateArrayProblem = new RotateArrayProblem();
+
+        // Act
+        rotateArrayProblem.Rotate(nums, k);
+
+        // Assert
+        nums.Should().BeEquivalentTo([-1], options => options.WithStrictOrdering());
+    }
+}
