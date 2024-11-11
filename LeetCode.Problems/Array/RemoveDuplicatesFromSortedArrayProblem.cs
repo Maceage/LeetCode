000456d68fe14ac6b51b29981d97c313ec07e9f9
Graham Mace <graham@maceage.com>
2024-11-11 17:38:20 +1100

    Add RemoveDuplicatesFromSortedArrayProblem and tests

---
+++ LeetCode.Problems/Array/RemoveDuplicatesFromSortedArrayProblem.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Problems.Array;
+
+/// <summary>
+/// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+/// </summary>
+public class RemoveDuplicatesFromSortedArrayProblem
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        int k = 1;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] != nums[i - 1])
+            {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+}
---
+++ LeetCode.Tests/Array/RemoveDuplicatesFromSortedArrayProblemTests.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.Array;
+
+namespace LeetCode.Tests.Array;
+
+public class RemoveDuplicatesFromSortedArrayProblemTests
+{
+    [Fact]
+    public void RemoveDuplicates_TestCase_1()
+    {
+        // Arrange
+        int[] nums = [1, 1, 2];
+        int[] expectedNums = [1, 2];
+
+        RemoveDuplicatesFromSortedArrayProblem removeDuplicatesFromSortedArrayProblem = new RemoveDuplicatesFromSortedArrayProblem();
+
+        // Act
+        int actual = removeDuplicatesFromSortedArrayProblem.RemoveDuplicates(nums);
+
+        // Assert
+        actual.Should().Be(2);
+
+        for (int i = 0; i < expectedNums.Length; i++)
+        {
+            nums[i].Should().Be(expectedNums[i]);
+        }
+    }
+
+    [Fact]
+    public void RemoveDuplicates_TestCase_2()
+    {
+        // Arrange
+        int[] nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+        int[] expectedNums = [0, 1, 2, 3, 4];
+
+        RemoveDuplicatesFromSortedArrayProblem removeDuplicatesFromSortedArrayProblem = new RemoveDuplicatesFromSortedArrayProblem();
+
+        // Act
+        int actual = removeDuplicatesFromSortedArrayProblem.RemoveDuplicates(nums);
+
+        // Assert
+        actual.Should().Be(5);
+
+        for (int i = 0; i < expectedNums.Length; i++)
+        {
+            nums[i].Should().Be(expectedNums[i]);
+        }
+    }
+}
