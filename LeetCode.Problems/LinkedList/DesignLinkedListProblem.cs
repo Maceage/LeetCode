6487d4db5b0895e7b957a4945a8e6ec4b127ab98
Graham Mace <graham@maceage.com>
2025-01-13 14:33:08 +1100

    Add DesignLinkedListProblem and tests

---
+++ LeetCode.Problems/LinkedList/DesignLinkedListProblem.cs
@@ -0,0 +1,103 @@
+﻿using LeetCode.Problems.Data;
+
+namespace LeetCode.Problems.LinkedList;
+
+public class DesignLinkedListProblem
+{
+    public class MyLinkedList
+    {
+        private readonly ListNode _headNode = new();
+
+        private int _size = 0;
+
+        /// <summary>
+        /// Get the value of the indexth node in the linked list. If the index is invalid, return -1.
+        /// </summary>
+        public int Get(int index)
+        {
+            if ((index < 0) || (index >= _size))
+            {
+                return -1;
+            }
+
+            ListNode currentNode = _headNode;
+
+            for (int i = 0; i < index + 1; ++i)
+            {
+                currentNode = currentNode.Next;
+            }
+
+            return currentNode.Value;
+        }
+
+        /// <summary>
+        /// Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+        /// </summary>
+        public void AddAtHead(int val)
+        {
+            AddAtIndex(0, val);
+        }
+
+        /// <summary>
+        /// Append a node of value val as the last element of the linked list.
+        /// </summary>
+        public void AddAtTail(int val)
+        {
+            AddAtIndex(_size, val);
+        }
+
+        /// <summary>
+        /// Add a node of value val before the indexth node in the linked list.
+        /// If index equals the length of the linked list, the node will be appended to the end of the linked list.
+        /// If index is greater than the length, the node will not be inserted.
+        /// </summary>
+        public void AddAtIndex(int index, int val)
+        {
+            if (index > _size)
+            {
+                return;
+            }
+
+            if (index < 0)
+            {
+                index = 0;
+            }
+
+            ++_size;
+
+            ListNode previousNode = _headNode;
+
+            for (int i = 0; i < index; ++i)
+            {
+                previousNode = previousNode.Next;
+            }
+
+            ListNode listNode = new ListNode(val);
+
+            listNode.Next = previousNode.Next;
+            previousNode.Next = listNode;
+        }
+
+        /// <summary>
+        /// Delete the indexth node in the linked list, if the index is valid.
+        /// </summary>
+        public void DeleteAtIndex(int index)
+        {
+            if ((index < 0) || (index >= _size))
+            {
+                return;
+            }
+
+            _size--;
+
+            ListNode previousNode = _headNode;
+
+            for (int i = 0; i < index; ++i)
+            {
+                previousNode = previousNode.Next;
+            }
+
+            previousNode.Next = previousNode.Next.Next;
+        }
+    }
+}
---
+++ LeetCode.Tests/LinkedList/DesignLinkedListProblemTests.cs
@@ -0,0 +1,73 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.LinkedList;
+
+namespace LeetCode.Tests.LinkedList;
+
+public class DesignLinkedListProblemTests
+{
+    [Fact]
+    public void DesignLinkedList_TestCase_1()
+    {
+        // Act/Assert
+        DesignLinkedListProblem.MyLinkedList myLinkedList = new DesignLinkedListProblem.MyLinkedList();
+
+        myLinkedList.AddAtHead(1);
+        myLinkedList.AddAtTail(3);
+        myLinkedList.AddAtIndex(1, 2);
+        myLinkedList.Get(1).Should().Be(2);
+        myLinkedList.DeleteAtIndex(1);
+        myLinkedList.Get(1).Should().Be(3);
+    }
+
+    [Fact]
+    public void DesignLinkedList_TestCase_2()
+    {
+        // Act/Assert
+        DesignLinkedListProblem.MyLinkedList myLinkedList = new DesignLinkedListProblem.MyLinkedList();
+
+        myLinkedList.AddAtHead(1);
+        myLinkedList.AddAtTail(3);
+        myLinkedList.AddAtIndex(1, 2);
+        myLinkedList.Get(1).Should().Be(2);
+        myLinkedList.DeleteAtIndex(0);
+        myLinkedList.Get(0).Should().Be(2);
+    }
+
+    [Fact]
+    public void DesignLinkedList_TestCase_3()
+    {
+        // Act/Assert
+        DesignLinkedListProblem.MyLinkedList myLinkedList = new DesignLinkedListProblem.MyLinkedList();
+
+        myLinkedList.AddAtHead(2);
+        myLinkedList.DeleteAtIndex(1);
+        myLinkedList.AddAtHead(2);
+        myLinkedList.AddAtHead(7);
+        myLinkedList.AddAtHead(3);
+        myLinkedList.AddAtHead(2);
+        myLinkedList.AddAtHead(5);
+        myLinkedList.AddAtTail(5);
+        myLinkedList.Get(5).Should().Be(2);
+        myLinkedList.DeleteAtIndex(6);
+        myLinkedList.DeleteAtIndex(4);
+    }
+
+    [Fact]
+    public void DesignLinkedList_TestCase_4()
+    {
+        // Act/Assert
+        DesignLinkedListProblem.MyLinkedList myLinkedList = new DesignLinkedListProblem.MyLinkedList();
+
+        myLinkedList.AddAtHead(7);
+        myLinkedList.AddAtHead(2);
+        myLinkedList.AddAtHead(1);
+        myLinkedList.AddAtIndex(3, 0);
+        myLinkedList.DeleteAtIndex(2);
+        myLinkedList.AddAtHead(6);
+        myLinkedList.AddAtTail(4);
+        myLinkedList.Get(4).Should().Be(4);
+        myLinkedList.AddAtHead(4);
+        myLinkedList.AddAtIndex(5, 0);
+        myLinkedList.AddAtHead(6);
+    }
+}
