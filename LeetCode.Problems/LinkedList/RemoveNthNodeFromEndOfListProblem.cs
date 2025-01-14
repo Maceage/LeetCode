90b6f8250da2e84b55389eec9aedd3aad82ee277
Graham Mace <graham@maceage.com>
2025-01-14 16:44:55 +1100

    Add RemoveNthNodeFromEndOfListProblem and tests

---
+++ LeetCode.Problems/LinkedList/RemoveNthNodeFromEndOfListProblem.cs
@@ -0,0 +1,62 @@
+﻿using LeetCode.Problems.Data;
+
+namespace LeetCode.Problems.LinkedList;
+
+public class RemoveNthNodeFromEndOfListProblem
+{
+    public ListNode RemoveNthFromEnd(ListNode head, int n)
+    {
+        return RemoveNthFromEndOnePass(head, n);
+        return RemoveNthFromEndTwoPass(head, n);
+    }
+
+    private static ListNode RemoveNthFromEndOnePass(ListNode head, int n)
+    {
+        ListNode dummy = new ListNode { Next = head };
+
+        ListNode first = dummy;
+        ListNode second = dummy;
+
+        for (int i = 1; i <= n + 1; i++)
+        {
+            first = first.Next;
+        }
+
+        while (first != null)
+        {
+            first = first.Next;
+            second = second.Next;
+        }
+
+        second.Next = second.Next.Next;
+
+        return dummy.Next;
+    }
+
+    private static ListNode RemoveNthFromEndTwoPass(ListNode head, int n)
+    {
+        ListNode dummy = new ListNode { Next = head };
+
+        int length = 0;
+        ListNode first = head;
+
+        while (first != null)
+        {
+            length++;
+            first = first.Next;
+        }
+
+        length -= n;
+        first = dummy;
+
+        while (length > 0)
+        {
+            length--;
+            first = first.Next;
+        }
+
+        first.Next = first.Next.Next;
+
+        return dummy.Next;
+    }
+}
---
+++ LeetCode.Tests/LinkedList/RemoveNthNodeFromEndOfListProblemTests.cs
@@ -0,0 +1,62 @@
+﻿using LeetCode.Problems.Data;
+using LeetCode.Problems.LinkedList;
+using LeetCode.Tests.Extensions;
+
+namespace LeetCode.Tests.LinkedList;
+
+public class RemoveNthNodeFromEndOfListProblemTests
+{
+    [Fact]
+    public void RemoveNthFromEnd_TestCase_1()
+    {
+        // Arrange
+        int[] head = [1, 2, 3, 4, 5];
+        int n = 2;
+
+        ListNode headNode = head.CreateLinkedList();
+
+        RemoveNthNodeFromEndOfListProblem removeNthNodeFromEndOfListProblem = new RemoveNthNodeFromEndOfListProblem();
+
+        // Act
+        ListNode actual = removeNthNodeFromEndOfListProblem.RemoveNthFromEnd(headNode, n);
+
+        // Assert
+        actual.AssertLinkedList([1, 2, 3, 5]);
+    }
+
+    [Fact]
+    public void RemoveNthFromEnd_TestCase_2()
+    {
+        // Arrange
+        int[] head = [1];
+        int n = 1;
+
+        ListNode headNode = head.CreateLinkedList();
+
+        RemoveNthNodeFromEndOfListProblem removeNthNodeFromEndOfListProblem = new RemoveNthNodeFromEndOfListProblem();
+
+        // Act
+        ListNode actual = removeNthNodeFromEndOfListProblem.RemoveNthFromEnd(headNode, n);
+
+        // Assert
+        actual.AssertLinkedList([]);
+    }
+
+    [Fact]
+    public void RemoveNthFromEnd_TestCase_3()
+    {
+        // Arrange
+        int[] head = [1, 2];
+        int n = 1;
+
+        ListNode headNode = head.CreateLinkedList();
+
+        RemoveNthNodeFromEndOfListProblem removeNthNodeFromEndOfListProblem = new RemoveNthNodeFromEndOfListProblem();
+
+        // Act
+        ListNode actual = removeNthNodeFromEndOfListProblem.RemoveNthFromEnd(headNode, n);
+
+        // Assert
+        actual.AssertLinkedList([1]);
+    }
+}
