6cbfa812285143c44ef743931b2770b1bfc1155b
Graham Mace <graham@maceage.com>
2025-01-16 15:20:05 +1100

    Add AddTwoNumbersProblem and tests

---
+++ LeetCode.Problems/LinkedList/AddTwoNumbersProblem.cs
@@ -0,0 +1,42 @@
+﻿using LeetCode.Problems.Data;
+
+namespace LeetCode.Problems.LinkedList;
+
+/// <summary>
+/// https://leetcode.com/problems/add-two-numbers/
+/// </summary>
+public class AddTwoNumbersProblem
+{
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        ListNode dummyHead = new ListNode();
+        ListNode currentNode = dummyHead;
+
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0)
+        {
+            int x = l1?.Value ?? 0;
+            int y = l2?.Value ?? 0;
+
+            int sum = carry + x + y;
+
+            carry = sum / 10;
+
+            currentNode.Next = new ListNode(sum % 10);
+            currentNode = currentNode.Next;
+
+            if (l1 != null)
+            {
+                l1 = l1.Next;
+            }
+
+            if (l2 != null)
+            {
+                l2 = l2.Next;
+            }
+        }
+
+        return dummyHead.Next;
+    }
+}
---
+++ LeetCode.Tests/LinkedList/AddTwoNumbersProblemTests.cs
@@ -0,0 +1,65 @@
+﻿using LeetCode.Problems.Data;
+using LeetCode.Problems.LinkedList;
+using LeetCode.Tests.Extensions;
+
+namespace LeetCode.Tests.LinkedList;
+
+public class AddTwoNumbersProblemTests
+{
+    [Fact]
+    public void AddTwoNumbers_TestCase_1()
+    {
+        // Arrange
+        int[] l1 = [2, 4, 3];
+        int[] l2 = [5, 6, 4];
+
+        ListNode head1Node = l1.CreateLinkedList();
+        ListNode head2Node = l2.CreateLinkedList();
+
+        AddTwoNumbersProblem addTwoNumbersProblem = new AddTwoNumbersProblem();
+
+        // Act
+        ListNode actual = addTwoNumbersProblem.AddTwoNumbers(head1Node, head2Node);
+
+        // Assert
+        actual.AssertLinkedList([7, 0, 8]);
+    }
+
+    [Fact]
+    public void AddTwoNumbers_TestCase_2()
+    {
+        // Arrange
+        int[] l1 = [0];
+        int[] l2 = [0];
+
+        ListNode head1Node = l1.CreateLinkedList();
+        ListNode head2Node = l2.CreateLinkedList();
+
+        AddTwoNumbersProblem addTwoNumbersProblem = new AddTwoNumbersProblem();
+
+        // Act
+        ListNode actual = addTwoNumbersProblem.AddTwoNumbers(head1Node, head2Node);
+
+        // Assert
+        actual.AssertLinkedList([0]);
+    }
+
+    [Fact]
+    public void AddTwoNumbers_TestCase_3()
+    {
+        // Arrange
+        int[] l1 = [9, 9, 9, 9, 9, 9, 9];
+        int[] l2 = [9, 9, 9, 9];
+
+        ListNode head1Node = l1.CreateLinkedList();
+        ListNode head2Node = l2.CreateLinkedList();
+
+        AddTwoNumbersProblem addTwoNumbersProblem = new AddTwoNumbersProblem();
+
+        // Act
+        ListNode actual = addTwoNumbersProblem.AddTwoNumbers(head1Node, head2Node);
+
+        // Assert
+        actual.AssertLinkedList([8, 9, 9, 9, 0, 0, 0, 1]);
+    }
+}
