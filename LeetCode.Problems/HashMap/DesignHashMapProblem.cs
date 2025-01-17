26e6157a652a1765e3ba92694e596412cbfff101
Graham Mace <graham@maceage.com>
2025-01-17 16:31:23 +1100

    Add DesignHashMapProblem and tests

---
+++ LeetCode.Problems/HashMap/DesignHashMapProblem.cs
@@ -0,0 +1,101 @@
+﻿namespace LeetCode.Problems.HashMap;
+
+/// <summary>
+/// https://leetcode.com/problems/design-hashmap
+/// </summary>
+public class DesignHashMapProblem
+{
+    public class MyHashMap
+    {
+        private readonly int _keySpace;
+        private readonly List<Bucket> _hashTable;
+
+        public MyHashMap()
+        {
+            // Prime number is best
+            _keySpace = 2069;
+            _hashTable = new List<Bucket>();
+
+            for (int i = 0; i < _keySpace; i++)
+            {
+                _hashTable.Add(new Bucket());
+            }
+        }
+
+        public void Put(int key, int value)
+        {
+            int hashKey = key % _keySpace;
+
+            _hashTable[hashKey].Update(key, value);
+        }
+
+        public int Get(int key)
+        {
+            int hashKey = key % _keySpace;
+
+            return _hashTable[hashKey].Get(key);
+        }
+
+        public void Remove(int key)
+        {
+            int hashKey = key % _keySpace;
+
+            _hashTable[hashKey].Remove(key);
+        }
+
+        private class Pair<T, U>(T first, U second)
+        {
+            public T First { get; set; } = first;
+            public U Second { get; set; } = second;
+        }
+
+        private class Bucket
+        {
+            private readonly List<Pair<int, int>> _bucket = [];
+
+            public int Get(int key)
+            {
+                foreach (var pair in _bucket)
+                {
+                    if (pair.First.Equals(key))
+                    {
+                        return pair.Second;
+                    }
+                }
+
+                return -1;
+            }
+
+            public void Update(int key, int value)
+            {
+                bool found = false;
+
+                foreach (var pair in _bucket)
+                {
+                    if (pair.First.Equals(key))
+                    {
+                        pair.Second = value;
+                        found = true;
+                    }
+                }
+
+                if (!found)
+                {
+                    _bucket.Add(new Pair<int, int>(key, value));
+                }
+            }
+
+            public void Remove(int key)
+            {
+                foreach (var pair in _bucket)
+                {
+                    if (pair.First.Equals(key))
+                    {
+                        _bucket.Remove(pair);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ LeetCode.Tests/HashMap/DesignHashMapProblemTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.HashMap;
+
+namespace LeetCode.Tests.HashMap;
+
+public class DesignHashMapProblemTests
+{
+    [Fact]
+    public void MyHashMap_TestCase_1()
+    {
+        // Arrange
+        DesignHashMapProblem.MyHashMap myHashMap = new DesignHashMapProblem.MyHashMap();
+
+        // Act/Assert
+        myHashMap.Put(1,1);
+        myHashMap.Put(2,2);
+        myHashMap.Get(1).Should().Be(1);
+        myHashMap.Get(3).Should().Be(-1);
+        myHashMap.Put(2,1);
+        myHashMap.Get(2).Should().Be(1);
+        myHashMap.Remove(2);
+        myHashMap.Get(2).Should().Be(-1);
+    }
+}
