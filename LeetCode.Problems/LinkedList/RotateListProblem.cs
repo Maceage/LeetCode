42782dce3f35a24950619f45703f430093561040
Graham Mace <graham@maceage.com>
2025-01-17 14:44:46 +1100

    Add RotateListProblem and tests

---
+++ LeetCode.Problems/LinkedList/RotateListProblem.cs
@@ -0,0 +1,49 @@
+﻿using LeetCode.Problems.Data;
+
+namespace LeetCode.Problems.LinkedList;
+
+/// <summary>
+/// https://leetcode.com/problems/rotate-list
+/// </summary>
+public class RotateListProblem
+{
+    public ListNode RotateRight(ListNode head, int k)
+    {
+        if (head == null)
+        {
+            return head;
+        }
+
+        if (head.Next == null)
+        {
+            return head;
+        }
+
+        ListNode oldTail = head;
+
+        int nodeCount;
+
+        for (nodeCount = 1; oldTail.Next != null; nodeCount++)
+        {
+            oldTail = oldTail.Next;
+        }
+
+        // Reached tail node so join to start
+        oldTail.Next = head;
+
+        ListNode newTail = head;
+
+        // Find new tail : (n - k % n - 1)th node
+        // And new head : (n - k % n)th node
+        for (int i = 0; i < nodeCount - (k % nodeCount) - 1; i++)
+        {
+            newTail = newTail.Next;
+        }
+
+        ListNode newHead = newTail.Next;
+
+        newTail.Next = null;
+
+        return newHead;
+    }
+}
---
+++ LeetCode.Tests/LinkedList/RotateListProblemTests.cs
@@ -0,0 +1,44 @@
+﻿using LeetCode.Problems.Data;
+using LeetCode.Problems.LinkedList;
+using LeetCode.Tests.Extensions;
+
+namespace LeetCode.Tests.LinkedList;
+
+public class RotateListProblemTests
+{
+    [Fact]
+    public void RotateRight_TestCase_1()
+    {
+        // Arrange
+        int[] head = [1, 2, 3, 4, 5];
+        int k = 2;
+
+        ListNode headNode = head.CreateLinkedList();
+
+        RotateListProblem rotateListProblem = new RotateListProblem();
+
+        // Act
+        ListNode actual = rotateListProblem.RotateRight(headNode, k);
+
+        // Assert
+        actual.AssertLinkedList([4, 5, 1, 2, 3]);
+    }
+
+    [Fact]
+    public void RotateRight_TestCase_2()
+    {
+        // Arrange
+        int[] head = [0, 1, 2];
+        int k = 4;
+
+        ListNode headNode = head.CreateLinkedList();
+
+        RotateListProblem rotateListProblem = new RotateListProblem();
+
+        // Act
+        ListNode actual = rotateListProblem.RotateRight(headNode, k);
+
+        // Assert
+        actual.AssertLinkedList([2, 0, 1]);
+    }
+}
