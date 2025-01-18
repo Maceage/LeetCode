8e6f131473444d07dcba3a7311b755eee53740bd
Graham Mace <graham@maceage.com>
2025-01-18 17:33:49 +1100

    Add HappyNumberProblem and tests

---
+++ LeetCode.Problems/HashMap/HappyNumberProblem.cs
@@ -0,0 +1,82 @@
+﻿namespace LeetCode.Problems.HashMap;
+
+/// <summary>
+/// https://leetcode.com/problems/happy-number
+/// </summary>
+public class HappyNumberProblem
+{
+    public bool IsHappy(int n)
+    {
+        return IsHappyHardcodingOnlyCycle(n);
+        return IsHappyFloydCycleFindingAlgorithm(n);
+        return IsHappyOriginal(n);
+    }
+
+    private static bool IsHappyHardcodingOnlyCycle(int n)
+    {
+        HashSet<int> cycleMembers = [4, 16, 37, 58, 89, 145, 42, 20];
+
+        while (n != 1 && !cycleMembers.Contains(n))
+        {
+            n = CalculateSumOfDigitSquares(n);
+        }
+
+        return n == 1;
+    }
+
+    private static bool IsHappyFloydCycleFindingAlgorithm(int n)
+    {
+        int slowRunner = n;
+        int fastRunner = CalculateSumOfDigitSquares(n);
+
+        while (fastRunner != 1 && slowRunner != fastRunner)
+        {
+            slowRunner = CalculateSumOfDigitSquares(slowRunner);
+            fastRunner = CalculateSumOfDigitSquares(CalculateSumOfDigitSquares(fastRunner));
+        }
+
+        return fastRunner == 1;
+    }
+
+    private static bool IsHappyOriginal(int n)
+    {
+        bool isHappy = false;
+
+        HashSet<int> previousSums = new HashSet<int>();
+
+        int sum = CalculateSumOfDigitSquares(n);
+
+        while (!previousSums.Contains(sum))
+        {
+            previousSums.Add(sum);
+
+            sum = CalculateSumOfDigitSquares(sum);
+
+            if (sum == 1)
+            {
+                isHappy = true;
+                break;
+            }
+        }
+        
+        return isHappy;
+    }
+
+    private static int CalculateSumOfDigitSquares(int n)
+    {
+        int result = 0;
+
+        while (n > 0)
+        {
+            int digit = n % 10;
+
+            n = n / 10;
+
+            int x = digit * digit;
+
+            result += x;
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode.Tests/HashMap/HappyNumberProblemTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.HashMap;
+
+namespace LeetCode.Tests.HashMap;
+
+public class HappyNumberProblemTests
+{
+    [Fact]
+    public void IsHappy_TestCase_1()
+    {
+        // Arrange
+        int n = 19;
+
+        HappyNumberProblem happyNumberProblem = new HappyNumberProblem();
+
+        // Act
+        bool actual = happyNumberProblem.IsHappy(n);
+
+        // Assert
+        actual.Should().BeTrue();
+    }
+
+    [Fact]
+    public void IsHappy_TestCase_2()
+    {
+        // Arrange
+        int n = 2;
+
+        HappyNumberProblem happyNumberProblem = new HappyNumberProblem();
+
+        // Act
+        bool actual = happyNumberProblem.IsHappy(n);
+
+        // Assert
+        actual.Should().BeFalse();
+    }
+}
