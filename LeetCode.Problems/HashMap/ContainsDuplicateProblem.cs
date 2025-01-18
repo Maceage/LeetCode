cdde8a511289dd5387e6f3e6780d8fe7c9918ae8
Graham Mace <graham@maceage.com>
2025-01-18 16:05:30 +1100

    Add ContainsDuplicateProblem and tests

---
+++ LeetCode.Problems/HashMap/ContainsDuplicateProblem.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Problems.HashMap;
+
+/// <summary>
+/// https://leetcode.com/problems/contains-duplicate
+/// </summary>
+public class ContainsDuplicateProblem
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        bool containsDuplicate = false;
+
+        HashSet<int> hashSet = new HashSet<int>();
+
+        if (nums != null)
+        {
+            foreach (int num in nums)
+            {
+                if (!hashSet.Add(num))
+                {
+                    containsDuplicate = true;
+                    break;
+                }
+            }
+        }
+
+        return containsDuplicate;
+    }
+}
---
+++ LeetCode.Tests/HashMap/ContainsDuplicateProblemTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.HashMap;
+
+namespace LeetCode.Tests.HashMap;
+
+public class ContainsDuplicateProblemTests
+{
+    [Fact]
+    public void ContainsDuplicate_TestCase_1()
+    {
+        // Arrange
+        int[] nums = [1, 2, 3, 1];
+
+        ContainsDuplicateProblem containsDuplicateProblem = new ContainsDuplicateProblem();
+
+        // Act
+        bool actual = containsDuplicateProblem.ContainsDuplicate(nums);
+
+        // Assert
+        actual.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ContainsDuplicate_TestCase_2()
+    {
+        // Arrange
+        int[] nums = [1, 2, 3, 4];
+
+        ContainsDuplicateProblem containsDuplicateProblem = new ContainsDuplicateProblem();
+
+        // Act
+        bool actual = containsDuplicateProblem.ContainsDuplicate(nums);
+
+        // Assert
+        actual.Should().BeFalse();
+    }
+
+    [Fact]
+    public void ContainsDuplicate_TestCase_3()
+    {
+        // Arrange
+        int[] nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2];
+
+        ContainsDuplicateProblem containsDuplicateProblem = new ContainsDuplicateProblem();
+
+        // Act
+        bool actual = containsDuplicateProblem.ContainsDuplicate(nums);
+
+        // Assert
+        actual.Should().BeTrue();
+    }
+}
