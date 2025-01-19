737e0464ce01be6752b80f36c60eef504f98f251
Graham Mace <graham@maceage.com>
2025-01-19 17:58:27 +1100

    Add FirstUniqueCharacterInAStringProblem and tests

---
+++ LeetCode.Problems/HashMap/FirstUniqueCharacterInAStringProblem.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode.Problems.HashMap;
+
+/// <summary>
+/// https://leetcode.com/problems/first-unique-character-in-a-string
+/// </summary>
+public class FirstUniqueCharacterInAStringProblem
+{
+    public int FirstUniqueChar(string s)
+    {
+        if (s == null)
+        {
+            return -1;
+        }
+
+        int charIndex = -1;
+
+        Dictionary<char, int> charactersDictionary = new Dictionary<char, int>();
+
+        foreach (char c in s)
+        {
+            if (!charactersDictionary.TryAdd(c, 1))
+            {
+                charactersDictionary[c]++;
+            }
+        }
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            char c = s[i];
+
+            if (charactersDictionary[c] == 1)
+            {
+                charIndex = i;
+                break;
+            }
+        }
+
+        return charIndex;
+    }
+}
---
+++ LeetCode.Tests/HashMap/FirstUniqueCharacterInAStringProblemTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.HashMap;
+
+namespace LeetCode.Tests.HashMap;
+
+public class FirstUniqueCharacterInAStringProblemTests
+{
+    [Fact]
+    public void FirstUniqueChar_TestCase_1()
+    {
+        // Arrange
+        string s = "leetcode";
+
+        FirstUniqueCharacterInAStringProblem firstUniqueCharacterInAStringProblem = new FirstUniqueCharacterInAStringProblem();
+
+        // Act
+        int actual = firstUniqueCharacterInAStringProblem.FirstUniqueChar(s);
+
+        // Assert
+        actual.Should().Be(0);
+    }
+
+    [Fact]
+    public void FirstUniqueChar_TestCase_2()
+    {
+        // Arrange
+        string s = "loveleetcode";
+
+        FirstUniqueCharacterInAStringProblem firstUniqueCharacterInAStringProblem = new FirstUniqueCharacterInAStringProblem();
+
+        // Act
+        int actual = firstUniqueCharacterInAStringProblem.FirstUniqueChar(s);
+
+        // Assert
+        actual.Should().Be(2);
+    }
+
+    [Fact]
+    public void FirstUniqueChar_TestCase_3()
+    {
+        // Arrange
+        string s = "aabb";
+
+        FirstUniqueCharacterInAStringProblem firstUniqueCharacterInAStringProblem = new FirstUniqueCharacterInAStringProblem();
+
+        // Act
+        int actual = firstUniqueCharacterInAStringProblem.FirstUniqueChar(s);
+
+        // Assert
+        actual.Should().Be(-1);
+    }
+}
