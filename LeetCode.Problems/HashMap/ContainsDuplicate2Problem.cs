c94b2feb9564c3298c46d1bc531a39e3a55fc4e2
Graham Mace <graham@maceage.com>
2025-01-19 18:29:57 +1100

    Add ContainsDuplicate2Problem and tests

---
+++ LeetCode.Problems/HashMap/ContainsDuplicate2Problem.cs
@@ -0,0 +1,70 @@
+﻿namespace LeetCode.Problems.HashMap;
+
+/// <summary>
+/// https://leetcode.com/problems/contains-duplicate-ii
+/// </summary>
+public class ContainsDuplicate2Problem
+{
+    public bool ContainsNearbyDuplicate(int[] nums, int k)
+    {
+        return ContainsNearbyDuplicateHashTable(nums, k);
+        return ContainsNearbyDuplicateBinarySearchTree(nums, k);
+        return ContainsNearbyDuplicateNaiveLinearSearch(nums, k);
+    }
+
+    private static bool ContainsNearbyDuplicateHashTable(int[] nums, int k)
+    {
+        HashSet<int> set = new HashSet<int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (!set.Add(nums[i]))
+            {
+                return true;
+            }
+
+            if (set.Count > k)
+            {
+                set.Remove(nums[i - k]);
+            }
+        }
+
+        return false;
+    }
+
+    private static bool ContainsNearbyDuplicateBinarySearchTree(int[] nums, int k)
+    {
+        SortedSet<int> set = new SortedSet<int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (!set.Add(nums[i]))
+            {
+                return true;
+            }
+
+            if (set.Count > k)
+            {
+                set.Remove(nums[i - k]);
+            }
+        }
+
+        return false;
+    }
+
+    private static bool ContainsNearbyDuplicateNaiveLinearSearch(int[] nums, int k)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = Math.Max(i - k, 0); j < i; j++)
+            {
+                if (nums[i] == nums[j])
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
---
+++ LeetCode.Tests/HashMap/ContainsDuplicate2ProblemTests.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.HashMap;
+
+namespace LeetCode.Tests.HashMap;
+
+public class ContainsDuplicate2ProblemTests
+{
+    [Fact]
+    public void ContainsNearbyDuplicate_TestCase_1()
+    {
+        // Arrange
+        int[] nums = [1, 2, 3, 1];
+        int k = 3;
+
+        ContainsDuplicate2Problem containsDuplicate2Problem = new ContainsDuplicate2Problem();
+
+        // Act
+        bool actual = containsDuplicate2Problem.ContainsNearbyDuplicate(nums, k);
+
+        // Assert
+        actual.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ContainsNearbyDuplicate_TestCase_2()
+    {
+        // Arrange
+        int[] nums = [1, 0, 1, 1];
+        int k = 1;
+
+        ContainsDuplicate2Problem containsDuplicate2Problem = new ContainsDuplicate2Problem();
+
+        // Act
+        bool actual = containsDuplicate2Problem.ContainsNearbyDuplicate(nums, k);
+
+        // Assert
+        actual.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ContainsNearbyDuplicate_TestCase_3()
+    {
+        // Arrange
+        int[] nums = [1, 2, 3, 1, 2, 3];
+        int k = 2;
+
+        ContainsDuplicate2Problem containsDuplicate2Problem = new ContainsDuplicate2Problem();
+
+        // Act
+        bool actual = containsDuplicate2Problem.ContainsNearbyDuplicate(nums, k);
+
+        // Assert
+        actual.Should().BeFalse();
+    }
+}
