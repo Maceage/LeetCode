d145a194f654aa83bd0e680056a03a80bd3d9899
Graham Mace <graham@maceage.com>
2025-01-19 18:50:57 +1100

    Add LoggerRateLimiterProblem and tests

---
+++ LeetCode.Problems/HashMap/LoggerRateLimiterProblem.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode.Problems.HashMap;
+
+/// <summary>
+/// https://leetcode.com/problems/logger-rate-limiter
+/// </summary>
+public class LoggerRateLimiterProblem
+{
+    public class Logger
+    {
+        private readonly Dictionary<string, int> _messagesDictionary = new();
+
+        public bool ShouldPrintMessage(int timestamp, string message)
+        {
+            bool shouldPrint = false;
+
+            if (!_messagesDictionary.TryAdd(message, timestamp))
+            {
+                int lastTimeStamp = _messagesDictionary[message];
+
+                int calculatedTimestamp = lastTimeStamp + 10;
+
+                if (timestamp >= calculatedTimestamp)
+                {
+                    _messagesDictionary.Remove(message);
+
+                    _messagesDictionary.Add(message, timestamp);
+
+                    shouldPrint = true;
+                }
+            }
+            else
+            {
+                shouldPrint = true;
+            }
+
+            return shouldPrint;
+        }
+    }
+}
---
+++ LeetCode.Tests/HashMap/LoggerRateLimiterProblemTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.HashMap;
+
+namespace LeetCode.Tests.HashMap;
+
+public class LoggerRateLimiterProblemTests
+{
+    [Fact]
+    public void Logger_TestCase_1()
+    {
+        // Arrange
+        LoggerRateLimiterProblem.Logger logger = new LoggerRateLimiterProblem.Logger();
+
+        // Act/Assert
+        logger.ShouldPrintMessage(1, "foo").Should().BeTrue();
+        logger.ShouldPrintMessage(2, "bar").Should().BeTrue();
+        logger.ShouldPrintMessage(3, "foo").Should().BeFalse();
+        logger.ShouldPrintMessage(8, "bar").Should().BeFalse();
+        logger.ShouldPrintMessage(10, "foo").Should().BeFalse();
+        logger.ShouldPrintMessage(11, "foo").Should().BeTrue();
+    }
+}
