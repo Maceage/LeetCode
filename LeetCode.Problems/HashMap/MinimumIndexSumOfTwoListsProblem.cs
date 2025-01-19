3c44b782eb2855090bfb2e46d1a980350a0cb87c
Graham Mace <graham@maceage.com>
2025-01-19 16:07:15 +1100

    Add MinimumIndexSumOfTwoListsProblem and tests

---
+++ LeetCode.Problems/HashMap/MinimumIndexSumOfTwoListsProblem.cs
@@ -0,0 +1,95 @@
+﻿namespace LeetCode.Problems.HashMap;
+
+/// <summary>
+/// https://leetcode.com/problems/minimum-index-sum-of-two-lists
+/// </summary>
+public class MinimumIndexSumOfTwoListsProblem
+{
+    public string[] FindRestaurant(string[] list1, string[] list2)
+    {
+        return FindRestaurantWithHashmap(list1, list2);
+        return FindRestaurantOriginal(list1, list2);
+    }
+
+    private static string[] FindRestaurantWithHashmap(string[] list1, string[] list2)
+    {
+        Dictionary<int, List<string>> map = new Dictionary<int, List<string>>();
+
+        for (int i = 0; i < list1.Length; i++)
+        {
+            for (int j = 0; j < list2.Length; j++)
+            {
+                if (list1[i].Equals(list2[j]))
+                {
+                    int sum = i + j;
+
+                    if (!map.ContainsKey(sum))
+                    {
+                        map.Add(sum, []);
+                    }
+
+                    map[sum].Add(list1[i]);
+                }
+            }
+        }
+
+        int minIndexSum = int.MaxValue;
+
+        foreach (int mapKey in map.Keys)
+        {
+            minIndexSum = Math.Min(minIndexSum, mapKey);
+        }
+
+        return map[minIndexSum].ToArray();
+    }
+
+    private static string[] FindRestaurantOriginal(string[] list1, string[] list2)
+    {
+        if (list1 == null || list2 == null)
+        {
+            return [];
+        }
+
+        int indexSum = int.MaxValue;
+        List<string> wordList = new List<string>();
+
+        Dictionary<string, int> list1Indexes = GetWordIndexesDictionary(list1);
+        Dictionary<string, int> list2Indexes = GetWordIndexesDictionary(list2);
+
+        foreach (string key in list1Indexes.Keys)
+        {
+            if (list1Indexes.TryGetValue(key, out var list1Index) && (list2Indexes.TryGetValue(key, out var list2Index)))
+            {
+                int sum = list1Index + list2Index;
+
+                if (sum < indexSum)
+                {
+                    wordList.Clear();
+                    wordList.Add(key);
+
+                    indexSum = sum;
+                }
+                else if (sum == indexSum)
+                {
+                    wordList.Add(key);
+                }
+            }
+        }
+
+        return wordList.ToArray();
+    }
+
+    private static Dictionary<string, int> GetWordIndexesDictionary(string[] list)
+    {
+        Dictionary<string, int> wordIndexes = new Dictionary<string, int>();
+
+        for (int i = 0; i < list.Length; i++)
+        {
+            string word = list[i];
+
+            wordIndexes.Add(word, i);
+        }
+
+        return wordIndexes;
+    }
+}
---
+++ LeetCode.Tests/HashMap/MinimumIndexSumOfTwoListsProblemTests.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using LeetCode.Problems.HashMap;
+
+namespace LeetCode.Tests.HashMap;
+
+public class MinimumIndexSumOfTwoListsProblemTests
+{
+    [Fact]
+    public void FindRestaurant_TestCase_1()
+    {
+        // Arrange
+        string[] list1 = ["Shogun", "Tapioca Express", "Burger King", "KFC"];
+        string[] list2 = ["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"];
+
+        MinimumIndexSumOfTwoListsProblem minimumIndexSumOfTwoListsProblem = new MinimumIndexSumOfTwoListsProblem();
+
+        // Act
+        string[] actual = minimumIndexSumOfTwoListsProblem.FindRestaurant(list1, list2);
+
+        // Assert
+        actual.Should().BeEquivalentTo("Shogun");
+    }
+
+    [Fact]
+    public void FindRestaurant_TestCase_2()
+    {
+        // Arrange
+        string[] list1 = ["Shogun", "Tapioca Express", "Burger King", "KFC"];
+        string[] list2 = ["KFC", "Shogun", "Burger King"];
+
+        MinimumIndexSumOfTwoListsProblem minimumIndexSumOfTwoListsProblem = new MinimumIndexSumOfTwoListsProblem();
+
+        // Act
+        string[] actual = minimumIndexSumOfTwoListsProblem.FindRestaurant(list1, list2);
+
+        // Assert
+        actual.Should().BeEquivalentTo("Shogun");
+    }
+
+    [Fact]
+    public void FindRestaurant_TestCase_3()
+    {
+        // Arrange
+        string[] list1 = ["happy", "sad", "good"];
+        string[] list2 = ["sad", "happy", "good"];
+
+        MinimumIndexSumOfTwoListsProblem minimumIndexSumOfTwoListsProblem = new MinimumIndexSumOfTwoListsProblem();
+
+        // Act
+        string[] actual = minimumIndexSumOfTwoListsProblem.FindRestaurant(list1, list2);
+
+        // Assert
+        actual.Should().BeEquivalentTo("sad", "happy");
+    }
+}
