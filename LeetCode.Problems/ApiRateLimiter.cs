3b6298cca672f5163ec28759e7a7da2bd048b748
Graham Mace <graham@maceage.com>
2025-01-23 02:53:19 +1100

    Add ApiRateLimiter and tests

---
+++ LeetCode.Problems/ApiRateLimiter.cs
@@ -0,0 +1,51 @@
+﻿namespace LeetCode.Problems;
+
+public class ApiRateLimiter(int timeWindow, int numberOfRequests)
+{
+    private readonly Dictionary<int, CustomerRequest> _customerRequestCount = new();
+
+    public bool RateLimit(int customerId, DateTime requestDateTime)
+    {
+        bool shouldRateLimit = false;
+
+        if (_customerRequestCount.TryGetValue(customerId, out var customerRequest))
+        {
+            int requestCount = customerRequest.RequestCount;
+            DateTime firstRequestDateTime = customerRequest.FirstRequestDateTime;
+
+            TimeSpan timeDifference = requestDateTime - firstRequestDateTime;
+
+            if ((requestCount > numberOfRequests) && (timeDifference.Seconds > timeWindow))
+            {
+                customerRequest.RequestCount = 1;
+                customerRequest.FirstRequestDateTime = requestDateTime;
+            }
+            else if ((requestCount >= numberOfRequests) && (timeDifference.Seconds < timeWindow))
+            {
+                shouldRateLimit = true;
+            }
+            else
+            {
+                customerRequest.RequestCount++;
+            }
+        }
+        else
+        {
+            customerRequest = new CustomerRequest
+            {
+                FirstRequestDateTime = requestDateTime,
+                RequestCount = 1
+            };
+
+            _customerRequestCount.Add(customerId, customerRequest);
+        }
+
+        return shouldRateLimit;
+    }
+
+    private class CustomerRequest
+    {
+        public DateTime FirstRequestDateTime { get; set; }
+        public int RequestCount { get; set; }
+    }
+}
---
+++ LeetCode.Tests/ApiRateLimiterTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using LeetCode.Problems;
+
+namespace LeetCode.Tests;
+
+public class ApiRateLimiterTests
+{
+    [Fact]
+    public void RateLimit_TestCase_1()
+    {
+        DateTime dateTime = DateTime.Now;
+
+        // Arrange
+        ApiRateLimiter apiRateLimiter = new ApiRateLimiter(60, 5);
+
+        // Act/Assert
+        apiRateLimiter.RateLimit(123, dateTime).Should().BeFalse();
+        apiRateLimiter.RateLimit(123, dateTime.AddSeconds(10)).Should().BeFalse();
+        apiRateLimiter.RateLimit(123, dateTime.AddSeconds(20)).Should().BeFalse();
+        apiRateLimiter.RateLimit(123, dateTime.AddSeconds(30)).Should().BeFalse();
+        apiRateLimiter.RateLimit(123, dateTime.AddSeconds(40)).Should().BeFalse();
+        apiRateLimiter.RateLimit(123, dateTime.AddSeconds(50)).Should().BeTrue();
+        apiRateLimiter.RateLimit(123, dateTime.AddSeconds(60)).Should().BeTrue();
+
+        apiRateLimiter.RateLimit(123, dateTime.AddSeconds(70)).Should().BeTrue();
+    }
+}
